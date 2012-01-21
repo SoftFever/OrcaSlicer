0e23f20df1af359aaabe0a3c7eabace3ee493aba
Alessandro Ranellucci <aar@cpan.org>
2012-01-21 11:48:03 +0100

    Poor overlap between perimeters and infill

--- lib/Slic3r/Fill/Rectilinear.pm
+++ lib/Slic3r/Fill/Rectilinear.pm
@@ -32,7 +32,7 @@ sub fill_surface {
         $flow_spacing = unscale $distance_between_lines;
     }
     
-    my $overlap_distance = $Slic3r::nozzle_diameter * 0.20;
+    my $overlap_distance = scale $Slic3r::flow_width * 0.2;
     
     my @paths = ();
     my $x = $bounding_box->[X1];
