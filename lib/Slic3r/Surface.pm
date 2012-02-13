0deee20ade3625d1b352c0327ccd57a92dada95c
Alessandro Ranellucci <aar@cpan.org>
2012-02-13 18:02:54 +0100

    Bugfix: bad syntax in last commit. #202

--- lib/Slic3r/Surface.pm
+++ lib/Slic3r/Surface.pm
@@ -39,10 +39,12 @@ sub group {
 
 sub offset {
     my $self = shift;
-    return map (ref $self)->new(
-        expolygon => $_,
-        map { $_ => $self->$_ } qw(surface_type depth_layers bridge_angle),
-    ), $self->expolygon->offset_ex(@_);
+    return map {
+        (ref $self)->new(
+            expolygon => $_,
+            map { $_ => $self->$_ } qw(surface_type depth_layers bridge_angle),
+        )
+    } $self->expolygon->offset_ex(@_);
 }
 
 sub add_hole {
