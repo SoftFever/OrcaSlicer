ba58e0733fffaa39e6ca6babf69a91de194ee575
Alessandro Ranellucci <aar@cpan.org>
2012-08-24 19:19:45 +0200

    Avoid backwards moves when doing honeycomb infill. #492

--- lib/Slic3r/Fill/Honeycomb.pm
+++ lib/Slic3r/Fill/Honeycomb.pm
@@ -83,7 +83,10 @@ sub fill_surface {
         $self->cache->{$cache_id} = [@polygons];
     }
     
-    my @paths = map Slic3r::Polyline->new(@$_, $_->[0]), map @$_, @{intersection_ex(
+    # build polylines from polygons without re-appending the initial point:
+    # this cuts the last segment on purpose, so that the jump to the next 
+    # path is more straight
+    my @paths = map Slic3r::Polyline->new(@$_), map @$_, @{intersection_ex(
         $self->cache->{$cache_id},
         [ map @$_, $expolygon->offset_ex($overlap_distance) ],
     )};
