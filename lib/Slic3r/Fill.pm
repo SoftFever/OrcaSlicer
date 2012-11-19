183736dd9afc711d47a2cef67cd5df8024856076
Alessandro Ranellucci <aar@cpan.org>
2012-11-19 12:37:05 +0100

    Infill flow for bottom layer was calculated incorrectly when first_layer_height != layer_height

--- lib/Slic3r/Fill.pm
+++ lib/Slic3r/Fill.pm
@@ -170,7 +170,7 @@ sub make_fill {
                         : $is_solid
                             ? ($surface->surface_type == S_TYPE_TOP ? EXTR_ROLE_TOPSOLIDFILL : EXTR_ROLE_SOLIDFILL)
                             : EXTR_ROLE_FILL),
-                    height => $surface->depth_layers * $Slic3r::Config->layer_height,
+                    height => $surface->depth_layers * $layer->height,
                     flow_spacing => $params->{flow_spacing} || (warn "Warning: no flow_spacing was returned by the infill engine, please report this to the developer\n"),
                 ), @paths,
             ],
