124a1fd84ee947821e99f45e0a8bfb91c561becb
Alessandro Ranellucci <aar@cpan.org>
2012-11-19 18:03:49 +0100

    Raise default frequency limit to 25Hz

--- lib/Slic3r/Config.pm
+++ lib/Slic3r/Config.pm
@@ -416,7 +416,7 @@ our $Options = {
         sidetext => 'Hz',
         cli     => 'vibration-limit=f',
         type    => 'f',
-        default => 15,
+        default => 25,
     },
     
     # print options
