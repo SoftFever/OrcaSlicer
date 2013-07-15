e485f0b130d5aad46cf62bd1e366a300cb642d34
Alessandro Ranellucci <aar@cpan.org>
2013-07-15 13:50:53 +0200

    Return a reference to the actual item instead of a clone when accessing contents of a SurfaceCollection

--- xs/src/Surface.hpp
+++ xs/src/Surface.hpp
@@ -23,6 +23,7 @@ class Surface
     unsigned short  thickness_layers;   // in layers
     double          bridge_angle;
     unsigned short  extra_perimeters;
+    bool            in_collection;
 };
 
 }
