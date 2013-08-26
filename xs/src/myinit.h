f8ac3aa68f3f562cb42eac71e96f027736bca84e
Alessandro Ranellucci <aar@cpan.org>
2013-08-26 20:56:36 +0200

    Fix compilation on Windows

--- xs/src/myinit.h
+++ xs/src/myinit.h
@@ -1,6 +1,9 @@
 #ifndef _myinit_h_
 #define _myinit_h_
 
+// this needs to be included early for MSVC (listing it in Build.PL is not enough)
+#include <ostream>
+
 extern "C" {
 #include "EXTERN.h"
 #include "perl.h"
