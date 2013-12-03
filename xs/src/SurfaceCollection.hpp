e9b87b69df85e0693224afb40ebbbd0e24d3d710
Alessandro Ranellucci <aar@cpan.org>
2013-12-04 00:10:31 +0100

    Bugfix: segfault in SurfaceCollection->group(), fixed with a better implementation. #1566

--- xs/src/SurfaceCollection.hpp
+++ xs/src/SurfaceCollection.hpp
@@ -6,26 +6,12 @@
 
 namespace Slic3r {
 
-struct t_surface_group_key {
-    SurfaceType     surface_type;
-    double          thickness;
-    unsigned short  thickness_layers;
-    double          bridge_angle;
-    
-    bool operator< (const t_surface_group_key &key) const {
-        return (this->surface_type      < key.surface_type)
-            || (this->thickness         < key.thickness)
-            || (this->thickness_layers  < key.thickness_layers)
-            || (this->bridge_angle      < key.bridge_angle);
-    }
-};
-
 class SurfaceCollection
 {
     public:
     Surfaces surfaces;
     void simplify(double tolerance);
-    void group(std::vector<SurfacesPtr> &retval, bool merge_solid = false);
+    void group(std::vector<SurfacesPtr> *retval, bool merge_solid = false);
 };
 
 }
