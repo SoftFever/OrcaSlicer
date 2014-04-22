370df56f732598a9de9db6966f517c552470cebb
Alessandro Ranellucci <aar@cpan.org>
2014-04-22 11:59:01 +0200

    Bugfix: version check caused crash

--- lib/Slic3r/GUI.pm
+++ lib/Slic3r/GUI.pm
@@ -334,6 +334,7 @@ sub check_version {
         } else {
             Slic3r::GUI::show_error(undef, "Failed to check for updates. Try later.") if $p{manual};
         }
+        Slic3r::thread_cleanup();
     })->detach;
 }
 
