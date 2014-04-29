913ab54a2bacf5944b2a6a3b1271fe903d1fc7bb
Alessandro Ranellucci <aar@cpan.org>
2014-04-29 15:25:14 +0200

    Bugfix: gap fill was not inserted in the correct order before leaving island. Includes regression test. #1907

--- lib/Slic3r/Fill.pm
+++ lib/Slic3r/Fill.pm
@@ -244,9 +244,9 @@ sub make_fill {
     }
     
     # add thin fill regions
-    if ($layerm->thin_fills->count > 0) {
-        push @fills, Slic3r::ExtrusionPath::Collection->new(@{$layerm->thin_fills});
-        push @fills_ordering_points, $fills[-1]->first_point;
+    foreach my $thin_fill (@{$layerm->thin_fills}) {
+        push @fills, Slic3r::ExtrusionPath::Collection->new($thin_fill);
+        push @fills_ordering_points, $thin_fill->first_point;
     }
     
     # organize infill paths using a nearest-neighbor search
