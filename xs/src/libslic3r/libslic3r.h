6adc3477c9d08d2cfa0e6902b3d241a9193e50d4
Alessandro Ranellucci <aar@cpan.org>
2014-08-03 19:42:29 +0200

    Moved C++ code into new libslic3r directory

---
+++ xs/src/libslic3r/libslic3r.h
@@ -0,0 +1,26 @@
+#ifndef _libslic3r_h_
+#define _libslic3r_h_
+
+// this needs to be included early for MSVC (listing it in Build.PL is not enough)
+#include <ostream>
+#include <iostream>
+#include <sstream>
+
+#define EPSILON 1e-4
+#define SCALING_FACTOR 0.000001
+#define PI 3.141592653589793238
+#define scale_(val) (val / SCALING_FACTOR)
+#define unscale(val) (val * SCALING_FACTOR)
+#define SCALED_EPSILON scale_(EPSILON)
+typedef long coord_t;
+typedef double coordf_t;
+
+namespace Slic3r {}
+using namespace Slic3r;
+
+/* Implementation of CONFESS("foo"): */
+#define CONFESS(...) confess_at(__FILE__, __LINE__, __func__, __VA_ARGS__)
+void confess_at(const char *file, int line, const char *func, const char *pat, ...);
+/* End implementation of CONFESS("foo"): */
+
+#endif
--- xs/src/myinit.h
+++ xs/src/myinit.h
@@ -20,21 +20,6 @@ extern "C" {
 #include "perlglue.hpp"
 #endif
 
-#define EPSILON 1e-4
-#define SCALING_FACTOR 0.000001
-#define PI 3.141592653589793238
-#define scale_(val) (val / SCALING_FACTOR)
-#define unscale(val) (val * SCALING_FACTOR)
-#define SCALED_EPSILON scale_(EPSILON)
-typedef long coord_t;
-typedef double coordf_t;
-
-namespace Slic3r {}
-using namespace Slic3r;
-
-/* Implementation of CONFESS("foo"): */
-#define CONFESS(...) confess_at(__FILE__, __LINE__, __func__, __VA_ARGS__)
-void confess_at(const char *file, int line, const char *func, const char *pat, ...);
-/* End implementation of CONFESS("foo"): */
+#include "libslic3r/libslic3r.h"
 
 #endif
