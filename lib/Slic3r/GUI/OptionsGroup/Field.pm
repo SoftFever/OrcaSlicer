0a351fe47d5ef917396526e3ad95a477ec4d21cb
Alessandro Ranellucci <aar@cpan.org>
2014-11-08 14:47:05 +0100

    Fix rendering issue of slider controls on Linux

--- lib/Slic3r/GUI/OptionsGroup/Field.pm
+++ lib/Slic3r/GUI/OptionsGroup/Field.pm
@@ -419,10 +419,12 @@ sub BUILD {
     );
     $self->slider($slider);
     
-    my $statictext = Wx::StaticText->new($self->parent, -1, $slider->GetValue/$self->scale);
+    my $statictext = Wx::StaticText->new($self->parent, -1, $slider->GetValue/$self->scale,
+        wxDefaultPosition, [20,-1]);
     $self->statictext($statictext);
     
-    $sizer->Add($_, 0, wxALIGN_CENTER_VERTICAL, 0) for $slider, $statictext;
+    $sizer->Add($slider, 1, wxALIGN_CENTER_VERTICAL, 0);
+    $sizer->Add($statictext, 0, wxALIGN_CENTER_VERTICAL, 0);
     
     EVT_SLIDER($self->parent, $slider, sub {
         $self->_update_statictext;
