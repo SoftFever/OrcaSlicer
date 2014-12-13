ac2b6de62bd38e6f9dd87f26f2a0394d7b6b41c7
Alessandro Ranellucci <aar@cpan.org>
2014-12-13 20:56:22 +0100

    Minor addition to OpenGL code to prevent darker models

--- lib/Slic3r/GUI/PreviewCanvas.pm
+++ lib/Slic3r/GUI/PreviewCanvas.pm
@@ -594,6 +594,8 @@ sub Render {
             }
         }
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glFlush();
+        glFinish();
         glEnable(GL_LIGHTING);
     }
     # draw objects
