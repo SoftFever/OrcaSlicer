e61deb3673cfaf97206d7241943762bf9eababb4
Alessandro Ranellucci <aar@cpan.org>
2015-01-30 13:21:07 +0100

    Check display area before moving window to the last saved position. #2600

--- lib/Slic3r/GUI/MainFrame.pm
+++ lib/Slic3r/GUI/MainFrame.pm
@@ -47,8 +47,14 @@ sub new {
         $self->Fit;
         $self->SetMinSize([760, 490]);
         if (defined $Slic3r::GUI::Settings->{_}{main_frame_size}) {
-            $self->SetSize([ split ',', $Slic3r::GUI::Settings->{_}{main_frame_size}, 2 ]);
-            $self->Move([ split ',', $Slic3r::GUI::Settings->{_}{main_frame_pos}, 2 ]);
+            my $size = [ split ',', $Slic3r::GUI::Settings->{_}{main_frame_size}, 2 ];
+            $self->SetSize($size);
+            
+            my $display = Wx::Display->new->GetClientArea();
+            my $pos = [ split ',', $Slic3r::GUI::Settings->{_}{main_frame_pos}, 2 ];
+            if (($pos->[X] + $size->[X]/2) < $display->GetRight && ($pos->[Y] + $size->[Y]/2) < $display->GetBottom) {
+                $self->Move($pos);
+            }
             $self->Maximize(1) if $Slic3r::GUI::Settings->{_}{main_frame_maximized};
         } else {
             $self->SetSize($self->GetMinSize);
