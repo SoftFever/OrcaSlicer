ef0050662ce91d0f69d1c25fb768dfe4ef019a6f
ntfshard <ntfs.hard@gmail.com>
2015-08-06 03:28:33 +0300

    Function arguments passed by reference

--- xs/src/clipper.hpp
+++ xs/src/clipper.hpp
@@ -340,10 +340,10 @@ private:
   bool IsHole(TEdge *e);
   bool FindOwnerFromSplitRecs(OutRec &outRec, OutRec *&currOrfl);
   void FixHoleLinkage(OutRec &outrec);
-  void AddJoin(OutPt *op1, OutPt *op2, const IntPoint offPt);
+  void AddJoin(OutPt *op1, OutPt *op2, const IntPoint &offPt);
   void ClearJoins();
   void ClearGhostJoins();
-  void AddGhostJoin(OutPt *op, const IntPoint offPt);
+  void AddGhostJoin(OutPt *op, const IntPoint &offPt);
   bool JoinPoints(Join *j, OutRec* outRec1, OutRec* outRec2);
   void JoinCommonEdges();
   void DoSimplePolygons();
