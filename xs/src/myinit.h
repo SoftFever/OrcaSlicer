9a8724cdd0832e9026908b64bba760e0257c64fd
Alessandro Ranellucci <aar@cpan.org>
2015-11-06 11:51:24 +0100

    More compilation changes for Win32

--- xs/src/myinit.h
+++ xs/src/myinit.h
@@ -1,9 +1,33 @@
 #ifndef _myinit_h_
 #define _myinit_h_
 
-// this needs to be included early for MSVC (listing it in Build.PL is not enough)
+// undef some macros set by Perl which cause compilation errors on Win32
 #undef read
 #undef seekdir
+#undef bind
+#undef send
+#undef connect
+#undef wait
+#undef accept
+#undef close
+#undef open
+#undef write
+#undef socket
+#undef listen
+#undef shutdown
+#undef ioctl
+#undef getpeername
+#undef rect
+#undef setsockopt
+#undef getsockopt
+#undef getsockname
+#undef gethostname
+#undef select
+#undef socketpair
+#undef recvfrom
+#undef sendto
+
+// these need to be included early for Win32 (listing it in Build.PL is not enough)
 #include <ostream>
 #include <iostream>
 #include <sstream>
