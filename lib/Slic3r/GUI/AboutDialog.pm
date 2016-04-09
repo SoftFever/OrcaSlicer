82b07a02fa97d5398a50b59257c14a221c4f76cb
Alessandro Ranellucci <aar@cpan.org>
2016-04-09 19:11:40 +0200

    One more year

--- lib/Slic3r/GUI/AboutDialog.pm
+++ lib/Slic3r/GUI/AboutDialog.pm
@@ -47,7 +47,7 @@ sub new {
         '<html>' .
         '<body bgcolor="#ffffff" link="#808080">' .
         '<font color="#808080">' .
-        'Copyright &copy; 2011-2015 Alessandro Ranellucci. <br />' .
+        'Copyright &copy; 2011-2016 Alessandro Ranellucci. <br />' .
         '<a href="http://slic3r.org/">Slic3r</a> is licensed under the ' .
         '<a href="http://www.gnu.org/licenses/agpl-3.0.html">GNU Affero General Public License, version 3</a>.' .
         '<br /><br /><br />' .
