17d9c8c9ddd4ae8651184013f6ea5d9855c89301
bubnikv <bubnikv@gmail.com>
2016-09-13 15:59:23 +0200

    fixes for gcc: Missing include float.h

--- xs/src/libslic3r/EdgeGrid.cpp
+++ xs/src/libslic3r/EdgeGrid.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <vector>
+#include <float.h>
 
 #include <wx/image.h>
 
--- xs/src/libslic3r/GCode/PressureEqualizer.cpp
+++ xs/src/libslic3r/GCode/PressureEqualizer.cpp
@@ -1,5 +1,6 @@
 #include <memory.h>
 #include <string.h>
+#include <float.h>
 
 #include "../libslic3r.h"
 #include "../PrintConfig.hpp"
