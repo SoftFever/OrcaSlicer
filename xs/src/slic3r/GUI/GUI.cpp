3b5d1d0e25f3d9268f820aad7d822678722a3066
bubnikv <bubnikv@gmail.com>
2017-03-23 10:28:08 +0100

    Try to enforce the use of a high performance NVIDIA / ATI GPU on dual graphic card laptops.

--- xs/src/slic3r/GUI/GUI.cpp
+++ xs/src/slic3r/GUI/GUI.cpp
@@ -5,6 +5,10 @@
 #elif _WIN32
 #include <Windows.h>
 #pragma comment(lib, "user32.lib")
+extern "C" {
+__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
 #endif
 
 namespace Slic3r { namespace GUI {
