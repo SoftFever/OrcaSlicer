c61e098066de255286659c72fc214d4531400637
bubnikv <bubnikv@gmail.com>
2017-06-14 16:24:49 +0200

    Integrated the not yet accepted boost::nowide library, thanks @alexrj.

---
+++ xs/src/boost/nowide/cstdlib.hpp
@@ -0,0 +1,16 @@
+//
+//  Copyright (c) 2012 Artyom Beilis (Tonkikh)
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+#ifndef BOOST_NOWIDE_CSTDLIB_HPP_INCLUDED
+#define BOOST_NOWIDE_CSTDLIB_HPP_INCLUDED
+
+#include <boost/nowide/cenv.hpp>
+#include <boost/nowide/system.hpp>
+
+#endif
+///
+// vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
