bff7065360bfa99fc9033db5a1c21b5186ecd17c
bubnikv <bubnikv@gmail.com>
2017-11-30 16:24:48 +0100

    Fixed a bug in the support generator: There was half extrusion width gap created between the support and the support sheath. Now the support sheath will overlap with the support base by 10% of the extrusion width by default.

--- xs/src/libslic3r/SupportMaterial.cpp
+++ xs/src/libslic3r/SupportMaterial.cpp
@@ -2569,7 +2569,7 @@ void PrintObjectSupportMaterial::generate_toolpaths(
                         // TODO: use brim ordering algorithm
                         to_infill_polygons = to_polygons(to_infill);
                         // TODO: use offset2_ex()
-                        to_infill = offset_ex(to_infill, float(- flow.scaled_spacing()));
+                        to_infill = offset_ex(to_infill, float(- 0.4 * flow.scaled_spacing()));
                         extrusion_entities_append_paths(
                             support_layer.support_fills.entities, 
                             to_polylines(STDMOVE(to_infill_polygons)),
@@ -2776,7 +2776,7 @@ void PrintObjectSupportMaterial::generate_toolpaths(
                     // TODO: use brim ordering algorithm
                     Polygons to_infill_polygons = to_polygons(to_infill);
                     // TODO: use offset2_ex()
-                    to_infill = offset_ex(to_infill, - float(flow.scaled_spacing()));
+                    to_infill = offset_ex(to_infill, - 0.4 * float(flow.scaled_spacing()));
                     extrusion_entities_append_paths(
                         base_layer.extrusions, 
                         to_polylines(STDMOVE(to_infill_polygons)),
