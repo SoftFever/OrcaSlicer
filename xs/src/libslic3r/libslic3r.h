73a539780a61621c1f2c3469d2ef74c2a00c62d8
bubnikv <bubnikv@gmail.com>
2017-12-01 18:56:32 +0100

    Bumped up a version number.

--- xs/src/libslic3r/libslic3r.h
+++ xs/src/libslic3r/libslic3r.h
@@ -14,7 +14,7 @@
 #include <boost/thread.hpp>
 
 #define SLIC3R_FORK_NAME "Slic3r Prusa Edition"
-#define SLIC3R_VERSION "1.38.2"
+#define SLIC3R_VERSION "1.38.3"
 #define SLIC3R_BUILD "UNKNOWN"
 
 typedef long coord_t;
