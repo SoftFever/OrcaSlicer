ca4bd96d5d31c5515fd14b144c3b281876861c11
bubnikv <bubnikv@gmail.com>
2017-12-10 21:14:03 +0100

    Modification of Slic3r to search the icons in resources/icons after they have been moved from var

--- lib/Slic3r.pm
+++ lib/Slic3r.pm
@@ -41,8 +41,8 @@ warn "Running Slic3r under Perl 5.16 is neither supported nor recommended\n"
 use FindBin;
 
 # Let the XS module know where the GUI resources reside.
-set_var_dir(decode_path($FindBin::Bin) . "/var");
-set_resources_dir(decode_path($FindBin::Bin) . (($^O eq 'darwin') ? '/Resources' : '/resources'));
+set_resources_dir(decode_path($FindBin::Bin) . (($^O eq 'darwin') ? '../Resources' : '/resources'));
+set_var_dir(resources_dir() . "/icons");
 
 use Moo 1.003001;
 
