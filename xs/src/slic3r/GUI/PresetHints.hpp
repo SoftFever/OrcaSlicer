696d420dc8017422b4a7b5b1c3c1b34f408c887d
bubnikv <bubnikv@gmail.com>
2018-01-04 15:38:06 +0100

    New feature: Recommended object thin wall thickness hint.

--- xs/src/slic3r/GUI/PresetHints.hpp
+++ xs/src/slic3r/GUI/PresetHints.hpp
@@ -13,11 +13,16 @@ class PresetHints
 public:
     // Produce a textual description of the cooling logic of a currently active filament.
     static std::string cooling_description(const Preset &preset);
+    
     // Produce a textual description of the maximum flow achived for the current configuration
     // (the current printer, filament and print settigns).
     // This description will be useful for getting a gut feeling for the maximum volumetric
     // print speed achievable with the extruder.
     static std::string maximum_volumetric_flow_description(const PresetBundle &preset_bundle);
+
+    // Produce a textual description of a recommended thin wall thickness
+    // from the provided number of perimeters and the external / internal perimeter width.
+    static std::string recommended_thin_wall_thickness(const PresetBundle &preset_bundle);
 };
 
 } // namespace Slic3r
