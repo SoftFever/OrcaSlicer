662ea15c239e810bfe5cccc99afc882eea84ef5d
Enrico Turri <enricoturri@seznam.cz>
2018-02-20 11:33:38 +0100

    3rd attempt to fix JIRA SPE-22 (wrong file export on MAC)

--- lib/Slic3r/GUI/Plater.pm
+++ lib/Slic3r/GUI/Plater.pm
@@ -1624,12 +1624,12 @@ sub _get_export_file {
     }
     elsif ($format eq 'AMF')
     {
-    if ($^O eq 'MacOS') {
-        # It seems that MacOS does not like double extension
-        $suffix = '.amf';
-    } else {
-        $suffix = '.zip.amf';
-    }
+        if (&Wx::wxMAC) {
+            # It seems that MacOS does not like double extension
+            $suffix = '.amf';
+        } else {
+            $suffix = '.zip.amf';
+        }
         $wildcard = 'amf';
     }
     elsif ($format eq '3MF')
