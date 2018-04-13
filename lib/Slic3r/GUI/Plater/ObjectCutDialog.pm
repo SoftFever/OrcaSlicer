659eb3412b132350bafad6a9c8fd74feaa5560fd
Enrico Turri <enricoturri@seznam.cz>
2018-04-13 14:40:42 +0200

    Fixed value of z in cut dialog when object is scaled

--- lib/Slic3r/GUI/Plater/ObjectCutDialog.pm
+++ lib/Slic3r/GUI/Plater/ObjectCutDialog.pm
@@ -60,7 +60,7 @@ sub new {
         label       => 'Z',
         default     => $self->{cut_options}{z},
         min         => 0,
-        max         => $self->{model_object}->bounding_box->size->z * $self->{model_object}->instances->[0]->scaling_factor,
+        max         => $self->{model_object}->bounding_box->size->z,
         full_width  => 1,
     ));
     {
@@ -244,7 +244,7 @@ sub _update {
             $self->{canvas}->reset_objects;
             $self->{canvas}->load_object($_, undef, undef, [0]) for @objects;
             $self->{canvas}->SetCuttingPlane(
-                $self->{cut_options}{z} / $self->{model_object}->instances->[0]->scaling_factor,
+                $self->{cut_options}{z},
                 [@expolygons],
             );
             $self->{canvas}->update_volumes_colors_by_extruder($self->GetParent->{config});
