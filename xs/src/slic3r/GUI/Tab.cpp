de0d1f34f3f13701d38d9dfb83e26feca1ded63f
Lukas Matena <lukasmatena@seznam.cz>
2018-05-02 10:52:17 +0200

    Label in filament settings changed

--- xs/src/slic3r/GUI/Tab.cpp
+++ xs/src/slic3r/GUI/Tab.cpp
@@ -1311,7 +1311,7 @@ void TabFilament::build()
 		};
 		optgroup->append_line(line);
 
-        optgroup = page->new_optgroup(_(L("Toolchange behaviour")));
+        optgroup = page->new_optgroup(_(L("Toolchange parameters with single extruder MM printers")));
 		optgroup->append_single_option_line("filament_loading_speed");
         optgroup->append_single_option_line("filament_unloading_speed");
         optgroup->append_single_option_line("filament_toolchange_delay");
