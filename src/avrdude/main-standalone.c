c426c99af0d1ce3b35303f005aab7d2fafdc888e
Vojtech Kral <vojtech@kral.hk>
2019-02-26 13:30:18 +0100

    avrdude: Build avrdude-slic3r by default

--- src/avrdude/main-standalone.c
+++ src/avrdude/main-standalone.c
@@ -1,9 +1,7 @@
 #include "avrdude.h"
 
 
-static const char* SYS_CONFIG = "/etc/avrdude-slic3r.conf";
-
 int main(int argc, char *argv[])
 {
-	return avrdude_main(argc, argv, SYS_CONFIG);
+	return avrdude_main(argc, argv);
 }
