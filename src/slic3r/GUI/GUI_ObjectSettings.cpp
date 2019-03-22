b2ef3ba291e3df265a52e8e69e3b99b10178aa75
bubnikv <bubnikv@gmail.com>
2019-03-22 23:00:23 +0100

    Fixed the options override box at the side bar.

--- src/slic3r/GUI/GUI_ObjectSettings.cpp
+++ src/slic3r/GUI/GUI_ObjectSettings.cpp
@@ -40,7 +40,7 @@ void OG_Settings::Hide()
 void OG_Settings::UpdateAndShow(const bool show)
 {
     Show(show);
-//     m_parent->Layout();
+//    m_parent->Layout();
 }
 
 wxSizer* OG_Settings::get_sizer()
@@ -119,7 +119,7 @@ void ObjectSettings::update_settings_list()
                 if (cat.second.size() == 1 && cat.second[0] == "extruder")
                     continue;
 
-                auto optgroup = std::make_shared<ConfigOptionsGroup>(m_parent, cat.first, config, false, extra_column);
+                auto optgroup = std::make_shared<ConfigOptionsGroup>(m_og->ctrl_parent(), cat.first, config, false, extra_column);
                 optgroup->label_width = 15 * wxGetApp().em_unit();//150;
                 optgroup->sidetext_width = 7 * wxGetApp().em_unit();//70;
 
