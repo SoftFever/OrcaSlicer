c37cdb6585d9624163d980501d55157426910453
Stephan <stephan@forheaven.de>
2019-05-07 20:30:28 +0200

    * fix compile error and removed json includes

--- src/slic3r/Utils/FlashAir.cpp
+++ src/slic3r/Utils/FlashAir.cpp
@@ -5,8 +5,6 @@
 #include <boost/filesystem/path.hpp>
 #include <boost/format.hpp>
 #include <boost/log/trivial.hpp>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
 
 #include <wx/frame.h>
 #include <wx/event.h>
@@ -73,7 +71,7 @@ wxString FlashAir::get_test_ok_msg () const
 
 wxString FlashAir::get_test_failed_msg (wxString &msg) const
 {
-	return wxString::Format("%s: %s", _(L("Could not connect to FlashAir")), msg, _(L("Note: FlashAir with firmware 2.00.02 or newer and activated upload function is required."));
+	return wxString::Format("%s: %s", _(L("Could not connect to FlashAir")), msg, _(L("Note: FlashAir with firmware 2.00.02 or newer and activated upload function is required.")));
 }
 
 bool FlashAir::upload(PrintHostUpload upload_data, ProgressFn prorgess_fn, ErrorFn error_fn) const
