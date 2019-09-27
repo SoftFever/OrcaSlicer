debd9e2f81afebe665fe391aa99107d58550be36
bubnikv <bubnikv@gmail.com>
2019-09-27 19:07:07 +0200

    One more try, fixing a missing parameter.

--- src/libslic3r/ShortestPath.hpp
+++ src/libslic3r/ShortestPath.hpp
@@ -23,7 +23,7 @@ void                                 reorder_extrusion_paths(std::vector<Extrusi
 void                                 chain_and_reorder_extrusion_paths(std::vector<ExtrusionPath> &extrusion_paths, const Point *start_near = nullptr);
 
 Polylines 							 chain_polylines(Polylines &&src, const Point *start_near = nullptr);
-inline Polylines 					 chain_polylines(const Polylines& src, const Point* start_near = nullptr) { Polylines tmp(src); return chain_polylines(std::move(tmp)); }
+inline Polylines 					 chain_polylines(const Polylines& src, const Point* start_near = nullptr) { Polylines tmp(src); return chain_polylines(std::move(tmp), start_near); }
 
 std::vector<ClipperLib::PolyNode*>	 chain_clipper_polynodes(const Points &points, const std::vector<ClipperLib::PolyNode*> &items);
 
