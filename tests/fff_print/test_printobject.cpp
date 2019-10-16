a5eb4a4ce1ca05bd1514cf541d5f36673a052191
bubnikv <bubnikv@gmail.com>
2019-10-16 13:20:09 +0200

    Ported test_print/test_printobject/test_3mf from upstream Slic3r, thanks @lordofhyphens

---
+++ tests/fff_print/test_printobject.cpp
@@ -0,0 +1,86 @@
+#include <catch2/catch.hpp>
+
+#include "libslic3r/libslic3r.h"
+#include "libslic3r/Print.hpp"
+
+#include "test_data.hpp"
+
+using namespace Slic3r;
+using namespace Slic3r::Test;
+
+SCENARIO("PrintObject: object layer heights", "[PrintObject]") {
+    GIVEN("20mm cube and default initial config, initial layer height of 2mm") {
+        Slic3r::DynamicPrintConfig config = Slic3r::DynamicPrintConfig::full_print_config();
+        TestMesh m = TestMesh::cube_20x20x20;
+        Slic3r::Model model;
+
+        config.set_deserialize("first_layer_height", "2");
+
+        WHEN("generate_object_layers() is called for 2mm layer heights and nozzle diameter of 3mm") {
+            config.opt_float("nozzle_diameter", 0) = 3;
+			config.opt_float("layer_height") = 2.0;
+			std::shared_ptr<Slic3r::Print> print = Slic3r::Test::init_print({m}, model, config);
+			print->process();
+			const std::vector<Slic3r::Layer*> &layers = print->objects().front()->layers();
+            THEN("The output vector has 10 entries") {
+                REQUIRE(layers.size() == 10);
+            }
+            AND_THEN("Each layer is approximately 2mm above the previous Z") {
+                coordf_t last = 0.0;
+                for (size_t i = 0; i < layers.size(); ++ i) {
+                    REQUIRE((layers[i]->print_z - last) == Approx(2.0));
+                    last = layers[i]->print_z;
+                }
+            }
+        }
+        WHEN("generate_object_layers() is called for 10mm layer heights and nozzle diameter of 11mm") {
+            config.opt_float("nozzle_diameter", 0) = 11;
+			config.opt_float("layer_height") = 10;
+            std::shared_ptr<Slic3r::Print> print = Slic3r::Test::init_print({m}, model, config);
+			print->process();
+			const std::vector<Slic3r::Layer*> &layers = print->objects().front()->layers();
+			THEN("The output vector has 3 entries") {
+                REQUIRE(layers.size() == 3);
+            }
+            AND_THEN("Layer 0 is at 2mm") {
+                REQUIRE(layers.front()->print_z == Approx(2.0));
+            }
+            AND_THEN("Layer 1 is at 12mm") {
+                REQUIRE(layers[1]->print_z == Approx(12.0));
+            }
+        }
+        WHEN("generate_object_layers() is called for 15mm layer heights and nozzle diameter of 16mm") {
+            config.opt_float("nozzle_diameter", 0) = 16;
+            config.opt_float("layer_height") = 15.0;
+            std::shared_ptr<Slic3r::Print> print = Slic3r::Test::init_print({m}, model, config);
+			print->process();
+			const std::vector<Slic3r::Layer*> &layers = print->objects().front()->layers();
+			THEN("The output vector has 2 entries") {
+                REQUIRE(layers.size() == 2);
+            }
+            AND_THEN("Layer 0 is at 2mm") {
+                REQUIRE(layers[0]->print_z == Approx(2.0));
+            }
+            AND_THEN("Layer 1 is at 17mm") {
+                REQUIRE(layers[1]->print_z == Approx(17.0));
+            }
+        }
+#if 0
+        WHEN("generate_object_layers() is called for 15mm layer heights and nozzle diameter of 5mm") {
+            config.opt_float("nozzle_diameter", 0) = 5;
+            config.opt_float("layer_height") = 15.0;
+            std::shared_ptr<Slic3r::Print> print = Slic3r::Test::init_print({m}, model, config);
+			print->process();
+			const std::vector<Slic3r::Layer*> &layers = print->objects().front()->layers();
+			THEN("The layer height is limited to 5mm.") {
+                CHECK(layers.size() == 5);
+                coordf_t last = 2.0;
+                for (size_t i = 1; i < layers.size(); i++) {
+                    REQUIRE((layers[i]->print_z - last) == Approx(5.0));
+                    last = layers[i]->print_z;
+                }
+            }
+        }
+#endif
+    }
+}
---
+++ tests/libslic3r/test_3mf.cpp
@@ -0,0 +1,20 @@
+#include <catch2/catch.hpp>
+
+#include "libslic3r/Model.hpp"
+#include "libslic3r/Format/3mf.hpp"
+
+using namespace Slic3r;
+
+SCENARIO("Reading 3mf file") {
+    GIVEN("umlauts in the path of the file") {
+        Slic3r::Model model;
+        WHEN("3mf model is read") {
+        	std::string path = std::string(TEST_DATA_DIR) + "/test_3mf/Geräte/Büchse.3mf";
+        	DynamicPrintConfig config;
+            bool ret = Slic3r::load_3mf(path.c_str(), &config, &model, false);
+            THEN("load should succeed") {
+                REQUIRE(ret);
+            }
+        }
+    }
+}
