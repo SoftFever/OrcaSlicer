784728bff8ff63b5b991505016d3be54e7be28ed
tamasmeszaros <meszaros.q@gmail.com>
2019-12-10 13:57:57 +0100

    Fix missing GLEW link to OpenCSG

--- deps/OpenCSG/OpenCSG.cmake
+++ deps/OpenCSG/OpenCSG.cmake
@@ -3,4 +3,5 @@ prusaslicer_add_cmake_project(OpenCSG
     GIT_REPOSITORY https://github.com/floriankirsch/OpenCSG.git
     GIT_TAG 83e274457b46c9ad11a4ee599203250b1618f3b9 #v1.4.2
     PATCH_COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt.in ./CMakeLists.txt
+    DEPENDS dep_GLEW
 )
