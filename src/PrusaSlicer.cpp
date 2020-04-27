9fdc54bfffbcb72241a0cbaa5960ed7d15f36c0b
tamasmeszaros <meszaros.q@gmail.com>
2020-04-27 13:02:16 +0200

    Fix app crash on startup under Linux/GTK3/Wayland config

--- src/PrusaSlicer.cpp
+++ src/PrusaSlicer.cpp
@@ -57,6 +57,14 @@ using namespace Slic3r;
 
 int CLI::run(int argc, char **argv)
 {
+
+#ifdef __WXGTK__
+    // On Linux, wxGTK has no support for Wayland, and the app crashes on
+    // startup if gtk3 is used. This env var has to be set explicitly to
+    // instruct the window manager to fall back to X server mode.
+    ::setenv("GDK_BACKEND", "x11", /* replace */ true);
+#endif
+
 	// Switch boost::filesystem to utf8.
     try {
         boost::nowide::nowide_filesystem();
