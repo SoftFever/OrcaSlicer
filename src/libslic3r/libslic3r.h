c64b7b2e2169592c6ca34e09457f507d349ac485
Vojtech Bubnik <bubnikv@gmail.com>
2020-05-21 17:58:07 +0200

    Fix of the previous commit.

--- src/libslic3r/libslic3r.h
+++ src/libslic3r/libslic3r.h
@@ -50,7 +50,7 @@ static constexpr double PI = 3.141592653589793238;
 // When extruding a closed loop, the loop is interrupted and shortened a bit to reduce the seam.
 static constexpr double LOOP_CLIPPING_LENGTH_OVER_NOZZLE_DIAMETER = 0.15;
 // Maximum perimeter length for the loop to apply the small perimeter speed. 
-#define                 SMALL_PERIMETER_LENGTH  ((6.5 / SCALING_FACTOR) * 2 * PI);
+#define                 SMALL_PERIMETER_LENGTH  ((6.5 / SCALING_FACTOR) * 2 * PI)
 static constexpr double INSET_OVERLAP_TOLERANCE = 0.4;
 // 3mm ring around the top / bottom / bridging areas.
 //FIXME This is quite a lot.
