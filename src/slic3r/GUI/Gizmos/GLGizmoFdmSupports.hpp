223eb6933c602a1c01fc2c14f3092504b05858f8
Lukas Matena <lukasmatena@seznam.cz>
2020-08-18 15:18:00 +0200

    TriangleSelector paints continuously when dragging fast Previously there would be distinct circles with gaps in between

--- src/slic3r/GUI/Gizmos/GLGizmoFdmSupports.hpp
+++ src/slic3r/GUI/Gizmos/GLGizmoFdmSupports.hpp
@@ -92,6 +92,7 @@ private:
     bool m_setting_angle = false;
     bool m_internal_stack_active = false;
     bool m_schedule_update = false;
+    Vec2d m_last_mouse_position = Vec2d::Zero();
 
     // This map holds all translated description texts, so they can be easily referenced during layout calculations
     // etc. When language changes, GUI is recreated and this class constructed again, so the change takes effect.
