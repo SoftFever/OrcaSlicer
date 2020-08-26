db7559157ca6b4e06481dfbf4eb3d8f823977da7
Lukas Matena <lukasmatena@seznam.cz>
2020-08-26 13:15:15 +0200

    Revert "Forbid translation of objects when SLA/Hollow/FDM gizmos are active"
    This reverts commit c29171790930a1a9f9b0374b6a5ab8ccec1e88a9. Translation of object when those
    gizmos are active should already be supressed by previous commit (ba97ebb0). The FDM gizmo was
    erroneously not blocking the translation, the commit that is reverted is therefore needless
    after this was fixed the way it should have been fixed in the first place.

--- src/slic3r/GUI/GLCanvas3D.cpp
+++ src/slic3r/GUI/GLCanvas3D.cpp
@@ -3657,14 +3657,6 @@ void GLCanvas3D::on_mouse(wxMouseEvent& evt)
         if (!m_mouse.drag.move_requires_threshold)
         {
             m_mouse.dragging = true;
-
-            // Translation of objects is forbidden when SLA supports/hollowing/fdm
-            // supports gizmo is active.
-            if (m_gizmos.get_current_type() == GLGizmosManager::SlaSupports
-             || m_gizmos.get_current_type() == GLGizmosManager::FdmSupports
-             || m_gizmos.get_current_type() == GLGizmosManager::Hollow)
-                return;
-
             Vec3d cur_pos = m_mouse.drag.start_position_3D;
             // we do not want to translate objects if the user just clicked on an object while pressing shift to remove it from the selection and then drag
             if (m_selection.contains_volume(get_first_hover_volume_idx()))
