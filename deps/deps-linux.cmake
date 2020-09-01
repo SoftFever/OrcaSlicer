d904862bc708ee3571480cc97b84c2def389c84e
tamasmeszaros <meszaros.q@gmail.com>
2020-09-01 19:04:22 +0200

    Build libpng as part of deps on Linux
    - We've found counter case where the system provided one is missing or is too old.

--- deps/deps-linux.cmake
+++ deps/deps-linux.cmake
@@ -3,10 +3,11 @@ set(DEP_CMAKE_OPTS "-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
 
 include("deps-unix-common.cmake")
 
-find_package(PNG QUIET)
-if (NOT PNG_FOUND)
-    message(WARNING "No PNG dev package found in system, building static library. You should install the system package.")
-endif ()
+# Some Linuxes may have very old libpng, so it's best to bundle it instead of relying on the system version.
+# find_package(PNG QUIET)
+# if (NOT PNG_FOUND)
+#     message(WARNING "No PNG dev package found in system, building static library. You should install the system package.")
+# endif ()
 
 #TODO UDEV
 
