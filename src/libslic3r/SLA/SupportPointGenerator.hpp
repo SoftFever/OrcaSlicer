50836914fc24290f20c886f563df21c1fd4e99df
tamasmeszaros <meszaros.q@gmail.com>
2020-09-10 13:37:58 +0200

    Calibration changes to address new algorithm behavior.

--- src/libslic3r/SLA/SupportPointGenerator.hpp
+++ src/libslic3r/SLA/SupportPointGenerator.hpp
@@ -22,8 +22,9 @@ public:
         float density_relative {1.f};
         float minimal_distance {1.f};
         float head_diameter {0.4f};
-        ///////////////
-        inline float support_force() const { return 7.7f / density_relative; } // a force one point can support       (arbitrary force unit)
+
+        // Originally calibrated to 7.7f, reduced density by Tamas to 70% which is 11.1 (7.7 / 0.7) to adjust for new algorithm changes in tm_suppt_gen_improve
+        inline float support_force() const { return 11.1f / density_relative; } // a force one point can support       (arbitrary force unit)
         inline float tear_pressure() const { return 1.f; }  // pressure that the display exerts    (the force unit per mm2)
     };
     
