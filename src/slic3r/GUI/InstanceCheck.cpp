2f5bce3d534daa63f4dd94ec0f0efa209cc0fd69
David Kocik <kocikdav@gmail.com>
2020-10-14 09:26:06 +0200

    bug fix std::optional on mac

--- src/slic3r/GUI/InstanceCheck.cpp
+++ src/slic3r/GUI/InstanceCheck.cpp
@@ -254,7 +254,7 @@ bool instance_check(int argc, char** argv, bool app_config_single_instance)
 	GUI::wxGetApp().init_single_instance_checker(lock_name + ".lock", data_dir() + "/cache/");
 	if (cla.should_send.value() && GUI::wxGetApp().single_instance_checker()->IsAnotherRunning()) {
 #else // mac & linx
-	if (cla.should_send.value() && instance_check_internal::get_lock(lock_name + ".lock", data_dir() + "/cache/")) {
+	if (*cla.should_send && instance_check_internal::get_lock(lock_name + ".lock", data_dir() + "/cache/")) {
 #endif
 		instance_check_internal::send_message(cla.cl_string, lock_name);
 		BOOST_LOG_TRIVIAL(info) << "instance check: Another instance found. This instance will terminate.";
