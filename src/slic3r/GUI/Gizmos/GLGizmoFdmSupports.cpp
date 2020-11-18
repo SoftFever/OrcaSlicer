4cc3e939da6511c7f4625a490bbc60c27c3ea545
YuSanka <yusanka@gmail.com>
2020-11-18 17:25:05 +0100

    Localization: Update for the CZ, DE, ES, FR, IT and PL languages

--- src/slic3r/GUI/Gizmos/GLGizmoFdmSupports.cpp
+++ src/slic3r/GUI/Gizmos/GLGizmoFdmSupports.cpp
@@ -45,7 +45,7 @@ bool GLGizmoFdmSupports::on_init()
     m_desc["cursor_type"]      = _L("Brush shape") + ": ";
     m_desc["enforce_caption"]  = _L("Left mouse button") + ": ";
     m_desc["enforce"]          = _L("Enforce supports");
-    m_desc["block_caption"]    = _L("Right mouse button") + " ";
+    m_desc["block_caption"]    = _L("Right mouse button") + ": ";
     m_desc["block"]            = _L("Block supports");
     m_desc["remove_caption"]   = _L("Shift + Left mouse button") + ": ";
     m_desc["remove"]           = _L("Remove selection");
