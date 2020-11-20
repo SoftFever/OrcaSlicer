bca000a739b4cd328fdf9c92ac66ad2e9ece54eb
YuSanka <yusanka@gmail.com>
2020-11-20 17:21:37 +0100

    Font of the DoubleSliders is changes to default (#5121)

--- src/slic3r/GUI/DoubleSlider.cpp
+++ src/slic3r/GUI/DoubleSlider.cpp
@@ -137,14 +137,13 @@ Control::Control( wxWindow *parent,
     m_line_pens = { &DARK_GREY_PEN, &GREY_PEN, &LIGHT_GREY_PEN };
     m_segm_pens = { &DARK_ORANGE_PEN, &ORANGE_PEN, &LIGHT_ORANGE_PEN };
 
-    const wxFont& font = GetFont();
-    m_font = is_osx ? font.Smaller().Smaller() : font.Smaller();
+    m_font = GetFont();
+    this->SetMinSize(get_min_size());
 }
 
 void Control::msw_rescale()
 {
-    const wxFont& font = GUI::wxGetApp().normal_font();
-    m_font = is_osx ? font.Smaller().Smaller() : font.Smaller();
+    m_font = GUI::wxGetApp().normal_font();
 
     m_bmp_thumb_higher.msw_rescale();
     m_bmp_thumb_lower .msw_rescale();
@@ -182,8 +181,7 @@ int Control::GetActiveValue() const
 
 wxSize Control::get_min_size() const
 {
-    const int min_side = GUI::wxGetApp().em_unit() * ( is_horizontal() ? (is_osx ? 8 : 6) : 10 );
-
+    const int min_side = GUI::wxGetApp().em_unit() * ( is_horizontal() ? 5 : 11 );
     return wxSize(min_side, min_side);
 }
 
@@ -713,7 +711,7 @@ void Control::draw_tick_text(wxDC& dc, const wxPoint& pos, int tick, LabelType l
             text_pos = wxPoint(xx, pos.y - m_thumb_size.x / 2 - text_height - 1);
         }
         else
-            text_pos = wxPoint(pos.x - text_width - 1 - m_thumb_size.x, pos.y - 0.5 * text_height + 1);
+            text_pos = wxPoint(std::max(2, pos.x - text_width - 1 - m_thumb_size.x), pos.y - 0.5 * text_height + 1);
     }
 
     if (label_type == ltEstimatedTime)
--- src/slic3r/GUI/GUI_Preview.cpp
+++ src/slic3r/GUI/GUI_Preview.cpp
@@ -280,7 +280,7 @@ bool Preview::init(wxWindow* parent, Model* model)
     wxBoxSizer* right_sizer = new wxBoxSizer(wxVERTICAL);
     right_sizer->Add(m_layers_slider_sizer, 1, wxEXPAND, 0);
 
-    m_moves_slider = new DoubleSlider::Control(m_bottom_toolbar_panel, wxID_ANY, 0, 0, 0, 100, wxDefaultPosition, wxSize(-1, 3 * GetTextExtent("m").y), wxSL_HORIZONTAL);
+    m_moves_slider = new DoubleSlider::Control(m_bottom_toolbar_panel, wxID_ANY, 0, 0, 0, 100, wxDefaultPosition, wxDefaultSize, wxSL_HORIZONTAL);
     m_moves_slider->SetDrawMode(DoubleSlider::dmSequentialGCodeView);
 
     wxBoxSizer* bottom_toolbar_sizer = new wxBoxSizer(wxHORIZONTAL);
