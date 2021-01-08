d19a7fcfad827c9b7d6bad49e9b0afcfaa73b14c
tamasmeszaros <meszaros.q@gmail.com>
2021-01-08 10:14:24 +0100

    Increase tolerance for objects being outside print bed.

--- src/slic3r/GUI/3DScene.hpp
+++ src/slic3r/GUI/3DScene.hpp
@@ -611,7 +611,7 @@ struct _3DScene
     static void point3_to_verts(const Vec3crd& point, double width, double height, GLVolume& volume);
 };
 
-static constexpr float BedEpsilon = float(EPSILON);
+static constexpr float BedEpsilon = 3.f * float(EPSILON);
 
 }
 
