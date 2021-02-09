a5972e64fac8a081c8b9387acf0c17fd23945568
Vojtech Bubnik <bubnikv@gmail.com>
2021-02-09 20:06:08 +0100

    Fixing compiler warnings: Fixed broken Linux build.

--- src/slic3r/GUI/RemovableDriveManager.cpp
+++ src/slic3r/GUI/RemovableDriveManager.cpp
@@ -203,7 +203,7 @@ namespace search_for_drives_internal
 		}
 	}
 
-#if 0
+#if ! __APPLE__
 	static void search_path(const std::string &path, const std::string &parent_path, std::vector<DriveData> &out)
 	{
 	    glob_t globbuf;
@@ -218,7 +218,7 @@ namespace search_for_drives_internal
 		}
 		globfree(&globbuf);
 	}
-#endif
+#endif // ! __APPLE__
 }
 
 std::vector<DriveData> RemovableDriveManager::search_for_removable_drives() const
