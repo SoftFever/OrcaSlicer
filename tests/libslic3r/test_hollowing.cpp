33d6655f26d70f23007afb26345dc302da894f23
tamasmeszaros <meszaros.q@gmail.com>
2021-03-02 19:35:46 +0100

    Clean up hollowing test
    Needs rethinking anyway

--- tests/libslic3r/test_hollowing.cpp
+++ tests/libslic3r/test_hollowing.cpp
@@ -2,29 +2,7 @@
 #include <fstream>
 #include <catch2/catch.hpp>
 
-#include <libslic3r/TriangleMesh.hpp>
 #include "libslic3r/SLA/Hollowing.hpp"
-#include <openvdb/tools/Filter.h>
-#include "libslic3r/Format/OBJ.hpp"
-
-#include <libnest2d/tools/benchmark.h>
-
-#include <libslic3r/SimplifyMesh.hpp>
-
-#if defined(WIN32) || defined(_WIN32)
-#define PATH_SEPARATOR R"(\)"
-#else
-#define PATH_SEPARATOR R"(/)"
-#endif
-
-static Slic3r::TriangleMesh load_model(const std::string &obj_filename)
-{
-    Slic3r::TriangleMesh mesh;
-    auto fpath = TEST_DATA_DIR PATH_SEPARATOR + obj_filename;
-    Slic3r::load_obj(fpath.c_str(), &mesh);
-    return mesh;
-}
-
 
 TEST_CASE("Hollow two overlapping spheres") {
     using namespace Slic3r;
