aa2d8915ef9702eb3c1f33bb958cf19412ee9445
tamasmeszaros <meszaros.q@gmail.com>
2021-03-19 14:02:20 +0100

    Fix crash caused by boost log trivial on mac

--- src/libslic3r/MeshBoolean.cpp
+++ src/libslic3r/MeshBoolean.cpp
@@ -1,7 +1,6 @@
 #include "Exception.hpp"
 #include "MeshBoolean.hpp"
 #include "libslic3r/TriangleMesh.hpp"
-#include "boost/log/trivial.hpp"
 #undef PI
 
 // Include igl first. It defines "L" macro which then clashes with our localization
@@ -160,11 +159,8 @@ template<class _Mesh> TriangleMesh cgal_to_triangle_mesh(const _Mesh &cgalmesh)
             facet(i++) = v;
         }
 
-        if (i == 3) {
+        if (i == 3)
             facets.emplace_back(facet);
-        } else {
-            BOOST_LOG_TRIVIAL(error) << "CGAL face is not a triangle.";
-        }
     }
     
     TriangleMesh out{points, facets};
