ca69309fa4e66682f5049c1ab65ed2c217ab2b59
tamasmeszaros <meszaros.q@gmail.com>
2021-03-24 11:14:28 +0100

    Fix qhull compilation on mac

--- deps/Qhull/Qhull.cmake
+++ deps/Qhull/Qhull.cmake
@@ -1,7 +1,7 @@
 include(GNUInstallDirs)
 prusaslicer_add_cmake_project(Qhull
-    URL "https://github.com/qhull/qhull/archive/v8.0.0.zip"
-    URL_HASH SHA256=7edae57142989690bc432893da47db5b7069a150c82ba113e46977904ca326ef
+    URL "https://github.com/qhull/qhull/archive/v8.0.1.zip"
+    URL_HASH SHA256=5287f5edd6a0372588f5d6640799086a4033d89d19711023ef8229dd9301d69b
     CMAKE_ARGS 
         -DINCLUDE_INSTALL_DIR=${CMAKE_INSTALL_INCLUDEDIR}
 )
