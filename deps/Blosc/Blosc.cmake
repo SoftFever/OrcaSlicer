fcc4ea3802153885c7de5f60028aab8bcf772e8b
tamasmeszaros <meszaros.q@gmail.com>
2021-03-24 11:56:07 +0100

    Accept new Blosc fix

--- deps/Blosc/Blosc.cmake
+++ deps/Blosc/Blosc.cmake
@@ -10,7 +10,7 @@ prusaslicer_add_cmake_project(Blosc
     #URL https://github.com/Blosc/c-blosc/archive/refs/tags/v1.17.0.zip
     #URL_HASH SHA256=7463a1df566704f212263312717ab2c36b45d45cba6cd0dccebf91b2cc4b4da9
     URL https://github.com/tamasmeszaros/c-blosc/archive/refs/heads/v1.17.0_tm.zip
-    URL_HASH SHA256=b69a0f8a66e420b3b57e159cc56e3ced803d5de8dbe35dcebb4c54671fdc2e7a
+    URL_HASH SHA256=dcb48bf43a672fa3de6a4b1de2c4c238709dad5893d1e097b8374ad84b1fc3b3
     DEPENDS ${ZLIB_PKG}
     # Patching upstream does not work this way with git version 2.28 installed on mac worker
     # PATCH_COMMAND  ${GIT_EXECUTABLE} apply --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/blosc-mods.patch
