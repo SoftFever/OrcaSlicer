18c8003a0fc7f919e20fb404b0a6f67880443c46
enricoturri1966 <enricoturri@seznam.cz>
2021-07-14 11:39:09 +0200

    Fixes into Cut gizmo

--- src/slic3r/GUI/Gizmos/GLGizmoCut.hpp
+++ src/slic3r/GUI/Gizmos/GLGizmoCut.hpp
@@ -13,14 +13,14 @@ class GLGizmoCut : public GLGizmoBase
     static const double Margin;
     static const std::array<float, 4> GrabberColor;
 
-    mutable double m_cut_z;
-    double m_start_z;
-    mutable double m_max_z;
+    mutable double m_cut_z{ 0.0 };
+    mutable double m_max_z{ 0.0 };
+    double m_start_z{ 0.0 };
     Vec3d m_drag_pos;
     Vec3d m_drag_center;
-    bool m_keep_upper;
-    bool m_keep_lower;
-    bool m_rotate_lower;
+    bool m_keep_upper{ true };
+    bool m_keep_lower{ true };
+    bool m_rotate_lower{ false };
 
 public:
     GLGizmoCut(GLCanvas3D& parent, const std::string& icon_filename, unsigned int sprite_id);
@@ -32,7 +32,7 @@ public:
 
 protected:
     virtual bool on_init() override;
-    virtual void on_load(cereal::BinaryInputArchive& ar)  override{ ar(m_cut_z, m_keep_upper, m_keep_lower, m_rotate_lower); }
+    virtual void on_load(cereal::BinaryInputArchive& ar)  override { ar(m_cut_z, m_keep_upper, m_keep_lower, m_rotate_lower); }
     virtual void on_save(cereal::BinaryOutputArchive& ar) const override { ar(m_cut_z, m_keep_upper, m_keep_lower, m_rotate_lower); }
     virtual std::string on_get_name() const override;
     virtual void on_set_state() override;
@@ -44,9 +44,9 @@ protected:
     virtual void on_render_input_window(float x, float y, float bottom_limit) override;
 
 private:
-    void update_max_z(const Selection& selection) const;
     void perform_cut(const Selection& selection);
     double calc_projection(const Linef3& mouse_ray) const;
+    BoundingBoxf3 bounding_box() const;
 };
 
 } // namespace GUI
