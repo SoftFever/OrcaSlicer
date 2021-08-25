783c9cf2028e994608100ba598fd206ceccec93b
Vojtech Bubnik <bubnikv@gmail.com>
2021-08-25 09:34:32 +0200

    Fix of d3f11a6ab7a7c350439929e9ebf2b03ef4989477

--- src/slic3r/Utils/Http.cpp
+++ src/slic3r/Utils/Http.cpp
@@ -491,6 +491,7 @@ Http& Http::form_add_file(const std::string &name, const fs::path &path, const s
 	return *this;
 }
 
+#ifdef WIN32
 // Tells libcurl to ignore certificate revocation checks in case of missing or offline distribution points for those SSL backends where such behavior is present. 
 // This option is only supported for Schannel (the native Windows SSL library).
 Http& Http::ssl_revoke_best_effort(bool set)
@@ -500,6 +501,7 @@ Http& Http::ssl_revoke_best_effort(bool set)
 	}
 	return *this;
 }
+#endif // WIN32
 
 Http& Http::set_post_body(const fs::path &path)
 {
