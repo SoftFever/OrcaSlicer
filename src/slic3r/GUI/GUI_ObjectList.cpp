SoftFever/OrcaSlicer

commit 1eebaa45765db037c82d6a333f94cd1303d72e7c

Update info items in object list after repair by netfabb, painted data are removed and the info line stays hanging

src/slic3r/GUI/GUI_ObjectList.cpp
@@ -3953,6 +3953,7 @@ void ObjectList::fix_through_netfabb()
     wxGetApp().plater()->fix_through_netfabb(obj_idx, vol_idx);
     
     update_item_error_icon(obj_idx, vol_idx);
+    update_info_items(obj_idx);
 }
 
 void ObjectList::simplify()

commit 0efa0fc512094d134fca71b1961ef2d6d9f638aa

Fixed a crash when 'Repair by Netfabb' was called with multiple object selection, the bug was introduced with 1eebaa4 and possibly manifested in other scenarios as well.

src/slic3r/GUI/GUI_ObjectList.cpp
@@ -2561,6 +2561,9 @@ wxDataViewItem ObjectList::add_settings_item(wxDataViewItem parent_item, const D
 
 void ObjectList::update_info_items(size_t obj_idx, wxDataViewItemArray* selections/* = nullptr*/, bool added_object/* = false*/)
 {
+    if (obj_idx >= m_objects->size())
+        return;
+
     const ModelObject* model_object = (*m_objects)[obj_idx];
     wxDataViewItem item_obj = m_objects_model->GetItemById(obj_idx);
     assert(item_obj.IsOk());