dfb4ccdb13fa514c718b8f451fb71e7e3764ce58
Lukáš Hejl <hejl.lukas@gmail.com>
2021-10-07 11:51:38 +0200

    Reworked seam painting gizmo to the same layout as multi-material painting gizmo.

--- src/slic3r/GUI/Gizmos/GLGizmoSeam.cpp
+++ src/slic3r/GUI/Gizmos/GLGizmoSeam.cpp
@@ -87,11 +87,11 @@ void GLGizmoSeam::on_render_input_window(float x, float y, float bottom_limit)
                                                 m_imgui->calc_text_size(m_desc.at("reset_direction")).x)
                                            + m_imgui->scaled(1.5f);
     const float cursor_size_slider_left = m_imgui->calc_text_size(m_desc.at("cursor_size")).x + m_imgui->scaled(1.f);
-    const float cursor_type_radio_left  = m_imgui->calc_text_size(m_desc.at("cursor_type")).x + m_imgui->scaled(1.f);
-    const float cursor_type_radio_width1 = m_imgui->calc_text_size(m_desc["circle"]).x
-                                             + m_imgui->scaled(2.5f);
-    const float cursor_type_radio_width2 = m_imgui->calc_text_size(m_desc["sphere"]).x
-                                             + m_imgui->scaled(2.5f);
+
+    const float cursor_type_radio_left   = m_imgui->calc_text_size(m_desc["cursor_type"]).x + m_imgui->scaled(1.f);
+    const float cursor_type_radio_sphere = m_imgui->calc_text_size(m_desc["sphere"]).x + m_imgui->scaled(2.5f);
+    const float cursor_type_radio_circle = m_imgui->calc_text_size(m_desc["circle"]).x + m_imgui->scaled(2.5f);
+
     const float button_width = m_imgui->calc_text_size(m_desc.at("remove_all")).x + m_imgui->scaled(1.f);
     const float minimal_slider_width = m_imgui->scaled(4.f);
 
@@ -108,7 +108,7 @@ void GLGizmoSeam::on_render_input_window(float x, float y, float bottom_limit)
     float window_width  = minimal_slider_width + sliders_width;
     window_width = std::max(window_width, total_text_max);
     window_width = std::max(window_width, button_width);
-    window_width = std::max(window_width, cursor_type_radio_left + cursor_type_radio_width1 + cursor_type_radio_width2);
+    window_width = std::max(window_width, cursor_type_radio_left + cursor_type_radio_sphere + cursor_type_radio_circle);
 
     auto draw_text_with_caption = [this, &caption_max](const wxString& caption, const wxString& text) {
         static const ImVec4 ORANGE(1.0f, 0.49f, 0.22f, 1.0f);
@@ -122,29 +122,12 @@ void GLGizmoSeam::on_render_input_window(float x, float y, float bottom_limit)
 
     ImGui::Separator();
 
-    if (m_imgui->button(m_desc.at("remove_all"))) {
-        Plater::TakeSnapshot snapshot(wxGetApp().plater(), _L("Reset selection"),
-                                      UndoRedo::SnapshotType::GizmoAction);
-        ModelObject* mo = m_c->selection_info()->model_object();
-        int idx = -1;
-        for (ModelVolume* mv : mo->volumes) {
-            if (mv->is_model_part()) {
-                ++idx;
-                m_triangle_selectors[idx]->reset();
-                m_triangle_selectors[idx]->request_update_render_data();
-            }
-        }
-
-        update_model_object();
-        m_parent.set_as_dirty();
-    }
-
     const float max_tooltip_width = ImGui::GetFontSize() * 20.0f;
 
     ImGui::AlignTextToFramePadding();
     m_imgui->text(m_desc.at("cursor_size"));
-    ImGui::SameLine(cursor_size_slider_left);
-    ImGui::PushItemWidth(window_width - cursor_size_slider_left);
+    ImGui::SameLine(sliders_width);
+    ImGui::PushItemWidth(window_width - sliders_width);
     m_imgui->slider_float("##cursor_radius", &m_cursor_radius, CursorRadiusMin, CursorRadiusMax, "%.2f");
     if (ImGui::IsItemHovered()) {
         ImGui::BeginTooltip();
@@ -156,12 +139,12 @@ void GLGizmoSeam::on_render_input_window(float x, float y, float bottom_limit)
 
     ImGui::AlignTextToFramePadding();
     m_imgui->text(m_desc.at("cursor_type"));
-    ImGui::SameLine(cursor_type_radio_left + m_imgui->scaled(0.f));
-    ImGui::PushItemWidth(cursor_type_radio_width1);
 
-    bool sphere_sel = m_cursor_type == TriangleSelector::CursorType::SPHERE;
-    if (m_imgui->radio_button(m_desc["sphere"], sphere_sel))
-        sphere_sel = true;
+    float cursor_type_offset = cursor_type_radio_left + (window_width - cursor_type_radio_left - cursor_type_radio_sphere - cursor_type_radio_circle + m_imgui->scaled(0.5f)) / 2.f;
+    ImGui::SameLine(cursor_type_offset);
+    ImGui::PushItemWidth(cursor_type_radio_sphere);
+    if (m_imgui->radio_button(m_desc["sphere"], m_cursor_type == TriangleSelector::CursorType::SPHERE))
+        m_cursor_type = TriangleSelector::CursorType::SPHERE;
 
     if (ImGui::IsItemHovered()) {
         ImGui::BeginTooltip();
@@ -171,11 +154,10 @@ void GLGizmoSeam::on_render_input_window(float x, float y, float bottom_limit)
         ImGui::EndTooltip();
     }
 
-    ImGui::SameLine(cursor_type_radio_left + cursor_type_radio_width2 + m_imgui->scaled(0.f));
-    ImGui::PushItemWidth(cursor_type_radio_width2);
-
-    if (m_imgui->radio_button(m_desc["circle"], ! sphere_sel))
-        sphere_sel = false;
+    ImGui::SameLine(cursor_type_offset + cursor_type_radio_sphere);
+    ImGui::PushItemWidth(cursor_type_radio_circle);
+    if (m_imgui->radio_button(m_desc["circle"], m_cursor_type == TriangleSelector::CursorType::CIRCLE))
+        m_cursor_type = TriangleSelector::CursorType::CIRCLE;
 
     if (ImGui::IsItemHovered()) {
         ImGui::BeginTooltip();
@@ -184,12 +166,6 @@ void GLGizmoSeam::on_render_input_window(float x, float y, float bottom_limit)
         ImGui::PopTextWrapPos();
         ImGui::EndTooltip();
     }
-
-    m_cursor_type = sphere_sel
-            ? TriangleSelector::CursorType::SPHERE
-            : TriangleSelector::CursorType::CIRCLE;
-
-
 
     ImGui::Separator();
     if (m_c->object_clipper()->get_position() == 0.f) {
@@ -204,8 +180,8 @@ void GLGizmoSeam::on_render_input_window(float x, float y, float bottom_limit)
         }
     }
 
-    ImGui::SameLine(clipping_slider_left);
-    ImGui::PushItemWidth(window_width - clipping_slider_left);
+    ImGui::SameLine(sliders_width);
+    ImGui::PushItemWidth(window_width - sliders_width);
     auto clp_dist = float(m_c->object_clipper()->get_position());
     if (m_imgui->slider_float("##clp_dist", &clp_dist, 0.f, 1.f, "%.2f"))
         m_c->object_clipper()->set_position(clp_dist, true);
@@ -216,6 +192,22 @@ void GLGizmoSeam::on_render_input_window(float x, float y, float bottom_limit)
         ImGui::TextUnformatted(_L("Ctrl + Mouse wheel").ToUTF8().data());
         ImGui::PopTextWrapPos();
         ImGui::EndTooltip();
+    }
+
+    ImGui::Separator();
+    if (m_imgui->button(m_desc.at("remove_all"))) {
+        Plater::TakeSnapshot snapshot(wxGetApp().plater(), _L("Reset selection"), UndoRedo::SnapshotType::GizmoAction);
+        ModelObject         *mo  = m_c->selection_info()->model_object();
+        int                  idx = -1;
+        for (ModelVolume *mv : mo->volumes)
+            if (mv->is_model_part()) {
+                ++idx;
+                m_triangle_selectors[idx]->reset();
+                m_triangle_selectors[idx]->request_update_render_data();
+            }
+
+        update_model_object();
+        m_parent.set_as_dirty();
     }
 
     m_imgui->end();
