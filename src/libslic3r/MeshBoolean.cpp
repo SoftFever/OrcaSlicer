5e3da340aeac014d1bea6634d74e45e5fc76b639
tamasmeszaros <meszaros.q@gmail.com>
2021-10-15 15:32:14 +0200

    Fix crash with some models after hole drilling

--- src/libslic3r/MeshBoolean.cpp
+++ src/libslic3r/MeshBoolean.cpp
@@ -159,7 +159,7 @@ template<class _Mesh> TriangleMesh cgal_to_triangle_mesh(const _Mesh &cgalmesh)
         int i = 0;
         Vec3i facet;
         for (auto v : vtc) {
-            if (i > 2) { i = 0; break; }
+            if (i > 2 || v < 0 || v >= cgalmesh.vertices().size()) { i = 0; break; }
             facet(i++) = v;
         }
 
