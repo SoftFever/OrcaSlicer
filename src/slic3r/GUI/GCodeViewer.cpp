e41928514748e1513584d7c058f6134269f91d96
enricoturri1966 <enricoturri@seznam.cz>
2021-11-15 14:25:15 +0100

    #7066 - Ignore purge line in 'Volumetric Flow' display when Custom extrusion paths are set to invisible

--- src/slic3r/GUI/GCodeViewer.cpp
+++ src/slic3r/GUI/GCodeViewer.cpp
@@ -789,7 +789,8 @@ void GCodeViewer::refresh(const GCodeProcessor::Result& gcode_result, const std:
             m_extrusions.ranges.width.update_from(round_to_nearest_percent(curr.width));
             m_extrusions.ranges.fan_speed.update_from(curr.fan_speed);
             m_extrusions.ranges.temperature.update_from(curr.temperature);
-            m_extrusions.ranges.volumetric_rate.update_from(round_to_nearest_percent(curr.volumetric_rate()));
+            if (curr.extrusion_role != erCustom || is_visible(erCustom))
+                m_extrusions.ranges.volumetric_rate.update_from(round_to_nearest_percent(curr.volumetric_rate()));
             [[fallthrough]];
         }
         case EMoveType::Travel:
