488eb5128234b3b68fffd6f4059b0fc52bc0b156
YuSanka <yusanka@gmail.com>
2021-12-07 15:19:04 +0100

    Fix for InfoDialog, when Info about in-recognized loaded config values is shown. Text in this dialog wasn't marked correctly.

--- src/slic3r/GUI/MsgDialog.cpp
+++ src/slic3r/GUI/MsgDialog.cpp
@@ -136,7 +136,8 @@ static void add_msg_content(wxWindow* parent, wxBoxSizer* content_sizer, wxStrin
     int em = wxGetApp().em_unit();
 
     // if message containes the table
-    if (msg.Contains("<tr>")) {
+    bool is_marked = msg.Contains("<tr>");
+    if (is_marked) {
         int lines = msg.Freq('\n') + 1;
         int pos = 0;
         while (pos < (int)msg.Len() && pos != wxNOT_FOUND) {
@@ -154,7 +155,7 @@ static void add_msg_content(wxWindow* parent, wxBoxSizer* content_sizer, wxStrin
     }
     html->SetMinSize(page_size);
 
-    std::string msg_escaped = xml_escape(msg.ToUTF8().data());
+    std::string msg_escaped = xml_escape(msg.ToUTF8().data(), is_marked);
     boost::replace_all(msg_escaped, "\r\n", "<br>");
     boost::replace_all(msg_escaped, "\n", "<br>");
     if (monospaced_font)
