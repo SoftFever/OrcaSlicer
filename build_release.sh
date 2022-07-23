9af793adeacd21350a6bd03224827ee0e911818b
SoftFever <103989404+SoftFever@users.noreply.github.com>
2022-07-23 14:14:17 +0800

    add mac build script

---
+++ build_release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+WD=$(pwd)
+cd deps
+mkdir build
+cd build
+DEPS=$PWD/BambuStudio_dep
+cmake ../ -DDESTDIR="$DEPS" -DOPENSSL_ARCH="darwin64-arm64-cc"
+make -j
+
+cd $WD
+mkdir build
+cd build
+cmake .. -DBBL_RELEASE_TO_PUBLIC=1 -DCMAKE_PREFIX_PATH="$DEPS/usr/local" -DCMAKE_INSTALL_PREFIX="$PWD/BambuStudio_SoftFever" -DCMAKE_BUILD_TYPE=Release -DCMAKE_MACOSX_RPATH=ON -DCMAKE_INSTALL_RPATH="$DEPS/usr/local" -DCMAKE_MACOSX_BUNDLE=on
+cmake --build . --target install --config Release -j
