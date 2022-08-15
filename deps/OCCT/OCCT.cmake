13b711602c56b2c384f7eb5552d25c5897c0c159
yifan.wu <yifan.wu@bambulab.com>
2022-08-15 13:58:35 +0800

    ENH: add freetype for occt
    Signed-off-by: yifan.wu <yifan.wu@bambulab.com>
    Change-Id: I3adcf4b27deb0953e059c3e7fa9b9d49e447c57b
    (cherry picked from commit 400cff06210c62e4019ff6f7174216d2faeaa8e2)

--- deps/OCCT/OCCT.cmake
+++ deps/OCCT/OCCT.cmake
@@ -10,19 +10,21 @@ bambustudio_add_cmake_project(OCCT
     #PATCH_COMMAND ${PATCH_CMD} ${CMAKE_CURRENT_LIST_DIR}/0001-OCCT-fix.patch
     PATCH_COMMAND git apply --directory deps/build/dep_OCCT-prefix/src/dep_OCCT --verbose --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/0001-OCCT-fix.patch
     #DEPENDS dep_Boost
+    #DEPENDS dep_FREETYPE
     CMAKE_ARGS
         -DBUILD_LIBRARY_TYPE=${library_build_type}
         -DUSE_TK=OFF
         -DUSE_TBB=OFF
-        -DUSE_FREETYPE=OFF
-		-DUSE_FFMPEG=OFF
-		-DUSE_VTK=OFF
-		-DUSE_FREETYPE=OFF
-		-DBUILD_MODULE_ApplicationFramework=OFF
-		#-DBUILD_MODULE_DataExchange=OFF
+	#-DUSE_FREETYPE=OFF
+        -DUSE_FFMPEG=OFF
+        -DUSE_VTK=OFF
+        -DBUILD_MODULE_ApplicationFramework=OFF
+        #-DBUILD_MODULE_DataExchange=OFF
         -DBUILD_MODULE_Draw=OFF
-		-DBUILD_MODULE_FoundationClasses=OFF
-		-DBUILD_MODULE_ModelingAlgorithms=OFF
-		-DBUILD_MODULE_ModelingData=OFF
-		-DBUILD_MODULE_Visualization=OFF
+        -DBUILD_MODULE_FoundationClasses=OFF
+        -DBUILD_MODULE_ModelingAlgorithms=OFF
+        -DBUILD_MODULE_ModelingData=OFF
+        -DBUILD_MODULE_Visualization=OFF
 )
+
+add_dependencies(dep_OCCT dep_FREETYPE)
