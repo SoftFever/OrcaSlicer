65ba9cafb306881927c923b075d064aee3fec618
Stone Li <stone.li@bambulab.com>
2022-08-16 14:34:18 +0800

    ENH: add more test cases in NetworkTestDialog
    Change-Id: I54f5a52d2bb993f3536c4ab75cb54190eafe49a5
    Signed-off-by: Stone Li <stone.li@bambulab.com>
    (cherry picked from commit b28b0d97ab51b0f2f9a6f37622e5cd93e8d8d8f6)

--- src/slic3r/GUI/NetworkTestDialog.hpp
+++ src/slic3r/GUI/NetworkTestDialog.hpp
@@ -40,13 +40,15 @@ enum TestJob {
 	TEST_OSS_UPGRADE_JOB = 4,
 	TEST_OSS_DOWNLOAD_JOB = 5,
 	TEST_OSS_UPLOAD_JOB = 6,
-	TEST_JOB_MAX = 7
+	TEST_PING_JOB = 7,
+	TEST_JOB_MAX = 8
 };
 
 class NetworkTestDialog : public DPIDialog
 {
 protected:
 	wxButton* btn_start;
+	wxButton* btn_start_sequence;
 	wxButton* btn_download_log;
 	wxStaticText* text_basic_info;
 	wxStaticText* text_version_title;
@@ -55,20 +57,29 @@ protected:
 	wxStaticText* txt_sys_info_value;
 	wxStaticText* txt_dns_info_title;
 	wxStaticText* txt_dns_info_value;
+	wxButton*     btn_link;
 	wxStaticText* text_link_title;
 	wxStaticText* text_link_val;
+	wxButton*     btn_bing;
 	wxStaticText* text_bing_title;
 	wxStaticText* text_bing_val;
+	wxButton*     btn_iot;
 	wxStaticText* text_iot_title;
 	wxStaticText* text_iot_value;
+	wxButton*     btn_oss;
 	wxStaticText* text_oss_title;
 	wxStaticText* text_oss_value;
+	wxButton*     btn_oss_upgrade;
 	wxStaticText* text_oss_upgrade_title;
 	wxStaticText* text_oss_upgrade_value;
+	wxButton*     btn_oss_download;
 	wxStaticText* text_oss_download_title;
 	wxStaticText* text_oss_download_value;
+	wxButton*     btn_oss_upload;
 	wxStaticText* text_oss_upload_title;
 	wxStaticText* text_oss_upload_value;
+	wxStaticText* text_ping_title;
+	wxStaticText* text_ping_value;
 	wxStaticText* text_result;
 	wxTextCtrl* txt_log;
 
@@ -78,8 +89,10 @@ protected:
 	wxBoxSizer* create_result_sizer(wxWindow* parent);
 
 	boost::thread* test_job[TEST_JOB_MAX];
+	boost::thread* m_sequence_job { nullptr };
 	bool		   m_in_testing[TEST_JOB_MAX];
 	bool           m_download_cancel = false;
+	bool           m_closing = false;
 
 	void init_bind();
 
@@ -96,6 +109,7 @@ public:
 	wxString get_dns_info();
 
 	void start_all_job();
+	void start_all_job_sequence();
 	void start_test_bing_thread();
 	void start_test_bambulab_thread();
 	void start_test_iot_thread();
@@ -103,6 +117,15 @@ public:
 	void start_test_oss_upgrade_thread();
 	void start_test_oss_download_thread();
 	void start_test_oss_upload_thread();
+	void start_test_ping_thread();
+
+	void start_test_bing();
+	void start_test_bambulab();
+	void start_test_iot();
+	void start_test_oss();
+	void start_test_oss_upgrade();
+	void start_test_oss_download();
+	void start_test_oss_upload();
 
 	void on_close(wxCloseEvent& event);
 
