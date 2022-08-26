8319ef604c22f12a7efc663d4d2ab50e94d49a11
zhimin.zeng <zhimin.zeng@bambulab.com>
2022-08-26 11:07:14 +0800

    FIX: report an error when slice in painting
    Change-Id: I55ba3687e3988ae96d33833c03efd66ea3f607d8

--- src/slic3r/GUI/MainFrame.cpp
+++ src/slic3r/GUI/MainFrame.cpp
@@ -1224,6 +1224,7 @@ wxBoxSizer* MainFrame::create_side_tools()
     m_slice_btn->Bind(wxEVT_BUTTON, [this](wxCommandEvent& event)
     {
         //this->m_plater->select_view_3D("Preview");
+        m_plater->update();
         if (m_slice_select == eSliceAll)
             wxPostEvent(m_plater, SimpleEvent(EVT_GLTOOLBAR_SLICE_ALL));
         else
