1c7b1830356b3a6b02e6cc7d1bf5cfe0ac9f045a
tao wang <tao.wang@bambulab.com>
2022-08-31 11:15:46 +0800

    FIX:add protection when slice information is not available
    Change-Id: Iaf654c8a14252f6647a64ded97d4ded3e79d7057

--- src/slic3r/GUI/StatusPanel.cpp
+++ src/slic3r/GUI/StatusPanel.cpp
@@ -1753,6 +1753,7 @@ void StatusPanel::update_basic_print_data(bool def)
 {
     if (def) {
         if (!obj) return;
+        if (!obj->slice_info) return;
         wxString prediction = wxString::Format("%s", get_bbl_time_dhms(obj->slice_info->prediction));
         wxString weight = wxString::Format("%.2fg", obj->slice_info->weight);
 
