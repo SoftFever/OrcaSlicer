7874704aa8a7ded641cde42d6d7c922fe614df1a
SoftFever <103989404+SoftFever@users.noreply.github.com>
2022-09-18 15:08:27 +0800

    update build_release.sh

--- build_release.sh
+++ build_release.sh
@@ -6,10 +6,10 @@ mkdir build
 cd build
 DEPS=$PWD/BambuStudio_dep
 cmake ../ -DDESTDIR="$DEPS" -DOPENSSL_ARCH="darwin64-arm64-cc"
-# make -j
+cmake --build . --config Release --target ALL_BUILD -- -m
 
 cd $WD
 mkdir build
 cd build
 cmake .. -DBBL_RELEASE_TO_PUBLIC=1 -DCMAKE_PREFIX_PATH="$DEPS/usr/local" -DCMAKE_INSTALL_PREFIX="$PWD/BambuStudio-SoftFever" -DCMAKE_BUILD_TYPE=Release -DCMAKE_MACOSX_RPATH=ON -DCMAKE_INSTALL_RPATH="$DEPS/usr/local" -DCMAKE_MACOSX_BUNDLE=on
-cmake --build . --target install --config Release -j
+cmake --build . --target install --config Release -- -m
