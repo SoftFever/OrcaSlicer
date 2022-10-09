457c7ec556cf832bd439dce2480ce6ca4f7dc3a1
SoftFever <103989404+SoftFever@users.noreply.github.com>
2022-10-09 21:55:54 +0800

    fix mac osx build script when ninja is installed

--- build_release_macos.sh
+++ build_release_macos.sh
@@ -13,5 +13,5 @@ cd $WD
 mkdir -p build
 cd build
 cmake .. -DBBL_RELEASE_TO_PUBLIC=0 -DCMAKE_PREFIX_PATH="$DEPS/usr/local" -DCMAKE_INSTALL_PREFIX="$PWD/BambuStudio-SoftFever" -DCMAKE_BUILD_TYPE=Release -DCMAKE_MACOSX_RPATH=ON -DCMAKE_INSTALL_RPATH="$DEPS/usr/local" -DCMAKE_MACOSX_BUNDLE=ON
-make -j10
+cmake --build . --config Release --target all
 cmake --build . --target install --config Release -j10
