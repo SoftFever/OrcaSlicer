f331d5998e7c47e3c34eac3b00617cb0cf9091c2
Stone Li <stone.li@bambulab.com>
2022-10-13 15:20:16 +0800

    ENH: add media file and upgrade panel to public
    Change-Id: I9fdc16fcc584bf72f85a02668a42c05af0065efc

--- src/slic3r/GUI/Monitor.cpp
+++ src/slic3r/GUI/Monitor.cpp
@@ -201,13 +201,11 @@ MonitorPanel::~MonitorPanel()
     m_status_info_panel        = new StatusPanel(m_tabpanel);
     m_tabpanel->AddPage(m_status_info_panel, _L("Status"), "", true);
 
-#if !BBL_RELEASE_TO_PUBLIC
     m_media_file_panel = new MediaFilePanel(m_tabpanel);
     m_tabpanel->AddPage(m_media_file_panel, _L("Media"), "", false);
 
     m_upgrade_panel = new UpgradePanel(m_tabpanel);
     m_tabpanel->AddPage(m_upgrade_panel, _L("Update"), "", false);
-#endif
 
     m_hms_panel = new HMSPanel(m_tabpanel);
     m_tabpanel->AddPage(m_hms_panel, _L("HMS"),"", false);
@@ -254,10 +252,8 @@ void MonitorPanel::msw_rescale()
     m_tabpanel->Rescale();
     //m_status_add_machine_panel->msw_rescale();
     m_status_info_panel->msw_rescale();
-#if !BBL_RELEASE_TO_PUBLIC
     m_media_file_panel->Rescale();
     m_upgrade_panel->msw_rescale();
-#endif
     m_hms_panel->msw_rescale();
 
     m_connection_info->SetCornerRadius(0);
@@ -408,15 +404,11 @@ void MonitorPanel::update_all()
     }
 
     m_status_info_panel->obj = obj;
-#if !BBL_RELEASE_TO_PUBLIC
     m_upgrade_panel->update(obj);
-#endif
 
     
     m_status_info_panel->m_media_play_ctrl->SetMachineObject(obj);
-#if !BBL_RELEASE_TO_PUBLIC
     m_media_file_panel->SetMachineObject(obj);
-#endif
 
     update_status(obj);
     
