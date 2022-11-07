6be79946f4bc0049ede1e67ccecaa4403ca55515
lane.wei <lane.wei@bambulab.com>
2022-11-07 08:52:45 +0800

    FIX: fix the appconfog:save exception under virtualbox of linux
    Change-Id: Iadf682727d8ac2ac8694b03e999790866120bf10

--- src/libslic3r/AppConfig.cpp
+++ src/libslic3r/AppConfig.cpp
@@ -537,7 +537,7 @@ void AppConfig::save()
     {
         // Returns "undefined" if the thread naming functionality is not supported by the operating system.
         std::optional<std::string> current_thread_name = get_current_thread_name();
-        if (current_thread_name && *current_thread_name != "bambustu_main") {
+        if (current_thread_name && *current_thread_name != "bambustu_main" && *current_thread_name != "main") {
             BOOST_LOG_TRIVIAL(error) << __FUNCTION__<<", current_thread_name is " << *current_thread_name;
             throw CriticalException("Calling AppConfig::save() from a worker thread, thread name: " + *current_thread_name);
         }
