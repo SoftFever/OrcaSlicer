dcb9862410b827c115f71c8c30e43531c6b3157f
chunmao.guo <chunmao.guo@bambulab.com>
2022-12-05 13:45:34 +0800

    FIX: [STUDIO-1570] webview background color
    Change-Id: Icdf41659f4ab18ce30c8b0f585bf702991531599

--- src/slic3r/GUI/Widgets/WebView.cpp
+++ src/slic3r/GUI/Widgets/WebView.cpp
@@ -106,6 +106,7 @@ wxWebView* WebView::CreateWebView(wxWindow * parent, wxString const & url)
 
     auto webView = wxWebView::New();
     if (webView) {
+        webView->SetBackgroundColour(StateColor::darkModeColorFor(*wxWHITE));
 #ifdef __WIN32__
         webView->SetUserAgent(wxString::Format("BBL-Slicer/v%s (%s) Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
             "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52", SLIC3R_VERSION, 
