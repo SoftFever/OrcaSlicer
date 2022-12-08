51e87881675034c75a9830678519d04536ef768d
deftdawg <deftdawg@gmail.com>
2022-12-08 18:35:48 -0500

    Add check_available_memory_and_disk to BuildLinux.sh

--- BuildLinux.sh
+++ BuildLinux.sh
@@ -7,6 +7,26 @@ export CMAKE_BUILD_PARALLEL_LEVEL=${NCORES}
 FOUND_GTK2=$(dpkg -l libgtk* | grep gtk2)
 FOUND_GTK3=$(dpkg -l libgtk* | grep gtk-3)
 
+function check_available_memory_and_disk() {
+    FREE_MEM_GB=$(free -g -t | grep 'Mem:' | rev | cut -d" " -f1 | rev)
+    MIN_MEM_GB=10
+
+    FREE_DISK_KB=$(df -k . | tail -1 | awk '{print $4}')
+    MIN_DISK_KB=$((10 * 1024 * 1024))
+
+    if [ ${FREE_MEM_GB} -le ${MIN_MEM_GB} ]; then
+        echo -e "\nERROR: Bambu Studio Builder requires at least ${MIN_MEM_GB}G of 'available' mem (systen has only ${FREE_MEM_GB}G available)"
+        echo && free -h && echo
+        exit 2
+    fi
+
+    if [[ ${FREE_DISK_KB} -le ${MIN_DISK_KB} ]]; then 
+        echo -e "\nERROR: Bambu Studio Builder requires at least $(echo $MIN_DISK_KB |awk '{ printf "%.1fG\n", $1/1024/1024; }') (systen has only $(echo ${FREE_DISK_KB} | awk '{ printf "%.1fG\n", $1/1024/1024; }') disk free)"
+        echo && df -h . && echo
+        exit 1
+    fi
+}
+
 unset name
 while getopts ":dsiuhgb" opt; do
   case ${opt} in
@@ -132,6 +152,8 @@ if [ ! -d "deps/build" ]
 then
     mkdir deps/build
 fi
+
+check_available_memory_and_disk
 
 if [[ -n "$BUILD_DEPS" ]]
 then
