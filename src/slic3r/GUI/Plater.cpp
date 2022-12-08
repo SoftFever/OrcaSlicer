4ea3d16fcc9959a0f12f41b8c8b9d1abe2eea96a
tao.jin <tao.jin@bambulab.com>
2022-12-08 10:38:15 +0800

    ENH: hint of lacking model repair on mac
    Change-Id: Ie6bb669c642e4fbdafd29218bc8af6b55b1adc36

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -10944,6 +10944,11 @@ void Plater::show_object_info()
     auto mesh_errors = p->sidebar->obj_list()->get_mesh_errors_info(&info_manifold, &non_manifold_edges);
     info_text += into_u8(info_manifold);
 
+    #ifdef __APPLE__
+    if (non_manifold_edges > 0) {
+        info_text += into_u8("\n" + _L("Tips:") + "\n" +_L("\"Fix Model\" feature is currently only on Windows. Please repair the model on Bambu Studio(windows) or CAD softwares."));
+    }
+    #endif //APPLE
     notify_manager->bbl_show_objectsinfo_notification(info_text, is_windows10()&&(non_manifold_edges > 0), !(p->current_panel == p->view3D));
 }
 
