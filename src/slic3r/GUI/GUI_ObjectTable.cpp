8ccf3cbd7c183a06ea021c53669a8ef9a55e4fb4
tao wang <tao.wang@bambulab.com>
2022-12-16 09:45:10 +0800

    FIX:fixed can not change filaments on objecttable
    Change-Id: Ic4a301a71af1406f02075edaf9ef24a273c82105

--- src/slic3r/GUI/GUI_ObjectTable.cpp
+++ src/slic3r/GUI/GUI_ObjectTable.cpp
@@ -171,6 +171,16 @@ void GridCellFilamentsEditor::Create(wxWindow* parent,
     }
     m_control = bitmap_combo;
     wxGridCellEditor::Create(parent, id, evtHandler);
+
+    /* bitmap_combo->GetDropDown().Bind(wxEVT_COMBOBOX, [this](wxCommandEvent& e) {
+         const wxString value = Combo()->GetValue();
+         if (value == m_value)
+             return false;
+
+         m_value = value;
+         return true;
+         e.Skip();
+     });*/
 }
 
 void GridCellFilamentsEditor::SetSize(const wxRect& rect)
@@ -242,7 +252,7 @@ void GridCellFilamentsEditor::BeginEdit(int row, int col, wxGrid* grid)
         // When dropping down the menu, a kill focus event
         // happens after this point, so we can't reset the flag yet.
 #if !defined(__WXGTK20__)
-        evtHandler->SetInSetFocus(false);
+        //evtHandler->SetInSetFocus(false);
 #endif
     }
 }
