04008162305a876137d1581ea8a08cdfe35a6733
SoftFever <softfeverever@gmail.com>
2023-01-08 22:02:46 +0800

    win cicd

--- build_release.bat
+++ build_release.bat
@@ -3,12 +3,21 @@ cd deps
 mkdir build
 cd build
 set DEPS=%CD%/BambuStudio_dep
-@REM cmake ../ -G "Visual Studio 16 2019" -DDESTDIR="%CD%/BambuStudio_dep" -DCMAKE_BUILD_TYPE=Release
-@REM cmake --build . --config Release --target ALL_BUILD -- -m
+if "%1"=="studio" (
+    GOTO :studio
+)
+echo "building deps.."
+cmake ../ -G "Visual Studio 16 2019" -DDESTDIR="%CD%/BambuStudio_dep" -DCMAKE_BUILD_TYPE=Release
+cmake --build . --config Release --target ALL_BUILD -- -m
 
+if "%1"=="deps" exit /b 0
+
+:studio
+echo "building studio..."
 cd %WP%
 mkdir build 
 cd build
+
 cmake .. -G "Visual Studio 16 2019" -DBBL_RELEASE_TO_PUBLIC=1 -DCMAKE_PREFIX_PATH="%DEPS%/usr/local" -DCMAKE_INSTALL_PREFIX="./BambuStudio-SoftFever" -DCMAKE_BUILD_TYPE=Release -DWIN10SDK_PATH="C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0"
-@REM cmake --build . --config Release --target ALL_BUILD -- -m
-@REM cmake --build . --target install --config Release
+cmake --build . --config Release --target ALL_BUILD -- -m
+cmake --build . --target install --config Release
