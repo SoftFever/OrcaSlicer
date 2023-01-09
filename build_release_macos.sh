cf558434e25101e1b83d5984210cba8d0d4c63ef
SoftFever <softfeverever@gmail.com>
2023-01-09 17:56:13 +0800

    44

--- build_release_macos.sh
+++ build_release_macos.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
+
 WD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $WD/deps
 mkdir -p build
 cd build
 DEPS=$PWD/BambuStudio_dep
 mkdir -p $DEPS
-cmake ../ -DDESTDIR="$DEPS" -DOPENSSL_ARCH="darwin64-$(uname -m)-cc" -DCMAKE_BUILD_TYPE=Release
-cmake --build . --config Release --target all 
+if [ "studio." != $1. ]; 
+then
+    echo "building deps..."
+    cmake ../ -DDESTDIR="$DEPS" -DOPENSSL_ARCH="darwin64-$(uname -m)-cc" -DCMAKE_BUILD_TYPE=Release
+    cmake --build . --config Release --target all 
+fi
+
+
+if [ "deps." == "$1". ];
+then
+    exit 0
+fi
 
 cd $WD
 mkdir -p build
 cd build
+echo "building studio..."
 cmake .. -GXcode -DBBL_RELEASE_TO_PUBLIC=1 -DCMAKE_PREFIX_PATH="$DEPS/usr/local" -DCMAKE_INSTALL_PREFIX="$PWD/BambuStudio-SoftFever" -DCMAKE_BUILD_TYPE=Release -DCMAKE_MACOSX_RPATH=ON -DCMAKE_INSTALL_RPATH="$DEPS/usr/local" -DCMAKE_MACOSX_BUNDLE=ON
 cmake --build . --config Release --target ALL_BUILD 
 mkdir -p BambuStudio-SoftFever
