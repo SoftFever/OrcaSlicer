96c861f906d9a04557ade8b28b6d5832b3219774
SoftFever <softfeverever@gmail.com>
2023-01-12 22:55:50 +0800

    CICD (#167)
    CICD

--- build_release_macos.sh
+++ build_release_macos.sh
@@ -1,17 +1,66 @@
 #!/bin/sh
+
+while getopts ":a:sdhn" opt; do
+  case ${opt} in
+    d )
+        export BUILD_TARGET="deps"
+        ;;
+    a )
+        export ARCH="$OPTARG"
+        ;;
+    s )
+        export BUILD_TARGET="studio"
+        ;;
+    n )
+        export NIGHTLY_BUILD="1"
+        ;;
+    h ) echo "Usage: ./build_release_macos.sh [-d]"
+        echo "   -d: Build deps only"
+        echo "   -a: Set ARCHITECTURE (arm64 or x86_64)"
+        echo "   -s: Build studio only"
+        echo "   -n: Nightly build"
+        exit 0
+        ;;
+  esac
+done
+
+if [ -z "$ARCH" ]
+then
+  export ARCH=$(uname -m)
+fi
+
+echo "Arch: $ARCH"
+echo "BUILD_TARGET: $BUILD_TARGET"
+
+brew --prefix libiconv
+brew --prefix zstd
+export LIBRARY_PATH=$LIBRARY_PATH:$(brew --prefix zstd)/lib/
+
 WD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $WD/deps
 mkdir -p build
 cd build
 DEPS=$PWD/BambuStudio_dep
 mkdir -p $DEPS
-cmake ../ -DDESTDIR="$DEPS" -DOPENSSL_ARCH="darwin64-$(uname -m)-cc" -DCMAKE_BUILD_TYPE=Release
-cmake --build . --config Release --target all 
+if [ "studio." != $BUILD_TARGET. ]; 
+then
+    echo "building deps..."
+    echo "cmake ../ -DDESTDIR=$DEPS -DOPENSSL_ARCH=darwin64-${ARCH}-cc -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES:STRING=${ARCH}"
+    cmake ../ -DDESTDIR="$DEPS" -DOPENSSL_ARCH="darwin64-${ARCH}-cc" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES:STRING=${ARCH}
+    cmake --build . --config Release --target all 
+fi
+
+
+if [ "deps." == "$BUILD_TARGET". ];
+then
+    exit 0
+fi
 
 cd $WD
 mkdir -p build
 cd build
-cmake .. -GXcode -DBBL_RELEASE_TO_PUBLIC=1 -DCMAKE_PREFIX_PATH="$DEPS/usr/local" -DCMAKE_INSTALL_PREFIX="$PWD/BambuStudio-SoftFever" -DCMAKE_BUILD_TYPE=Release -DCMAKE_MACOSX_RPATH=ON -DCMAKE_INSTALL_RPATH="$DEPS/usr/local" -DCMAKE_MACOSX_BUNDLE=ON
+echo "building studio..."
+cmake .. -GXcode -DBBL_RELEASE_TO_PUBLIC=1 -DCMAKE_PREFIX_PATH="$DEPS/usr/local" -DCMAKE_INSTALL_PREFIX="$PWD/BambuStudio-SoftFever" -DCMAKE_BUILD_TYPE=Release -DCMAKE_MACOSX_RPATH=ON -DCMAKE_INSTALL_RPATH="$DEPS/usr/local" -DCMAKE_MACOSX_BUNDLE=ON -DCMAKE_OSX_ARCHITECTURES=${ARCH}
 cmake --build . --config Release --target ALL_BUILD 
 mkdir -p BambuStudio-SoftFever
 cd BambuStudio-SoftFever
@@ -21,7 +70,14 @@ resources_path=$(readlink ./BambuStudio-SoftFever.app/Contents/Resources)
 rm ./BambuStudio-SoftFever.app/Contents/Resources
 cp -R $resources_path ./BambuStudio-SoftFever.app/Contents/Resources
 # extract version
-ver=$(grep '^#define SoftFever_VERSION' ../src/libslic3r/libslic3r_version.h | cut -d ' ' -f3)
-ver="${ver//\"}"
-zip -FSr BambuStudio-SoftFever_V${ver}_Mac_$(uname -m).zip BambuStudio-SoftFever.app
+export ver="_dev"
+echo $PWD
+if [ "1." != "$NIGHTLY_BUILD". ];
+then
+    ver=$(grep '^#define SoftFever_VERSION' ../src/libslic3r/libslic3r_version.h | cut -d ' ' -f3)
+    ver="_V${ver//\"}"
+fi
+
+
+zip -FSr BambuStudio-SoftFever${ver}_Mac_${ARCH}.zip BambuStudio-SoftFever.app
 
--- deps/OpenEXR/OpenEXR.cmake
+++ deps/OpenEXR/OpenEXR.cmake
@@ -1,3 +1,34 @@
+# Check if we're building for arm on x86_64 and just for OpenEXR, build fat
+# binaries.  We need this because it compiles some code to generate other
+# source and we need to be able to run the executables.  When we link the
+# library, the x86_64 part will be ignored.
+if (APPLE AND IS_CROSS_COMPILE)
+    if (${CMAKE_SYSTEM_PROCESSOR} MATCHES "x86_64" AND ${CMAKE_OSX_ARCHITECTURES} MATCHES "arm")
+        set(_openexr_arch arm64^^x86_64)
+        set(_openxr_list_sep LIST_SEPARATOR ^^)
+        set(_cmake_openexr_arch -DCMAKE_OSX_ARCHITECTURES:STRING=${_openexr_arch})
+    else()
+        set(_openexr_arch ${CMAKE_OSX_ARCHITECTURES})
+        set(_cmake_openexr_arch -DCMAKE_OSX_ARCHITECTURES:STRING=${_openexr_arch})
+    endif()
+    ExternalProject_Add(dep_OpenEXR
+        EXCLUDE_FROM_ALL    ON
+        URL https://github.com/AcademySoftwareFoundation/openexr/archive/refs/tags/v2.5.5.zip
+        URL_HASH SHA256=0307a3d7e1fa1e77e9d84d7e9a8694583fbbbfd50bdc6884e2c96b8ef6b902de
+        INSTALL_DIR         ${DESTDIR}/usr/local
+        DOWNLOAD_DIR        ${DEP_DOWNLOAD_DIR}/OpenEXR
+        ${_openxr_list_sep}
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX:STRING=${DESTDIR}/usr/local
+            -DBUILD_SHARED_LIBS:BOOL=OFF
+            -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+            -DBUILD_TESTING=OFF 
+            -DPYILMBASE_ENABLE:BOOL=OFF 
+            -DOPENEXR_VIEWERS_ENABLE:BOOL=OFF
+            -DOPENEXR_BUILD_UTILS:BOOL=OFF
+            ${_cmake_openexr_arch}
+    )
+else()
 bambustudio_add_cmake_project(OpenEXR
     # GIT_REPOSITORY https://github.com/openexr/openexr.git
     URL https://github.com/AcademySoftwareFoundation/openexr/archive/refs/tags/v2.5.5.zip
@@ -11,6 +42,7 @@ bambustudio_add_cmake_project(OpenEXR
         -DOPENEXR_VIEWERS_ENABLE:BOOL=OFF
         -DOPENEXR_BUILD_UTILS:BOOL=OFF
 )
+endif()
 
 if (MSVC)
     add_debug_dep(dep_OpenEXR)
