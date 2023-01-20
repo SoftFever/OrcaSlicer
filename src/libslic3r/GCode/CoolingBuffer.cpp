7f1fe0bd544077626080aa1a9a0576aa735da1a4
Patrice Côté <pat@patricecote.com>
2023-01-20 07:16:45 -0500

    Enable absolute/relative extrusion option on non BBL printers
    Add the option to use non-relative extrusion in printer config screen. Some extruders do not play well with relative extrusion...
    
    Remove reference to multi-extruder
    
    Remove reference to multi-extruder
    
    change g-code comment for M83 relative extrusion
    
    changed comment for M83
    
    Remove commented line of code
    
    In this specific case, we delete the old line of code because it refers to a variable that was deleted from the rest of the code (RELATIVE_E_AXIS)

--- src/libslic3r/GCode/CoolingBuffer.cpp
+++ src/libslic3r/GCode/CoolingBuffer.cpp
@@ -401,7 +401,7 @@ std::vector<PerExtruderAdjustments> CoolingBuffer::parse_layer_gcode(const std::
             }
             if ((line.type & CoolingLine::TYPE_G92) == 0) {
                 //BBS: G0, G1, G2, G3. Calculate the duration.
-                if (RELATIVE_E_AXIS)
+                if (m_config.use_relative_e_distances.value)
                     // Reset extruder accumulator.
                     current_pos[3] = 0.f;
                 float dif[4];
--- src/libslic3r/libslic3r.h
+++ src/libslic3r/libslic3r.h
@@ -78,10 +78,6 @@ static constexpr double BRIDGE_INFILL_MARGIN = 1;
 #define scale_(val) ((val) / SCALING_FACTOR)
 #define unscale_(val) ((val) * SCALING_FACTOR)
 
-//BBS: BBS only support relative E and can't been changed by user at the moment. because
-//BBS need to support skip object when printing.
-static constexpr bool RELATIVE_E_AXIS = 1;
-
 #define SCALED_EPSILON scale_(EPSILON)
 
 #ifndef UNUSED
