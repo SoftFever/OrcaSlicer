1d92895456e8888d155afd82dfc38ae43c89b4a6
MusangKing <xiangyu.wang@bambulab.com>
2023-02-16 18:16:02 +0800

    FIX: withdraw support enforcers on vertical faces
    JIRA: studio-2274
    
    Now support enforcer on vertical faces function is invalid. Previous
    enhancement of support enforcer on vertical faces brounght more conficts
    than benefits.
    
    Change-Id: Icbb0df3995d8ac8b3c22dda1bd4615da78af963b
    (cherry picked from commit bca003cbc35791838484afe4032276be8fa91db0)

--- src/libslic3r/PrintObject.cpp
+++ src/libslic3r/PrintObject.cpp
@@ -2841,10 +2841,6 @@ static void project_triangles_to_slabs(ConstLayerPtrsAdaptor layers, const index
 void PrintObject::project_and_append_custom_facets(
         bool seam, EnforcerBlockerType type, std::vector<Polygons>& out) const
 {
-    // BBS: Approve adding enforcer support on vertical faces
-    SlabSlicingConfig config;
-    config.isVertical = type == EnforcerBlockerType::ENFORCER ? true : false;
-
     for (const ModelVolume* mv : this->model_object()->volumes)
         if (mv->is_model_part()) {
             const indexed_triangle_set custom_facets = seam
@@ -2858,7 +2854,7 @@ void PrintObject::project_and_append_custom_facets(
                 else {
                     std::vector<Polygons> projected;
                     // Support blockers or enforcers. Project downward facing painted areas upwards to their respective slicing plane.
-                    slice_mesh_slabs(custom_facets, zs_from_layers(this->layers()), this->trafo_centered() * mv->get_matrix(), nullptr, &projected, [](){}, config);
+                    slice_mesh_slabs(custom_facets, zs_from_layers(this->layers()), this->trafo_centered() * mv->get_matrix(), nullptr, &projected, [](){});
                     // Merge these projections with the output, layer by layer.
                     assert(! projected.empty());
                     assert(out.empty() || out.size() == projected.size());
