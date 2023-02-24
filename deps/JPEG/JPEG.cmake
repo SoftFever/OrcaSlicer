a2f88ac9d5c272e5f2cb2251f3658839cadf9360
lane.wei <lane.wei@bambulab.com>
2023-02-24 15:43:00 +0800

    ENH: deps: add a flag to select jpeg lib's version under linux
    by default use libjpeg.so.8
    
    Change-Id: I2602820e7ef901badc7e27b6108b02eaf3597ffe

--- deps/JPEG/JPEG.cmake
+++ deps/JPEG/JPEG.cmake
@@ -1,3 +1,16 @@
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    if (JPEG_VERSION STREQUAL "6")
+        message("Using Jpeg Lib 62")
+        set(jpeg_flag "")
+    elseif (JPEG_VERSION STREQUAL "7")
+        message("Using Jpeg Lib 70")
+        set(jpeg_flag "-DWITH_JPEG7=ON")
+    else ()
+        message("Using Jpeg Lib 80")
+        set(jpeg_flag "-DWITH_JPEG8=ON")
+    endif ()
+endif()
+
 bambustudio_add_cmake_project(JPEG
     URL https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/2.0.6.zip
     URL_HASH SHA256=017bdc33ff3a72e11301c0feb4657cb27719d7f97fa67a78ed506c594218bbf1
@@ -5,4 +18,5 @@ bambustudio_add_cmake_project(JPEG
     CMAKE_ARGS
         -DENABLE_SHARED=OFF
         -DENABLE_STATIC=ON
+        ${jpeg_flag}
 )
