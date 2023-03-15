3570b93d65bece882d510f10ae19a502e750d449
SoftFever <103989404+SoftFever@users.noreply.github.com>
2023-03-15 23:51:42 +0800

    sign mac app

--- build_release_macos.sh
+++ build_release_macos.sh
@@ -88,12 +88,12 @@ cp -R $resources_path ./OrcaSlicer.app/Contents/Resources
 # delete .DS_Store file
 find ./OrcaSlicer.app/ -name '.DS_Store' -delete
 # extract version
-export ver="_dev"
+export ver=$(grep '^#define SoftFever_VERSION' ../src/libslic3r/libslic3r_version.h | cut -d ' ' -f3)
+ver="_V${ver//\"}"
 echo $PWD
 if [ "1." != "$NIGHTLY_BUILD". ];
 then
-    ver=$(grep '^#define SoftFever_VERSION' ../src/libslic3r/libslic3r_version.h | cut -d ' ' -f3)
-    ver="_V${ver//\"}"
+    ver=${ver}_dev
 fi
 
 
