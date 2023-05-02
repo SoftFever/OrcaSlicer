b274ed6475a2e7425cf5ac72cdfdc0bc08f35db6
Scott Mudge <mail@scottmudge.com>
2023-05-02 11:34:03 -0400

    Boost update to 1.78 only for Windows, fixed wxWidgets re-patching issue for Windows, corrected wxWidgets webview nuget hash.

--- deps/wxWidgets/wxWidgets.cmake
+++ deps/wxWidgets/wxWidgets.cmake
@@ -1,4 +1,4 @@
-set(_wx_git_tag v3.1.5-bambu)
+set(_wx_git_tag v3.1.5)
 
 set(_wx_toolkit "")
     set(_wx_private_font "-DwxUSE_PRIVATE_FONTS=1")
@@ -17,7 +17,7 @@ else ()
 endif ()
 
 if (MSVC)
-    set(_patch_cmd ${PATCH_CMD} ${CMAKE_CURRENT_LIST_DIR}/0001-wxWidget-fix.patch)
+    set(_patch_cmd if not exist WXWIDGETS_PATCHED ( "${GIT_EXECUTABLE}" apply --verbose --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/0001-wxWidget-fix.patch && type nul > WXWIDGETS_PATCHED ) )
 else ()
     set(_patch_cmd test -f WXWIDGETS_PATCHED || ${PATCH_CMD} ${CMAKE_CURRENT_LIST_DIR}/0001-wxWidget-fix.patch && touch WXWIDGETS_PATCHED)
 endif ()
@@ -26,10 +26,11 @@ if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
     set(_patch_cmd ${PATCH_CMD} ${CMAKE_CURRENT_LIST_DIR}/0001-wxWidget-fix.patch)
 endif ()
 
-
-bambustudio_add_cmake_project(wxWidgets
-    GIT_REPOSITORY "https://github.com/scottmudge/wxWidgets-bambustudio"
+bambustudio_add_cmake_project(
+    wxWidgets
+    GIT_REPOSITORY "https://github.com/wxWidgets/wxWidgets"
     GIT_TAG ${_wx_git_tag}
+    PATCH_COMMAND ${_patch_cmd}
     DEPENDS ${PNG_PKG} ${ZLIB_PKG} ${EXPAT_PKG} dep_TIFF dep_JPEG
     CMAKE_ARGS
         -DwxBUILD_PRECOMP=ON
