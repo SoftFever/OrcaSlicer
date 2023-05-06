9f01d69d835ea63967ca05eedefcbbf83624013f
Heiko Liebscher <hliebscher@idn.de>
2023-05-06 14:36:42 +0200

    fix empty label

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -1764,7 +1764,7 @@ void PrintConfigDef::init_fff_params()
     def->set_default_value(new ConfigOptionBool(false));
 
     def = this->add("fan_speedup_time", coFloat);
-    def->label = L("");
+    def->label = L("Fan Speedup Time");
     def->tooltip = L("Start the fan this number of seconds earlier than its target start time (you can use fractional seconds)."
         " It assumes infinite acceleration for this time estimation, and will only take into account G1 and G0 moves (arc fitting"
         " is unsupported)."
