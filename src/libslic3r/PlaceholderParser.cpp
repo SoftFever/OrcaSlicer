e23b600239e752713e36afda15c2b2a518b11166
Scott Mudge <19617165+scottmudge@users.noreply.github.com>
2023-05-11 11:46:15 -0400

    Fix build warnings, update deprecated boost headers (#1035)
    Fix build warnings, update deprecated boost headers.

--- src/libslic3r/PlaceholderParser.cpp
+++ src/libslic3r/PlaceholderParser.cpp
@@ -40,11 +40,19 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/qi_lit.hpp>
+#if BOOST_VERSION >= 107800
+#include <boost/phoenix/core.hpp>
+#include <boost/phoenix/operator.hpp>
+#include <boost/phoenix/fusion.hpp>
+#include <boost/phoenix/stl.hpp>
+#include <boost/phoenix/object.hpp>
+#else
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
 #include <boost/spirit/include/phoenix_fusion.hpp>
 #include <boost/spirit/include/phoenix_stl.hpp>
 #include <boost/spirit/include/phoenix_object.hpp>
+#endif
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/spirit/repository/include/qi_distinct.hpp>
 #include <boost/spirit/repository/include/qi_iter_pos.hpp>
