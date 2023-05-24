049a5c5a8fb968f2217d07ff4e7c9263e07d4750
maosheng.wei <maosheng.wei@bambulab.com>
2023-05-24 09:54:26 +0800

    NEW: add Report issue entry
    Change-Id: Ie5cc195e34f2dbb94b4958c495cd28cd5bf4fd51

--- src/slic3r/GUI/MainFrame.cpp
+++ src/slic3r/GUI/MainFrame.cpp
@@ -1941,6 +1941,11 @@ static wxMenu* generate_help_menu()
         wxGetApp().plater()->get_current_canvas3D()->set_as_dirty();
         });
 
+    append_menu_item(helpMenu, wxID_ANY, _L("Report issue"), _L("Report issue"), [](wxCommandEvent &) {
+        std::string url = "https://github.com/bambulab/BambuStudio/issues/new/choose";
+        wxLaunchDefaultBrowser(url);
+    });
+
     // Report a bug
     //append_menu_item(helpMenu, wxID_ANY, _L("Report Bug(TODO)"), _L("Report a bug of BambuStudio"),
     //    [](wxCommandEvent&) {
