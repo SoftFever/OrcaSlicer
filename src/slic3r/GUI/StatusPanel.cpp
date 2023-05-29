c7278dffeb75df2544d5476d9c3bd7f7478a51d9
tao wang <tao.wang@bambulab.com>
2023-05-29 18:06:39 +0800

    FIX:filament backup does not interact after clicking
    Change-Id: I47086e92e74512661149557434c17a9587232ce6

--- src/slic3r/GUI/StatusPanel.cpp
+++ src/slic3r/GUI/StatusPanel.cpp
@@ -2871,7 +2871,7 @@ void StatusPanel::on_ams_unload(SimpleEvent &event)
 
 void StatusPanel::on_ams_filament_backup(SimpleEvent& event)
 {
-    if (obj && obj->filam_bak.size() > 0) {
+    if (obj) {
         AmsReplaceMaterialDialog* m_replace_material_popup = new AmsReplaceMaterialDialog(this);
         m_replace_material_popup->update_machine_obj(obj);
         m_replace_material_popup->ShowModal();
