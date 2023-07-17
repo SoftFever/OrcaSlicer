19525b656427e90b4d850fbfc1f010c459f09896
manch1n <miaoxin.chen@bambulab.com>
2023-07-17 15:59:20 +0800

    FIX: let independent support layer height option show all time.
    STUDIO-3703
    
    Change-Id: Ib67f9a9894cca8cb391bcceae087f026c0060f4d
    (cherry picked from commit 6c4bdcf1b7dbf8f55ec7bf5186ba3ccdde75e611)

--- src/slic3r/GUI/ConfigManipulation.cpp
+++ src/slic3r/GUI/ConfigManipulation.cpp
@@ -663,9 +663,6 @@ void ConfigManipulation::toggle_print_fff_options(DynamicPrintConfig *config, co
     for (auto el : {"flush_into_infill", "flush_into_support", "flush_into_objects"})
         toggle_field(el, have_prime_tower);
 
-    // BBS: MusangKing - Hide "Independent support layer height" option 
-    toggle_line("independent_support_layer_height", have_support_material && !have_prime_tower);
-
     bool have_avoid_crossing_perimeters = config->opt_bool("reduce_crossing_wall");
     toggle_line("max_travel_detour_distance", have_avoid_crossing_perimeters);
 
