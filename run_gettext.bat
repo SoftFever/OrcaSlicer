a4c632026ffab0e570735d3fc3cdce6a9cdccdc2
SoftFever <softfeverever@gmail.com>
2023-08-09 11:50:35 +0800

    update loacale

--- run_gettext.bat
+++ run_gettext.bat
@@ -2,7 +2,7 @@
 REM OrcaSlicer gettext
 REM Created by SoftFever on 27/5/23.
 xgettext --keyword=L --keyword=_L --keyword=_u8L --keyword=L_CONTEXT:1,2c --keyword=_L_PLURAL:1,2 --add-comments=TRN --from-code=UTF-8 --no-location --debug --boost -f ./bbl/i18n/list.txt -o ./bbl/i18n/OrcaSlicer.pot
-./build/src/hints/Release/hintsToPot.exe ./resources ./bbl/i18n
+build\src\hints\Release\hintsToPot ./resources ./bbl/i18n
 
 REM Print the current directory
 echo %cd%
