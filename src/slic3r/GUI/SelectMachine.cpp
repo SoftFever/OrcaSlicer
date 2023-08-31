b5e38cfe30e63fc8e2da760e7eb95f7f6300f910
tao wang <tao.wang@bambulab.com>
2023-08-31 09:15:02 +0800

    ENH:optimized display of German and Swedish
    jira:[STUDIO-3722]
    
    Change-Id: I4562b1d55c709a6aac56f91601b12ae3f3459fd1

--- src/slic3r/GUI/SelectMachine.cpp
+++ src/slic3r/GUI/SelectMachine.cpp
@@ -1950,10 +1950,10 @@ void SelectMachineDialog::update_priner_status_msg(wxString msg, bool is_warning
     } else {
         msg          = format_text(msg);
 
-        auto str_new = msg.ToStdString();
+        auto str_new = msg.utf8_string();
         stripWhiteSpace(str_new);
 
-        auto str_old = m_statictext_printer_msg->GetLabel().ToStdString();
+        auto str_old = m_statictext_printer_msg->GetLabel().utf8_string();
         stripWhiteSpace(str_old);
 
         if (str_new != str_old) {
