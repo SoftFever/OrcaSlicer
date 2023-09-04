596fcb0f690c757ea0b198975ece33d0133cbde6
xun.zhang <xun.zhang@bambulab.com>
2023-09-04 22:11:38 +0800

    ENH: add filement type pps-cf
    jira: STUDIO-3518
    
    Change-Id: Ifebaa96ca2e8336bf5449d7c9bd053ab00ab8041

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -1357,6 +1357,8 @@ void PrintConfigDef::init_fff_params()
     def->enum_values.push_back("PVA");
     def->enum_values.push_back("HIPS");
     def->enum_values.push_back("PLA-AERO");
+    def->enum_values.push_back("PPS");
+    def->enum_values.push_back("PPS-CF");
     def->mode = comSimple;
     def->set_default_value(new ConfigOptionStrings { "PLA" });
 
