26b8ca6599ce9d084d7971e4ac8bcf86d2ec3277
zhou.xu <zhou.xu@bambulab.com>
2023-09-08 09:41:45 +0800

    NEW:add flat shader for cut tool
    Jira:STUDIO-4227
    thanks for PrusaSlcer and enricoturri1966
    
    commit 76d1d4949bbd0964717f8112aae992a1267c5eb4
    Author: enricoturri1966 <enricoturri@seznam.cz>
    Date:   Mon Mar 21 13:10:47 2022 +0100
        Tech ENABLE_GL_SHADERS_ATTRIBUTES - Added shaders for glsl version 140
    ...
    
    Change-Id: I4db00805830f07a5eaa95cfc7b8254c61b86f0c8

--- src/slic3r/GUI/GLShadersManager.cpp
+++ src/slic3r/GUI/GLShadersManager.cpp
@@ -34,11 +34,12 @@ std::pair<bool, std::string> GLShadersManager::init()
     bool valid = true;
 
     // used to render bed axes and model, selection hints, gcode sequential view marker model, preview shells, options in gcode preview
-   valid &= append_shader("gouraud_light", { "gouraud_light.vs", "gouraud_light.fs" });
+    valid &= append_shader("gouraud_light", { "gouraud_light.vs", "gouraud_light.fs" });
     //used to render thumbnail
-   valid &= append_shader("thumbnail", { "thumbnail.vs", "thumbnail.fs" });
-   // used to render first layer for calibration
-   valid &= append_shader("cali", { "cali.vs", "cali.fs"});
+    valid &= append_shader("thumbnail", { "thumbnail.vs", "thumbnail.fs" });
+    // used to render first layer for calibration
+    valid &= append_shader("cali", { "cali.vs", "cali.fs"});
+    valid &= append_shader("flat", {"flat.vs", "flat.fs"});
     // used to render printbed
     valid &= append_shader("printbed", { "printbed.vs", "printbed.fs" });
     // used to render options in gcode preview
