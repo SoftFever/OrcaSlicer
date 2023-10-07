84524e76857a2468da1aa447e12c3058c6f3a217
tao wang <tao.wang@bambulab.com>
2023-10-07 16:57:46 +0800

    FIX:set print mode before print all plate
    jira:[STUDIO-4649,STUDIO-4650]
    
    Change-Id: Ib4ddda6f1461a31a7d7407786bcb0d1859b3fb82

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -6314,6 +6314,7 @@ void Plater::priv::on_action_print_all(SimpleEvent&)
 
     //BBS
     if (!m_select_machine_dlg) m_select_machine_dlg = new SelectMachineDialog(q);
+    m_select_machine_dlg->set_print_type(PrintFromType::FROM_NORMAL);
     m_select_machine_dlg->prepare(PLATE_ALL_IDX);
     m_select_machine_dlg->ShowModal();
     record_start_print_preset("print_all");
