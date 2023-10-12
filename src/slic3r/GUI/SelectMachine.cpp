bfe0cb823d3e78ba2acf672c7ff560f01f0e6b1b
xun.zhang <xun.zhang@bambulab.com>
2023-10-12 18:23:10 +0800

    ENH: do not pop up open door tip
    1. Do not pop up open door tip. Write the info to slicer info
    
    jira: [NEW]
    Signed-off-by: xun.zhang <xun.zhang@bambulab.com>
    Change-Id: I1f611ee18faa5820a26443a13614f68f3e6ba780

--- src/slic3r/GUI/SelectMachine.cpp
+++ src/slic3r/GUI/SelectMachine.cpp
@@ -2379,8 +2379,8 @@ void SelectMachineDialog::on_ok_btn(wxCommandEvent &event)
     for (auto warning : plate->get_slice_result()->warnings) {
         if (warning.msg == BED_TEMP_TOO_HIGH_THAN_FILAMENT) {
             if ((obj_->get_printer_is_enclosed())){
-                confirm_text.push_back(Plater::get_slice_warning_string(warning) + "\n");
-                has_slice_warnings = true;
+                // confirm_text.push_back(Plater::get_slice_warning_string(warning) + "\n");
+                // has_slice_warnings = true;
             }
         }
         else if (warning.msg == NOT_SUPPORT_TRADITIONAL_TIMELAPSE) {
