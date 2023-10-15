1c26ec9e236ddb33be9f22f83054e59bd76e73b2
SoftFever <softfeverever@gmail.com>
2023-10-15 18:08:43 +0800

    Increase painting resolution

--- src/slic3r/GUI/Gizmos/GLGizmoSeam.hpp
+++ src/slic3r/GUI/Gizmos/GLGizmoSeam.hpp
@@ -35,7 +35,7 @@ protected:
     std::string get_gizmo_entering_text() const override { return "Entering Seam painting"; }
     std::string get_gizmo_leaving_text() const override { return "Leaving Seam painting"; }
     std::string get_action_snapshot_name() override { return "Paint-on seam editing"; }
-    static const constexpr float      CursorRadiusMin = 0.1f; // cannot be zero
+    static const constexpr float      CursorRadiusMin = 0.05f; // cannot be zero
 
 private:
     bool on_init() override;
