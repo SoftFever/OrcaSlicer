76441c2ea37c15152fdbe733a22362260a547553
maosheng.wei <maosheng.wei@bambulab.com>
2023-10-17 14:38:40 +0800

    ENH: Add a link to a parameter
    Jira: XXXX
    
    Change-Id: Ic8871cdada4646ba8740d7262fbd2944b24c3171
    Signed-off-by: maosheng.wei <maosheng.wei@bambulab.com>

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -1864,8 +1864,8 @@ void TabPrint::build()
         optgroup->append_single_option_line("slice_closing_radius");
         optgroup->append_single_option_line("resolution");
         optgroup->append_single_option_line("enable_arc_fitting");
-        optgroup->append_single_option_line("xy_hole_compensation");
-        optgroup->append_single_option_line("xy_contour_compensation");
+        optgroup->append_single_option_line("xy_hole_compensation", "xy-hole-contour-compensation");
+        optgroup->append_single_option_line("xy_contour_compensation", "xy-hole-contour-compensation");
         optgroup->append_single_option_line("elefant_foot_compensation");
 
         optgroup = page->new_optgroup(L("Ironing"), L"param_ironing");
