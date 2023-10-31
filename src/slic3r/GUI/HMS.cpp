88ed51e11d3b5c6ee6e03efda662a15d26395759
Stone Li <stone.li@bambulab.com>
2023-10-31 09:38:38 +0800

    ENH: fix language cs/ru no hms info
    JIRA：STUDIO-4973 STUDIO-4972 STUDIO-4966
    
    Change-Id: Ic1f668eb24009fc81096cf87cb8eebed6551c656
    Signed-off-by: Stone Li <stone.li@bambulab.com>

--- src/slic3r/GUI/HMS.cpp
+++ src/slic3r/GUI/HMS.cpp
@@ -139,7 +139,13 @@ std::string HMSQuery::hms_language_code()
         // set language code to en by default
         return "en";
     std::string lang_code = wxGetApp().app_config->get_language_code();
-    if (lang_code.empty()) {
+    if (lang_code.compare("uk") == 0
+        || lang_code.compare("cs") == 0
+        || lang_code.compare("ru") == 0) {
+        BOOST_LOG_TRIVIAL(info) << "HMS: using english for lang_code = " << lang_code;
+        return "en";
+    }
+    else if (lang_code.empty()) {
         // set language code to en by default
         return "en";
     }
