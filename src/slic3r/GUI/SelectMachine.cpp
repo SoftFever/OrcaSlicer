cd02791647dd3c845dfb78f5197c27d690d6e97d
wenjie.guo <wenjie.guo@bambulab.com>
2023-11-20 16:26:01 +0800

    FIX: the word search is not translated
    Jira: STUDIO-5224
    
    The world search in the device panel is not translated.
    
    Signed-off-by: wenjie.guo <wenjie.guo@bambulab.com>
    Change-Id: Ia3d651c2159a3aad94e10cd0a6da98848f53ee2a
    (cherry picked from commit 4a46a0a4750d82d49c9523f4b668a1a00c41ed83)

--- src/slic3r/GUI/SelectMachine.cpp
+++ src/slic3r/GUI/SelectMachine.cpp
@@ -382,6 +382,7 @@ SelectMachinePopup::SelectMachinePopup(wxWindow *parent)
 #if !BBL_RELEASE_TO_PUBLIC && defined(__WINDOWS__)
 	m_sizer_search_bar = new wxBoxSizer(wxVERTICAL);
 	m_search_bar = new wxSearchCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
+	m_search_bar->SetDescriptiveText(_L("Search"));
 	m_search_bar->ShowSearchButton( true );
 	m_search_bar->ShowCancelButton( false );
 	m_sizer_search_bar->Add( m_search_bar, 1, wxALL| wxEXPAND, 1 );
