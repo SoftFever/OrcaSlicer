ce9a8d7b20b336319bec7c79554ca24192e38d94
Ioannis Giannakas <59056762+igiannakas@users.noreply.github.com>
2023-11-26 13:43:47 +0000

    Internal Bridge Flow rate parameter introduction (#2859)
    * Internal Bridge Flow rate parameter introduction
    
    * updated incorrect capitalisation
    
    * Updated parameter ordering

--- src/libslic3r/PrintObject.cpp
+++ src/libslic3r/PrintObject.cpp
@@ -1106,7 +1106,7 @@ bool PrintObject::invalidate_state_by_config_options(
             || opt_key == "overhang_speed_classic") {
             steps.emplace_back(posPerimeters);
             steps.emplace_back(posSupportMaterial);
-        } else if (opt_key == "bridge_flow") {
+        } else if (opt_key == "bridge_flow" || opt_key == "internal_bridge_flow") {
             if (m_config.support_top_z_distance > 0.) {
             	// Only invalidate due to bridging if bridging is enabled.
             	// If later "support_top_z_distance" is modified, the complete PrintObject is invalidated anyway.
