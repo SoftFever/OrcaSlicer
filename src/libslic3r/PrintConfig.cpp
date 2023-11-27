c21a1bf1df56f0626657e00538b2586f5dcc8310
Arthur <arthur.tang@bambulab.com>
2023-11-27 09:56:52 +0800

    ENH: change default tree_support_wall_count to 0
    Now normal support also uses this option, so we can't default it to 1, otherwise normal supports will be too hard to remove.
    
    jira: none
    Change-Id: Ic5700af5c17e3a7b265c8915f28b0db35c6e06e6
    (cherry picked from commit 6b84a9826da108b76569e686bd9def0b23ae29fd)

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -3278,7 +3278,7 @@ void PrintConfigDef::init_fff_params()
     def->tooltip = L("This setting specify the count of walls around support");
     def->min = 0;
     def->mode = comAdvanced;
-    def->set_default_value(new ConfigOptionInt(1));
+    def->set_default_value(new ConfigOptionInt(0));
 
     def = this->add("tree_support_brim_width", coFloat);
     def->label = L("Tree support brim width");
