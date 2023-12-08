913154fb739602b2d9f2d9bcf90b730373b5f504
PilotMatt <55398315+PilotMatt@users.noreply.github.com>
2023-12-08 14:26:45 -0500

    few small changes from Mac attack

--- src/slic3r/GUI/SelectMachine.hpp
+++ src/slic3r/GUI/SelectMachine.hpp
@@ -422,7 +422,6 @@ public:
     void Enable_Auto_Refill(bool enable);
     void connect_printer_mqtt();
     void on_send_print();
-    void on_printago_send_print(MachineObject* obj);
     void clear_ip_address_config(wxCommandEvent& e);
     void on_refresh(wxCommandEvent& event);
     void on_set_finish_mapping(wxCommandEvent& evt);
