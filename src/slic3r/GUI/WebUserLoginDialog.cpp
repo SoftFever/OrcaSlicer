ae8995fec224e026b3f9ec43269bddcad19e32d0
SoftFever <softfeverever@gmail.com>
2023-12-11 22:20:34 +0800

    update user agent

--- src/slic3r/GUI/WebUserLoginDialog.cpp
+++ src/slic3r/GUI/WebUserLoginDialog.cpp
@@ -87,7 +87,7 @@ ZUserLogin::ZUserLogin() : wxDialog((wxWindow *) (wxGetApp().mainframe), wxID_AN
 
         BOOST_LOG_TRIVIAL(info) << "login url = " << TargetUrl.ToStdString();
 
-        m_bbl_user_agent = wxString::Format("BBL-Slicer/v%s", SLIC3R_VERSION);
+        m_bbl_user_agent = wxString::Format("OrcaSlicer/v%s", SLIC3R_VERSION);
 
         // set the frame icon
 
