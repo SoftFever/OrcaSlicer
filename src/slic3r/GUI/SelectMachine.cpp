463699930ac9146f6ac0d0c1ca922683b726a161
tao wang <tao.wang@bambulab.com>
2023-12-22 11:39:16 +0800

    ENH:adjusting the warning level of timelpase
    jira:[STUDIO-5662]
    
    Change-Id: I4902b22d316f5e09a97a62c88b8a98e55c405434

--- src/slic3r/GUI/SelectMachine.cpp
+++ src/slic3r/GUI/SelectMachine.cpp
@@ -3442,15 +3442,16 @@ void SelectMachineDialog::update_show_status()
         }
     }
 
-    if (has_timelapse_warning()) {
-        show_status(PrintDialogStatus::PrintStatusTimelapseWarning);
-        return;
-    }
-
     // no ams
     if (!obj_->has_ams() || !m_checkbox_list["use_ams"]->GetValue()) {
-        if (!has_tips(obj_))
-            show_status(PrintDialogStatus::PrintStatusReadingFinished);
+        if (!has_tips(obj_)) {
+            if (has_timelapse_warning()) {
+                show_status(PrintDialogStatus::PrintStatusTimelapseWarning);
+            }
+            else {
+                show_status(PrintDialogStatus::PrintStatusReadingFinished);
+            }
+        }
         return;
     }
 
@@ -3458,7 +3459,14 @@ void SelectMachineDialog::update_show_status()
         if (!m_checkbox_list["use_ams"]->GetValue()) {
             m_ams_mapping_result.clear();
             sync_ams_mapping_result(m_ams_mapping_result);
-            show_status(PrintDialogStatus::PrintStatusDisableAms);
+
+            if (has_timelapse_warning()) {
+                show_status(PrintDialogStatus::PrintStatusTimelapseWarning);
+            }
+            else {
+                show_status(PrintDialogStatus::PrintStatusDisableAms);
+            }
+
             return;
         }
     }
@@ -3477,7 +3485,14 @@ void SelectMachineDialog::update_show_status()
             show_status(PrintDialogStatus::PrintStatusNeedUpgradingAms, params);
         } else {
             if (obj_->is_valid_mapping_result(m_ams_mapping_result)) {
-                show_status(PrintDialogStatus::PrintStatusAmsMappingByOrder);
+
+                if (has_timelapse_warning()) {
+                    show_status(PrintDialogStatus::PrintStatusTimelapseWarning);
+                }
+                else {
+                    show_status(PrintDialogStatus::PrintStatusAmsMappingByOrder);
+                }
+                
             } else {
                 int mismatch_index = -1;
                 for (int i = 0; i < m_ams_mapping_result.size(); i++) {
@@ -3498,20 +3513,31 @@ void SelectMachineDialog::update_show_status()
     }
 
     if (m_ams_mapping_res) {
-        show_status(PrintDialogStatus::PrintStatusAmsMappingSuccess);
+        if (has_timelapse_warning()) {
+            show_status(PrintDialogStatus::PrintStatusTimelapseWarning);
+        }
+        else {
+            show_status(PrintDialogStatus::PrintStatusAmsMappingSuccess);
+        }
         return;
     }
     else {
         if (obj_->is_valid_mapping_result(m_ams_mapping_result)) {
-            if (!has_tips(obj_))
-                show_status(PrintDialogStatus::PrintStatusAmsMappingValid);
-            return;
+            if (!has_tips(obj_)){
+                if (has_timelapse_warning()) {
+                    show_status(PrintDialogStatus::PrintStatusTimelapseWarning);
+                }
+                else {
+                    show_status(PrintDialogStatus::PrintStatusAmsMappingValid);
+                }
+                return;
+            }       
         }
         else {
             show_status(PrintDialogStatus::PrintStatusAmsMappingInvalid);
             return;
         }
-    }
+    } 
 }
 
 bool SelectMachineDialog::has_timelapse_warning()
