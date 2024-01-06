28b96ae9daecbb9f53f0b0c10951af16fd30988e
Leland Lucius <github@homerow.net>
2024-01-05 22:21:12 -0600

    Only use modified scaling on Windows
    And only if the system's scaling factor is greater than 300%.

--- src/slic3r/GUI/ImGuiWrapper.cpp
+++ src/slic3r/GUI/ImGuiWrapper.cpp
@@ -2656,14 +2656,21 @@ void ImGuiWrapper::init_font(bool compress)
             throw Slic3r::RuntimeError("ImGui: Could not load deafult font");
         }
     }
-    default_font->Scale = 1.5f;
 
     bold_font        = io.Fonts->AddFontFromFileTTF((Slic3r::resources_dir() + "/fonts/" + font_name_bold).c_str(), m_font_size, &cfg, ranges.Data);
     if (bold_font == nullptr) {
         bold_font = io.Fonts->AddFontDefault();
         if (bold_font == nullptr) { throw Slic3r::RuntimeError("ImGui: Could not load deafult font"); }
     }
-    bold_font->Scale = 1.5f;
+
+#ifdef _WIN32
+    // Render the text a bit larger (see GLCanvas3D::_resize() and issue #3401), but only if the scale factor
+    // for the Display is greater than 300%.
+    if (wxGetApp().em_unit() > 30) {
+        default_font->Scale = 1.5f;
+        bold_font->Scale    = 1.5f;
+    }
+#endif
 
 #ifdef __APPLE__
     ImFontConfig config;
