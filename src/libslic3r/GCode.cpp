ab161d5a2b82cbe7f3b2a26a68ba0e417d703571
Seth LaForge <sethml@ofb.net>
2024-01-28 23:14:54 -0800

    Fix DEBUG build (#3861)
    * Fix asserts which would not compile.
    
    * Fix DEBUG build.
    
    Fully enable checking gcode placeholders in debug builds.

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -2956,7 +2956,7 @@ std::string GCode::placeholder_parser_process(const std::string &name, const std
     // Orca: Added CMake config option since debug is rarely used in current workflow.
     // Also changed from throwing error immediately to storing messages till slicing is completed
     // to raise all errors at the same time.
-#if !defined(NDEBUG) || ORCA_CHECK_GCODE_PLACEHOLDERS // CHECK_CUSTOM_GCODE_PLACEHOLDERS
+#if ORCA_CHECK_GCODE_PLACEHOLDERS
     if (config_override) {
         const auto& custom_gcode_placeholders = custom_gcode_specific_placeholders();
 
