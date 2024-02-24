78d5369546ad6bf29f386ec832603cc1f4aa073a
SoftFever <softfeverever@gmail.com>
2024-02-24 14:03:41 +0800

    fix Linux build error

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -2335,7 +2335,7 @@ void GCode::_do_export(Print& print, GCodeOutputStream &file, ThumbnailsGenerato
         auto probe_dist_x  = std::max(1., m_config.bed_mesh_probe_distance.value.x());
         auto probe_dist_y  = std::max(1., m_config.bed_mesh_probe_distance.value.y());
         int  probe_count_x = std::max(3, (int) std::ceil(mesh_bbox.size().x() / probe_dist_x));
-        int  probe_count_y = std::max(3, (int) std::ceill(mesh_bbox.size().y() / probe_dist_y));
+        int  probe_count_y = std::max(3, (int) std::ceil(mesh_bbox.size().y() / probe_dist_y));
         this->placeholder_parser().set("bed_mesh_probe_count", new ConfigOptionInts({probe_count_x, probe_count_y}));
         auto bed_mesh_algo = "bicubic";
         if (probe_count_x < 4 || probe_count_y < 4) {
