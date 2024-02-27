517631fe528f78bae156b6db440921de583ca3f7
Eric Lin <anselor@gmail.com>
2024-02-27 17:42:29 -0500

    Refactor in preparation for supporting both validation and generation use cases.

--- resources/profiles/update_profile.py
+++ resources/profiles/update_profile.py
@@ -9,58 +9,18 @@ import json
 import locale
 from pathlib import Path
 import sys
-from typing import Callable, Dict, List
-
-VERBOSE = False
-
-cli_args = ArgumentParser()
-cli_args.add_argument('vendor', help='Vendor name. (Anker, BBL, etc)')
-cli_args.add_argument('--verbose', '-v', action="store_true", default=False)
-
-args = cli_args.parse_args()
-
-VERBOSE = args.verbose
-
-script_path = Path(__file__).resolve().parent
-profile_path = script_path / args.vendor
-json_path = script_path / f'{args.vendor}.json'
-
-if not (profile_path.exists() and profile_path.is_dir()):
-    print(f'Could not find vendor profile path: {profile_path}')
-    sys.exit(-1)
-if not (json_path.exists() and json_path.is_file()):
-    print(f'Could not find vendor profile definition: {json_path}')
-    sys.exit(-1)
-
-if json_path.exists():
-    print(f'found: {json_path}')
-else:
-    sys.exit()
-
-anker_data = json.loads(json_path.read_text())
-
+from typing import Callable, cast, Dict, List, Union
 
 machine_model_key = 'machine_model_list'
 machine_key = 'machine_list'
 process_key = 'process_list'
 filament_key = 'filament_list'
 
-machine_path = profile_path / 'machine'
-process_path = profile_path / 'process'
-filament_path = profile_path / 'filament'
+VERBOSE = False
 
-new_models = []
-new_machines = []
-new_processes = []
-new_filaments = []
+script_path = Path(__file__).resolve().parent
 
-found_machines = {}
-
-machine_paths = []
-
-missing_deps = False
-
-def anker_name_cmp(left: str, right: str) -> int:
+def profile_name_cmp(left: str, right: str) -> int:
     if left.startswith('fdm'):
         if right.startswith('fdm'):
             return locale.strcoll(left, right)
@@ -78,94 +38,149 @@ def anker_name_cmp(left: str, right: str) -> int:
     return locale.strcoll(left, right)
 
 
-def read_config_matches(found_entries: Dict[str, Path],
-                        expected_entry_type: str,
-                        sub_path: Path,
-                        matcher: Callable[[Path], bool] = lambda path: path.is_file() and path.suffix.lower() == '.json') -> List[Dict[str, str]]:
-    pending_path_entries = {}
-    new_items = []
+class VendorProfile:
 
-    for entry in sub_path.iterdir():
-        if VERBOSE: print(f'Checking {entry}')
-        if matcher(entry):
-            try:
-                entry_data = json.loads(entry.read_text())
-            except JSONDecodeError:
-                print(f'Could not parse JSON in {entry}')
-                continue
-            entry_type = entry_data.get('type', None)
-            entry_name = entry_data.get('name', None)
-            if entry_type == expected_entry_type and entry_name and entry_name not in found_entries:
-                if VERBOSE: print(f'{entry} matched')
-                pending_path_entries[entry_name] = (entry, entry_data)
-        else:
-            if VERBOSE: print('... not a match')
+    def __init__(self, vendor_name: str) -> None:
+        self.json_path = script_path / f'{vendor_name}.json'
+        self.manifest_data = cast(Dict[str, Union[str, List[Dict[str, str]]]], json.loads(self.json_path.read_text()))
+        self.profile_path = script_path / vendor_name
 
-    failed_dependency = False
-    first_pass = len(found_entries) == 0
+        self.machine_path = self.profile_path / 'machine'
+        self.process_path = self.profile_path / 'process'
+        self.filament_path = self.profile_path / 'filament'
 
-    if VERBOSE: print(f'Processing {len(pending_path_entries)} entries')
+        self.found_machines = {}
 
-    while pending_path_entries and not failed_dependency:
-        failed_dependency = True
-        working_path_entries = dict(pending_path_entries)
+        self.missing_deps: List[str, Path] = []
+
+    def validate(self) -> bool:
+        if not (self.json_path.exists() and self.json_path.is_file()):
+            raise ValueError(f'Could not find vendor profile definition: {self.json_path}')
+        if not (self.profile_path.exists() and self.profile_path.is_dir()):
+            raise ValueError(f'Could not find vendor profile path: {self.profile_path}')
+
+    def read_config_matches(self,
+                            found_entries: Dict[str, Path],
+                            expected_entry_type: str,
+                            sub_path: Path,
+                            matcher: Callable[[Path], bool] = lambda
+                                    path: path.is_file() and path.suffix.lower() == '.json') -> List[Dict[str, str]]:
         pending_path_entries = {}
-        for entry_name in sorted(working_path_entries.keys(), key=cmp_to_key(anker_name_cmp)):
-            entry, entry_data = working_path_entries[entry_name]
-            if VERBOSE: print(f'Inspecting {entry}')
-            if (first_pass and 'inherits' not in entry_data) or (not first_pass and entry_data['inherits'] in found_entries):
-                failed_dependency = False
-                found_entries[entry_name] = entry.relative_to(profile_path)
-                new_items.append({'name': entry_name, 'sub_path': str(entry.relative_to(profile_path))})
+        new_items = []
+
+        for entry in sub_path.iterdir():
+            if VERBOSE: print(f'Checking {entry}')
+            if matcher(entry):
+                try:
+                    entry_data = json.loads(entry.read_text())
+                except JSONDecodeError:
+                    print(f'Could not parse JSON in {entry}')
+                    continue
+                entry_type = entry_data.get('type', None)
+                entry_name = entry_data.get('name', None)
+                if entry_type == expected_entry_type and entry_name and entry_name not in found_entries:
+                    if VERBOSE: print(f'{entry} matched')
+                    pending_path_entries[entry_name] = (entry, entry_data)
             else:
-                if VERBOSE: print(f'Saving {entry_name} for next pass')
-                pending_path_entries[entry_name] = (entry, entry_data)
-        
-        first_pass = False
+                if VERBOSE: print('... not a match')
 
-    if failed_dependency:
-        global missing_deps
-        missing_deps = failed_dependency
-        print(f'Dependencies missing for the following:')
-        for entry_name, entry_tuple in pending_path_entries.items():
-            entry, entry_data = entry_tuple
-            print(f' - [{entry_data["type"]}] {entry_name}: {str(entry.relative_to(profile_path))}')
-    return new_items
+        # if we make it through the entire list of pending path entries and find
+        # no entries that can be added that means all remaining entries have
+        # unmet dependencies.
+        failed_dependency = False
+        first_pass = len(found_entries) == 0
 
+        if VERBOSE: print(f'Processing {len(pending_path_entries)} entries')
 
-def read_configs(expected_entry_type: str, sub_path: Path) -> List[Dict[str, str]]:
-    found_entries: Dict[str, Path] = {}
-    print(f'Loading {expected_entry_type} from {sub_path}...')
-    if VERBOSE: print(f'...fdm')
-    new_items = read_config_matches(
-        found_entries,
-        expected_entry_type,
-        sub_path,
-        lambda path: path.is_file() and path.suffix.lower() == '.json' and path.name.startswith('fdm'))
-    
-    if VERBOSE: print(f'...base')
-    new_items.extend(
-        read_config_matches(
+        while pending_path_entries and not failed_dependency:
+            failed_dependency = True
+            working_path_entries = dict(pending_path_entries)
+            pending_path_entries = {}
+            for entry_name in sorted(working_path_entries.keys(), key=cmp_to_key(profile_name_cmp)):
+                entry, entry_data = working_path_entries[entry_name]
+                if VERBOSE: print(f'Inspecting {entry}')
+                if (first_pass and 'inherits' not in entry_data) or (
+                        not first_pass and entry_data['inherits'] in found_entries):
+                    failed_dependency = False
+                    found_entries[entry_name] = entry.relative_to(self.profile_path)
+                    new_items.append({'name': entry_name, 'sub_path': str(entry.relative_to(self.profile_path))})
+                else:
+                    if VERBOSE: print(f'Saving {entry_name} for next pass')
+                    pending_path_entries[entry_name] = (entry, entry_data)
+
+            first_pass = False
+
+        if failed_dependency:
+            self.missing_deps.extend(list(pending_path_entries.items()))
+        return new_items
+
+    def read_configs(self,
+                     expected_entry_type: str,
+                     sub_path: Path) -> List[Dict[str, str]]:
+        found_entries: Dict[str, Path] = {}
+        print(f'Loading {expected_entry_type} from {sub_path}...')
+        if VERBOSE: print(f'...fdm')
+        new_items = self.read_config_matches(
             found_entries,
             expected_entry_type,
             sub_path,
-            lambda path: path.is_file() and path.suffix.lower() == '.json' and '@base' in path.name.lower()))
-    
-    if VERBOSE: print(f'...defs')
-    new_items.extend(read_config_matches(found_entries, expected_entry_type, sub_path))
-    print(' - ' + '\n - '.join([str(item) for item in new_items]))
-    
-    return new_items
-    
+            lambda path: path.is_file() and path.suffix.lower() == '.json' and path.name.startswith('fdm'))
 
-anker_data[machine_model_key] = read_configs('machine_model', machine_path)
-anker_data[machine_key] = read_configs('machine', machine_path)
-anker_data[process_key] = read_configs('process', process_path)
-anker_data[filament_key] = read_configs('filament', filament_path)
+        if VERBOSE: print(f'...base')
+        new_items.extend(
+            self.read_config_matches(
+                found_entries,
+                expected_entry_type,
+                sub_path,
+                lambda path: path.is_file() and path.suffix.lower() == '.json' and '@base' in path.name.lower()))
 
-if not missing_deps:
-    input(f'Press Enter to write results to {json_path}')
-    with open(json_path, 'w', encoding='utf-8') as f:
-        json.dump(anker_data, f, ensure_ascii=False, indent=4)
-else:
-    print('Could not generate due to missing dependencies')
+        if VERBOSE: print(f'...defs')
+        new_items.extend(self.read_config_matches(found_entries, expected_entry_type, sub_path))
+        print(' - ' + '\n - '.join([str(item) for item in new_items]))
+
+        return new_items
+
+    def generate_manifest(self) -> Dict[str, Union[str, List[Dict[str, str]]]]:
+        manifest_data = dict(self.manifest_data)
+        manifest_data[machine_model_key] = self.read_configs('machine_model', self.machine_path)
+        manifest_data[machine_key] = self.read_configs('machine', self.machine_path)
+        manifest_data[process_key] = self.read_configs('process', self.process_path)
+        manifest_data[filament_key] = self.read_configs('filament', self.filament_path)
+        return manifest_data
+
+    def write(self, manifest_data: Dict[str, Union[str, List[Dict[str, str]]]]) -> None:
+        with open(self.json_path, 'w', encoding='utf-8') as f:
+            json.dump(manifest_data, f, ensure_ascii=False, indent=4)
+
+def main():
+    global VERBOSE
+
+    cli_args = ArgumentParser()
+    cli_args.add_argument('vendor', help='Vendor name. (Anker, BBL, etc)')
+    cli_args.add_argument('--verbose', '-v', action="store_true", default=False)
+
+    args = cli_args.parse_args()
+
+    VERBOSE = args.verbose
+
+    vendor_profile = VendorProfile(args.vendor)
+    try:
+        vendor_profile.validate()
+    except ValueError as ex:
+        print(ex)
+        return
+
+    manifest_data = vendor_profile.generate_manifest()
+
+    if vendor_profile.missing_deps:
+        print(f'The following profile definitions had missing dependencies:')
+        for entry_name, entry_tuple in vendor_profile.missing_deps:
+            entry, entry_data = entry_tuple
+            print(f'{entry.relative_to(vendor_profile.profile_path)} [{entry_data["type"]}] {entry_name}: {str(entry.relative_to(profile_path))}')
+    else:
+        input(f'Press Enter to write results to {vendor_profile.json_path}')
+        vendor_profile.write(manifest_data)
+
+
+if __name__ == '__main__':
+    main()
