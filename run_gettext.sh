92274bb061c6aea03fad9ef43876472638b2593f
Dzmitry Neviadomski <nevack.d@gmail.com>
2024-03-03 05:41:22 +0300

    FIX: Partially revent changes in run_gettext.sh
    Revert changes for option parsing.
    Leave fixes for issues found by ShellCheck linter tool.
    
    Signed-off-by: Dzmitry Neviadomski <nevack.d@gmail.com>

--- run_gettext.sh
+++ run_gettext.sh
@@ -10,20 +10,12 @@ for arg in "$@"
 do
     if [ "$arg" = "--full" ]; then
         FULL_MODE=true
-        shift
-        FULL_MODE_BUILD_PATH="$1"
-        FULL_MODE_BUILD_CONFIG_SUBDIR="$2"
     fi
 done
 
 if $FULL_MODE; then
     xgettext --keyword=L --keyword=_L --keyword=_u8L --keyword=L_CONTEXT:1,2c --keyword=_L_PLURAL:1,2 --add-comments=TRN --from-code=UTF-8 --no-location --debug --boost -f ./localization/i18n/list.txt -o ./localization/i18n/OrcaSlicer.pot
-    if [ -d "$FULL_MODE_BUILD_PATH/src/hints$FULL_MODE_BUILD_CONFIG_SUBDIR" ]; then
-        "$FULL_MODE_BUILD_PATH/src/hints$FULL_MODE_BUILD_CONFIG_SUBDIR/hintsToPot.app/Contents/MacOS/hintsToPot" ./resources ./localization/i18n
-    else 
-        echo "Cannot find $FULL_MODE_BUILD_PATH/src/hints$FULL_MODE_BUILD_CONFIG_SUBDIR dir"
-        echo "Skipping hintsToPot run!"
-    fi
+    ./build_arm64/src/hints/Release/hintsToPot.app/Contents/MacOS/hintsToPot ./resources ./localization/i18n
 fi
 
 
@@ -36,7 +28,7 @@ do
 
     if [ -f "$dir/OrcaSlicer_${lang}.po" ]; then
         if $FULL_MODE; then
-            msgmerge -N -o "$dir/OrcaSlicer_${lang}.po" "$dir/OrcaSlicer_${lang}.po" $pot_file
+            msgmerge -N -o "$dir/OrcaSlicer_${lang}.po" "$dir/OrcaSlicer_${lang}.po" "$pot_file"
         fi
         mkdir -p "resources/i18n/${lang}"
         msgfmt --check-format -o "resources/i18n/${lang}/OrcaSlicer.mo" "$dir/OrcaSlicer_${lang}.po"
