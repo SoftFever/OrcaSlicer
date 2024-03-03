0d9451917b66b969ecd037aca3de2ed8f2ec42b8
Dzmitry Neviadomski <nevack.d@gmail.com>
2024-03-03 04:03:43 +0300

    ENH: Improve macOS build script
     - Update shebang to bash, as it is already expected to be present in
       run_gettext.sh
     - Added fail-fast shell options.
     - Changed default CMake generator to Ninja
     - Adopted configuration for non-multi-config generators (Ninja, Make)
     - Added new options:
       - Allow to set CMake generator back to Xcode, no option for make
         thou.
       - Allow to build without reconfiguring CMake, improves build times.
         Unnecessary Cmake reconfigurations require full rebuild.
       - Allow to set build configuration for CMake
     - Reorganized targets into separate function to break "cd" dependency.
     - Reformat shell code.
     - Fix all warnings reported by ShellCheck linter tool.
     - Update run_gettext.sh to respect build dir in full mode.
    
    Signed-off-by: Dzmitry Neviadomski <nevack.d@gmail.com>

--- run_gettext.sh
+++ run_gettext.sh
@@ -8,18 +8,26 @@
 FULL_MODE=false
 for arg in "$@"
 do
-    if [ "$arg" == "--full" ]; then
+    if [ "$arg" = "--full" ]; then
         FULL_MODE=true
+        shift
+        FULL_MODE_BUILD_PATH="$1"
+        FULL_MODE_BUILD_CONFIG_SUBDIR="$2"
     fi
 done
 
 if $FULL_MODE; then
     xgettext --keyword=L --keyword=_L --keyword=_u8L --keyword=L_CONTEXT:1,2c --keyword=_L_PLURAL:1,2 --add-comments=TRN --from-code=UTF-8 --no-location --debug --boost -f ./localization/i18n/list.txt -o ./localization/i18n/OrcaSlicer.pot
-    ./build_arm64/src/hints/Release/hintsToPot.app/Contents/MacOS/hintsToPot ./resources ./localization/i18n
+    if [ -d "$FULL_MODE_BUILD_PATH/src/hints$FULL_MODE_BUILD_CONFIG_SUBDIR" ]; then
+        "$FULL_MODE_BUILD_PATH/src/hints$FULL_MODE_BUILD_CONFIG_SUBDIR/hintsToPot.app/Contents/MacOS/hintsToPot" ./resources ./localization/i18n
+    else 
+        echo "Cannot find $FULL_MODE_BUILD_PATH/src/hints$FULL_MODE_BUILD_CONFIG_SUBDIR dir"
+        echo "Skipping hintsToPot run!"
+    fi
 fi
 
 
-echo $PWD
+echo "$0: working dir = $PWD"
 pot_file="./localization/i18n/OrcaSlicer.pot"
 for dir in ./localization/i18n/*/
 do
@@ -28,10 +36,10 @@ do
 
     if [ -f "$dir/OrcaSlicer_${lang}.po" ]; then
         if $FULL_MODE; then
-            msgmerge -N -o $dir/OrcaSlicer_${lang}.po $dir/OrcaSlicer_${lang}.po $pot_file
-         fi
-        mkdir -p ./resources/i18n/${lang}/
-        msgfmt --check-format -o ./resources/i18n/${lang}/OrcaSlicer.mo $dir/OrcaSlicer_${lang}.po
+            msgmerge -N -o "$dir/OrcaSlicer_${lang}.po" "$dir/OrcaSlicer_${lang}.po" $pot_file
+        fi
+        mkdir -p "resources/i18n/${lang}"
+        msgfmt --check-format -o "resources/i18n/${lang}/OrcaSlicer.mo" "$dir/OrcaSlicer_${lang}.po"
         # Check the exit status of the msgfmt command
         if [ $? -ne 0 ]; then
             echo "Error encountered with msgfmt command for language ${lang}."
