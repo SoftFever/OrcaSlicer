1cd4d1ece1fe5ffd1e1e9231cc9550198503ed12
hu.wang <hu.wang@bambulab.com>
2024-03-19 20:24:34 +0800

    FIX:Prioritize selecting filament with smaller serial numbers in AMS
    JIRA: 5909
    Change-Id: If3030d4dd8d59af36bc1ae1801be1b89b0027a71

--- src/slic3r/GUI/DeviceManager.cpp
+++ src/slic3r/GUI/DeviceManager.cpp
@@ -944,7 +944,8 @@ int MachineObject::ams_filament_mapping(std::vector<FilamentInfo> filaments, std
                         picked_src_idx = i;
                         picked_tar_idx = j;
                     } 
-                    else if (min_val == distance_map[i][j].distance && filaments[i].filament_id == tray_filaments[j].filament_id) {
+                    else if (min_val == distance_map[i][j].distance&& filaments[picked_src_idx].filament_id!= tray_filaments[picked_tar_idx].filament_id && filaments[i].filament_id == tray_filaments[j].filament_id) {
+
                         picked_src_idx = i;
                         picked_tar_idx = j;
                     }
