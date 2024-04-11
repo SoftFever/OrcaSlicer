0d33f0e7fb5bee3ce2ada7a30475acdfd4e5db00
Jamin W. Collins <jamin.collins@gmail.com>
2024-04-11 10:28:15 -0600

    Fixes 3667: light/dark fixes for Unsaved Changes dialog
    Signed-off-by: Jamin W. Collins <jamin.collins@gmail.com>

--- src/slic3r/GUI/UnsavedChangesDialog.cpp
+++ src/slic3r/GUI/UnsavedChangesDialog.cpp
@@ -1523,7 +1523,6 @@ void UnsavedChangesDialog::update_list()
                      text_left->SetFont(::Label::Head_13);
                      text_left->Wrap(-1);
                      text_left->SetForegroundColour(GREY700);
-                     text_left->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
 
                      sizer_left_v->Add(text_left, 0, wxLEFT, 37);
 
@@ -1552,7 +1551,6 @@ void UnsavedChangesDialog::update_list()
                 text_left->SetFont(::Label::Body_13);
                 text_left->Wrap(-1);
                 text_left->SetForegroundColour(GREY700);
-                text_left->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
 
                 sizer_left_v->Add(text_left, 0, wxLEFT, 51 );
 
