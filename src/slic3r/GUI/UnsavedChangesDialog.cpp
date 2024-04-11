f47a2bff2b7390779f18d10a1bc9925311204f9f
Jamin W. Collins <jamin.collins@gmail.com>
2024-04-11 10:13:41 -0600

    UnsavedChangesDialog.cpp: trailing whitespace cleanup
    Signed-off-by: Jamin W. Collins <jamin.collins@gmail.com>

--- src/slic3r/GUI/UnsavedChangesDialog.cpp
+++ src/slic3r/GUI/UnsavedChangesDialog.cpp
@@ -606,7 +606,7 @@ DiffViewCtrl::DiffViewCtrl(wxWindow* parent, wxSize size)
     this->Bind(wxEVT_DATAVIEW_ITEM_VALUE_CHANGED, &DiffViewCtrl::item_value_changed, this);
 }
 
-DiffViewCtrl::~DiffViewCtrl() { 
+DiffViewCtrl::~DiffViewCtrl() {
     this->AssociateModel(nullptr);
     delete model;
 }
@@ -825,7 +825,7 @@ UnsavedChangesDialog::UnsavedChangesDialog(Preset::Type type, PresetCollection *
 
 inline int UnsavedChangesDialog::ShowModal()
 {
-    auto choise_key = "save_preset_choise"; 
+    auto choise_key = "save_preset_choise";
     auto choise     = wxGetApp().app_config->get(choise_key);
     long result = 0;
     if ((m_buttons & REMEMBER_CHOISE) && !choise.empty() && wxString(choise).ToLong(&result) && (1 << result) & (m_buttons | DONT_SAVE)) {
@@ -1936,7 +1936,7 @@ void DiffPresetDialog::create_buttons()
         if (e) e->Skip();
     };
 
-    // Transfer 
+    // Transfer
     m_transfer_btn = new Button(this, L("Transfer"));
     m_transfer_btn->SetBackgroundColor(btn_bg_green);
     m_transfer_btn->SetBorderColor(wxColour(0, 150, 136));
@@ -2010,7 +2010,7 @@ void DiffPresetDialog::create_edit_sizer()
             this->Layout();
     });
 
-    // Add Buttons 
+    // Add Buttons
     create_buttons();
 
     // Create and fill edit sizer
@@ -2251,7 +2251,7 @@ void DiffPresetDialog::update_tree()
     bool can_transfer_options = m_view_type == Preset::TYPE_INVALID || get_left_preset_name(m_view_type) != get_right_preset_name(m_view_type);
     m_edit_sizer->Show(show_tree && can_transfer_options);
     m_buttons->Show(m_edit_sizer->IsShown(size_t(0)) && m_use_for_transfer->GetValue());
-   
+
     update_bottom_info(bottom_info);
 
     if (tree_was_shown == m_tree->IsShown())
