a205eadd2a0fe6deb387caf8af2fa219ea1265ba
Gabriel E. Dvoranen <g_eugeniodvoranen@outlook.com>
2024-04-16 11:24:05 -0300

    Initial Add of Brazilian Portuguese translation
    Team:
    
    https://github.com/DvoraGames
    https://github.com/jcalmeida85
    https://github.com/ramorimdias
    https://github.com/Caminhaleonel

--- src/slic3r/GUI/GUI_App.cpp
+++ src/slic3r/GUI/GUI_App.cpp
@@ -6027,6 +6027,7 @@ wxString GUI_App::current_language_code_safe() const
 		{ "zh", 	"zh_CN", },
 		{ "ru", 	"ru_RU", },
         { "tr", 	"tr_TR", },
+        { "pt", 	"pt_BR", },
 	};
 	wxString language_code = this->current_language_code().BeforeFirst('_');
 	auto it = mapping.find(language_code);
