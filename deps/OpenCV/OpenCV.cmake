48bea59290bad52792fb456bb90454c8c86b5c51
zhou.xu <zhou.xu@bambulab.com>
2024-04-23 20:54:14 +0800

    NEW:add OpenCV.cmake in deps
    jira: none
    Change-Id: I1ae4a2bd5618e9e620b08a937904d6af5d00bc41

---
+++ deps/OpenCV/OpenCV.cmake
@@ -0,0 +1,72 @@
+if (MSVC)
+    set(_use_IPP "-DWITH_IPP=ON")
+else ()
+    set(_use_IPP "-DWITH_IPP=OFF")
+endif ()
+
+bambustudio_add_cmake_project(OpenCV
+    URL https://github.com/opencv/opencv/archive/refs/tags/4.6.0.tar.gz
+    URL_HASH SHA256=1ec1cba65f9f20fe5a41fda1586e01c70ea0c9a6d7b67c9e13edf0cfe2239277
+    CMAKE_ARGS
+    -DBUILD_SHARED_LIBS=0
+       -DBUILD_PERE_TESTS=OFF
+       -DBUILD_TESTS=OFF
+       -DBUILD_opencv_python_tests=OFF
+       -DBUILD_EXAMPLES=OFF
+       -DBUILD_JASPER=OFF
+       -DBUILD_JAVA=OFF
+       -DBUILD_JPEG=ON
+       -DBUILD_APPS_LIST=version
+       -DBUILD_opencv_apps=OFF
+       -DBUILD_opencv_java=OFF
+       -DBUILD_OPENEXR=OFF
+       -DBUILD_PNG=ON
+       -DBUILD_TBB=OFF
+       -DBUILD_WEBP=OFF
+       -DBUILD_ZLIB=OFF
+       -DWITH_1394=OFF
+       -DWITH_CUDA=OFF
+       -DWITH_EIGEN=ON
+       ${_use_IPP}
+       -DWITH_ITT=OFF
+       -DWITH_FFMPEG=OFF
+       -DWITH_GPHOTO2=OFF
+       -DWITH_GSTREAMER=OFF
+       -DOPENCV_GAPI_GSTREAMER=OFF
+       -DWITH_GTK_2_X=OFF
+       -DWITH_JASPER=OFF
+       -DWITH_LAPACK=OFF
+       -DWITH_MATLAB=OFF
+       -DWITH_MFX=OFF
+       -DWITH_DIRECTX=OFF
+       -DWITH_DIRECTML=OFF
+       -DWITH_OPENCL=OFF
+       -DWITH_OPENCL_D3D11_NV=OFF
+       -DWITH_OPENCLAMDBLAS=OFF
+       -DWITH_OPENCLAMDFFT=OFF
+       -DWITH_OPENEXR=OFF
+       -DWITH_OPENJPEG=OFF
+       -DWITH_QUIRC=OFF
+       -DWITH_VTK=OFF
+       -DWITH_WEBP=OFF
+       -DENABLE_PRECOMPILED_HEADERS=OFF
+       -DINSTALL_TESTS=OFF
+       -DINSTALL_C_EXAMPLES=OFF
+       -DINSTALL_PYTHON_EXAMPLES=OFF
+       -DOPENCV_GENERATE_SETUPVARS=OFF
+       -DOPENCV_INSTALL_FFMPEG_DOWNLOAD_SCRIPT=OFF
+       -DBUILD_opencv_python2=OFF
+       -DBUILD_opencv_python3=OFF
+       -DWITH_OPENVINO=OFF
+       -DWITH_INF_ENGINE=OFF
+       -DWITH_NGRAPH=OFF
+       -DBUILD_WITH_STATIC_CRT=OFF#set /MDd /MD
+       -DBUILD_LIST=core,imgcodecs,imgproc,world
+       -DBUILD_opencv_highgui=OFF
+       -DWITH_ADE=OFF
+       -DBUILD_opencv_world=ON
+       -DWITH_PROTOBUF=OFF
+       -DWITH_WIN32UI=OFF
+       -DHAVE_WIN32UI=FALSE
+)
+
