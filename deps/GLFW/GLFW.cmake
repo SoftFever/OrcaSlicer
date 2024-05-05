dbfb5d829790675e4563f3a80363b88efe378d1d
Jamin W. Collins <jamin.collins@gmail.com>
2024-05-05 13:58:07 -0600

    update GLFW to 3.4, supports both X11 and Wayland
    Signed-off-by: Jamin W. Collins <jamin.collins@gmail.com>

--- deps/GLFW/GLFW.cmake
+++ deps/GLFW/GLFW.cmake
@@ -6,22 +6,15 @@ else()
     set(_build_static ON)
 endif()
 
-if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    set(_glfw_use_wayland "-DGLFW_USE_WAYLAND=ON")
-else()
-    set(_glfw_use_wayland "-DGLFW_USE_WAYLAND=FF")
-endif()
-
 orcaslicer_add_cmake_project(GLFW
-    URL https://github.com/glfw/glfw/archive/refs/tags/3.3.7.zip
-    URL_HASH SHA256=e02d956935e5b9fb4abf90e2c2e07c9a0526d7eacae8ee5353484c69a2a76cd0
+    URL https://github.com/glfw/glfw/archive/refs/tags/3.4.zip
+    URL_HASH SHA256=a133ddc3d3c66143eba9035621db8e0bcf34dba1ee9514a9e23e96afd39fd57a
     #DEPENDS dep_Boost
     CMAKE_ARGS
-        -DBUILD_SHARED_LIBS=${_build_shared} 
+        -DBUILD_SHARED_LIBS=${_build_shared}
         -DGLFW_BUILD_DOCS=OFF
         -DGLFW_BUILD_EXAMPLES=OFF
 	-DGLFW_BUILD_TESTS=OFF
-	${_glfw_use_wayland}
 )
 
 if (MSVC)
