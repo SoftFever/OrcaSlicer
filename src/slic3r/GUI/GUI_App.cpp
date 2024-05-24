f7b605088b740b1e9da0530d3ad36422501f28af
SoftFever <softfeverever@gmail.com>
2024-05-24 19:42:32 +0800

    fix an issue that orcaslicer:// didn't work on Mac

--- src/slic3r/GUI/GUI_App.cpp
+++ src/slic3r/GUI/GUI_App.cpp
@@ -5903,7 +5903,7 @@ void GUI_App::MacOpenURL(const wxString& url)
                 m_download_file_url = download_file_url;
             }
         }
-    } else if (boost::starts_with(url, "prusasliceropen://"))
+    } else if (boost::starts_with(url, "orcaslicer://"))
         start_download(boost::nowide::narrow(url));
 }
 
