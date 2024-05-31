77b4a628741fd518a4555e10deafa67078be6794
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2024-05-31 13:25:10 +0100

    [#2025] Added toolbar icon fro "Duplicate Current Plate" action.

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -2949,10 +2949,10 @@ Plater::priv::priv(Plater *q, MainFrame *main_frame)
         view3D_canvas->Bind(EVT_GLCANVAS_OBJECT_SELECT, &priv::on_object_select, this);
         view3D_canvas->Bind(EVT_GLCANVAS_RIGHT_CLICK, &priv::on_right_click, this);
         //BBS: add part plate related logic
-        // view3D_canvas->Bind(EVT_GLCANVAS_DUPLICATE_PLATE, [this](SimpleEvent& evt){
-        //     this->q->set_prepare_state(Job::PREPARE_STATE_DEFAULT);
-        //     this->q->duplicate_plate();
-        // });
+        view3D_canvas->Bind(EVT_GLCANVAS_DUPLICATE_PLATE, [this](SimpleEvent& evt){
+            this->q->set_prepare_state(Job::PREPARE_STATE_DEFAULT);
+            this->q->duplicate_plate();
+        });
         view3D_canvas->Bind(EVT_GLCANVAS_PLATE_RIGHT_CLICK, &priv::on_plate_right_click, this);
         view3D_canvas->Bind(EVT_GLCANVAS_REMOVE_OBJECT, [q](SimpleEvent&) { q->remove_selected(); });
         view3D_canvas->Bind(EVT_GLCANVAS_ARRANGE, [this](SimpleEvent& evt) {
@@ -3006,6 +3006,10 @@ Plater::priv::priv(Plater *q, MainFrame *main_frame)
 
         view3D_canvas->Bind(EVT_GLTOOLBAR_ADD_PLATE, &priv::on_action_add_plate, this);
         view3D_canvas->Bind(EVT_GLTOOLBAR_DEL_PLATE, &priv::on_action_del_plate, this);
+        view3D_canvas->Bind(EVT_GLTOOLBAR_DUPLICATE_PLATE, [this](SimpleEvent& evt){
+            this->q->set_prepare_state(Job::PREPARE_STATE_DEFAULT);
+            this->q->duplicate_plate();
+        });
         view3D_canvas->Bind(EVT_GLTOOLBAR_ORIENT, [this](SimpleEvent&) {
             //BBS arrage from EVT set default state.
             this->q->set_prepare_state(Job::PREPARE_STATE_DEFAULT);
