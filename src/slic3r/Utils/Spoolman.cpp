b5c3a8f54675c08eb5e9ebb5fbfaeb8fe1a18253
Ocraftyone <Ocraftyone@users.noreply.github.com>
2024-06-26 23:17:09 -0400

    Refactoring

--- src/slic3r/Utils/Spoolman.cpp
+++ src/slic3r/Utils/Spoolman.cpp
@@ -265,11 +265,11 @@ void Spoolman::update_visible_spool_statistics(bool clear_cache)
     PresetCollection& filaments    = preset_bundle->filaments;
 
     // Clear the cache so that it can be repopulated with the correct info
-    if (clear_cache) Spoolman::get_instance()->clear();
-    if (printers.get_edited_preset().spoolman_enabled() && Spoolman::is_server_valid()) {
+    if (clear_cache) get_instance()->clear();
+    if (printers.get_edited_preset().spoolman_enabled() && is_server_valid()) {
         for (auto item : filaments.get_visible()) {
             if (item->is_user() && item->spoolman_enabled()) {
-                if (auto res = Spoolman::update_filament_preset_from_spool(item, true, true); res.has_failed())
+                if (auto res = update_filament_preset_from_spool(item, true, true); res.has_failed())
                     BOOST_LOG_TRIVIAL(error) << __FUNCTION__ << ": Failed to update spoolman statistics with the following error: "
                                              << res.build_single_line_message() << "Spool ID: " << item->config.opt_int("spoolman_spool_id");
             }
