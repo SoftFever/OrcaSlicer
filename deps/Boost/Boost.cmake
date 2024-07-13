6e5a01f7f93d7b11ebb9a81943d3c9689339f4e7
Aidan <aidanzcase@gmail.com>
2024-07-13 13:19:23 -0500

    build without openmpi and numeric

--- deps/Boost/Boost.cmake
+++ deps/Boost/Boost.cmake
@@ -15,12 +15,11 @@ orcaslicer_add_cmake_project(Boost
     URL_HASH SHA256=4d27e9efed0f6f152dc28db6430b9d3dfb40c0345da7342eaa5a987dde57bd95
     LIST_SEPARATOR |
     CMAKE_ARGS
-        -DBOOST_EXCLUDE_LIBRARIES:STRING=contract|fiber|numpy|stacktrace|wave|test
+        -DBOOST_EXCLUDE_LIBRARIES:STRING=contract|fiber|numpy|stacktrace|wave|test|numeric
         -DBOOST_LOCALE_ENABLE_ICU:BOOL=OFF # do not link to libicu, breaks compatibility between distros
         -DBUILD_TESTING:BOOL=OFF
-        -DBOOST_ENABLE_MPI=ON
         "${_context_abi_line}"
         "${_context_arch_line}"
 )
 
-set(DEP_Boost_DEPENDS ZLIB OpenMPI)
+set(DEP_Boost_DEPENDS ZLIB)
