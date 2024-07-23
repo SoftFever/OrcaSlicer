4f6c17f5681495db6b7a1c8fcd404e5330f3d945
yw4z <ywsyildiz@gmail.com>
2024-07-23 13:00:21 +0300

    Color fixes (#5712)
    * Flushing volumes button background color for active state
    
    * Sliced plates bar > Use different colors on plate numbers for light / dark theme
    
    * Remove 3D navigator background
    
    * Device & Project Tab > Sidebar > Selected tab background color
    
    * Merge branch 'SoftFever:main' into color-fixes-3
    
    * Correct sidebar button color for light mode for bbl monitor / calibration and project page
    
    * Homepage fixes
    
    * Fix parameters group title color too bright on dark theme
    
    * Search popup hovered item background color
    
    * Sidebar > Search box > match border color with other input/combo boxes
    
    * fix indents
    
    * Add descriptions to statecolors
    
    * Paint gizmos > remove background of non active Tool / Brush buttons on dark mode
    
    * Merge branch 'SoftFever:main' into color-fixes-3
    
    * Top bar / Main tab bar button hover background
    
    * Fix broken color change from last merges
    
    * Gizmos selected text background
    
    * Keyboard shortcuts window selected tab bg color
    
    * About page link color
    
    * Project page fixes
    
    * match disabled text color on combo boxes with input boxes
    
    * Use better background color for disabled elements on dark mode
    
    * match all colors for disabled elements
    
    * BBL > Monitor tab > Add printer text and icon not visible on dark theme
    
    * even darker bg color for homepage thumbnails
    
    * Sidebar arrow not visible
    
    * Better bg color for row highlighting
    
    * match style of gizmo combo box
    
    * Merge branch 'SoftFever:main' into color-fixes-3
    
    * Revert changes for main tab bar background color of button while hover

--- src/slic3r/GUI/Widgets/TabCtrl.cpp
+++ src/slic3r/GUI/Widgets/TabCtrl.cpp
@@ -304,7 +304,7 @@ void TabCtrl::doRender(wxDC& dc)
 #else
     dc.SetPen(wxPen(border_color.colorForStates(states), border_width));
     dc.DrawLine(0, size.y - BS2, size.x, size.y - BS2);
-    wxColour c(0xf2, 0x75, 0x4e, 0xff);
+    wxColour c("#009688"); // ORCA selected tab underline stroke color
     dc.SetPen(wxPen(c, 1));
     dc.SetBrush(c);
     dc.DrawRoundedRectangle(x1 - radius, size.y - BS2 - border_width * 3, x2 + radius * 2 - x1, border_width * 3, radius);
