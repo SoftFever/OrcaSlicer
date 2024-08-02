94abdf8088c96d91e808bc8a80a4e4e1e7a727cf
zhimin.zeng <zhimin.zeng@bambulab.com>
2024-08-02 14:48:56 +0800

    FIX: only single extruder printer display the icon
    of FilamentSettingDialog
    jira: 7714
    
    Change-Id: I5d6eca802b9be1fcdeaa28d9e4fbc4862d8c8378
    (cherry picked from commit b7176bfdd14c88362058d374c8573d1bb22d6a03)

--- src/slic3r/GUI/PartPlate.cpp
+++ src/slic3r/GUI/PartPlate.cpp
@@ -1034,12 +1034,12 @@ void PartPlate::render_icons(bool bottom, bool only_name, int hover_id)
             }
 
 			int extruder_count = wxGetApp().preset_bundle->get_printer_extruder_count();
-			if (extruder_count == 2) {
-				if (hover_id == PLATE_FILAMENT_MAP_ID)
-					render_icon_texture(m_plate_filament_map_icon.model, m_partplate_list->m_plate_set_filament_map_hovered_texture);
-				else
-					render_icon_texture(m_plate_filament_map_icon.model, m_partplate_list->m_plate_set_filament_map_texture);
-			}
+            if (extruder_count == 2) {
+                if (hover_id == PLATE_FILAMENT_MAP_ID)
+                    render_icon_texture(m_plate_filament_map_icon.model, m_partplate_list->m_plate_set_filament_map_hovered_texture);
+                else
+                    render_icon_texture(m_plate_filament_map_icon.model, m_partplate_list->m_plate_set_filament_map_texture);
+            }
 
 			if (hover_id == 6) {
                 render_icon_texture(m_plate_name_edit_icon.model, m_partplate_list->m_plate_name_edit_hovered_texture);
