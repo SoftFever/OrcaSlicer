543e0d684998ae6ec033157f1b96656ccf411718
Ocraftyone <ocraftyone@users.noreply.github.com>
2024-09-12 09:28:08 +0000

    Revert Qhull changes
    The root cause for not being able to find the debug version of Qhull was solved in SoftFever/OrcaSlicer#6438

--- deps/Qhull/Qhull.cmake
+++ deps/Qhull/Qhull.cmake
@@ -1,15 +1,9 @@
 include(GNUInstallDirs)
-
-if(LINUX AND CMAKE_BUILD_TYPE STREQUAL "Debug") # compiling in debug mode on linux causes errors
-    set(_build_args "-DCMAKE_BUILD_TYPE=RelWithDebInfo")
-endif()
-
 orcaslicer_add_cmake_project(Qhull
     URL "https://github.com/qhull/qhull/archive/v8.0.1.zip"
     URL_HASH SHA256=5287f5edd6a0372588f5d6640799086a4033d89d19711023ef8229dd9301d69b
     CMAKE_ARGS 
         -DINCLUDE_INSTALL_DIR=${CMAKE_INSTALL_INCLUDEDIR}
-        ${_build_args}
 )
 
 if (MSVC)
