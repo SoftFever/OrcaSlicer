af118e408aa7890264970af9b98318e351ea03ba
Ocraftyone <ocraftyone@users.noreply.github.com>
2024-09-12 20:11:09 +0000

    Revert all BuildLinux.sh changes

--- BuildLinux.sh
+++ BuildLinux.sh
@@ -25,12 +25,11 @@ function check_available_memory_and_disk() {
 }
 
 function usage() {
-    echo "Usage: ./BuildLinux.sh [-1][-b][-c][-f][-d][-i][-r][-s][-u]"
+    echo "Usage: ./BuildLinux.sh [-1][-b][-c][-d][-i][-r][-s][-u]"
     echo "   -1: limit builds to 1 core (where possible)"
     echo "   -b: build in debug mode"
     echo "   -c: force a clean build"
-    echo "   -f: force a deps build (even if they are already built)"
-    echo "   -d: build deps (if needed)"
+    echo "   -d: build deps (optional)"
     echo "   -h: this help output"
     echo "   -i: Generate appimage (optional)"
     echo "   -r: skip ram and disk checks (low ram compiling)"
@@ -40,23 +39,17 @@ function usage() {
     echo "   and then './BuildLinux.sh -dsi'"
 }
 
-PRESET="linux-release"
-BUILD_DEPS="0"
-FORCE_BUILD_DEPS="0"
-while getopts ":1bcfdghirsu" opt; do
+unset name
+while getopts ":1bcdghirsu" opt; do
   case ${opt} in
     1 )
         export CMAKE_BUILD_PARALLEL_LEVEL=1
         ;;
     b )
         BUILD_DEBUG="1"
-        PRESET="linux-debug"
         ;;
     c )
         CLEAN_BUILD=1
-        ;;
-    f )
-        FORCE_BUILD_DEPS="1"
         ;;
     d )
         BUILD_DEPS="1"
@@ -68,8 +61,8 @@ while getopts ":1bcfdghirsu" opt; do
         BUILD_IMAGE="1"
         ;;
     r )
-	      SKIP_RAM_CHECK="1"
-	      ;;
+	    SKIP_RAM_CHECK="1"
+	;;
     s )
         BUILD_ORCA="1"
         ;;
@@ -119,40 +112,81 @@ then
     check_available_memory_and_disk
 fi
 
-if [[ -n "${CLEAN_BUILD}" ]]
+if [[ -n "${BUILD_DEPS}" ]]
 then
-  CONFIG_ARGS="--fresh"
+    echo "Configuring dependencies..."
+    BUILD_ARGS="-DDEP_WX_GTK3=ON"
+    if [[ -n "${CLEAN_BUILD}" ]]
+    then
+        rm -fr deps/build
+    fi
+    if [ ! -d "deps/build" ]
+    then
+        mkdir deps/build
+    fi
+    if [[ -n "${BUILD_DEBUG}" ]]
+    then
+        # have to build deps with debug & release or the cmake won't find everything it needs
+        if [ ! -d "deps/build/release" ]
+        then
+            mkdir deps/build/release
+        fi
+        cmake -S deps -B deps/build/release -G Ninja -DDESTDIR="${PWD}/deps/build/destdir" -DDEP_DOWNLOAD_DIR="${PWD}/deps/DL_CACHE" ${BUILD_ARGS}
+        cmake --build deps/build/release
+        BUILD_ARGS="${BUILD_ARGS} -DCMAKE_BUILD_TYPE=Debug"
+    fi
+
+    echo "cmake -S deps -B deps/build -G Ninja ${BUILD_ARGS}"
+    cmake -S deps -B deps/build -G Ninja ${BUILD_ARGS}
+    cmake --build deps/build
 fi
 
-cmake --preset ${PRESET} -DBUILD_DEPS=${BUILD_DEPS} -DCLEAN_DEPS=${CLEAN_BUILD} -DFORCE_DEPS=${FORCE_BUILD_DEPS} ${CONFIG_ARGS}
-
-# get the build directory's name from the cmake cache
-BUILD_DIR_NAME=$(cmake --preset ${PRESET} -LA -N | grep BIN_DIR_NAME | cut -d "=" -f2)
 
 if [[ -n "${BUILD_ORCA}" ]]
 then
+    echo "Configuring OrcaSlicer..."
     if [[ -n "${CLEAN_BUILD}" ]]
     then
-      echo "Cleaning OrcaSlicer ..."
-      cmake --build --preset ${PRESET} --target clean
+        rm -fr build
     fi
+    BUILD_ARGS=""
+    if [[ -n "${FOUND_GTK3_DEV}" ]]
+    then
+        BUILD_ARGS="-DSLIC3R_GTK=3"
+    fi
+    if [[ -n "${BUILD_DEBUG}" ]]
+    then
+        BUILD_ARGS="${BUILD_ARGS} -DCMAKE_BUILD_TYPE=Debug -DBBL_INTERNAL_TESTING=1"
+    else
+        BUILD_ARGS="${BUILD_ARGS} -DBBL_RELEASE_TO_PUBLIC=1 -DBBL_INTERNAL_TESTING=0"
+    fi
+    echo -e "cmake -S . -B build -G Ninja -DCMAKE_PREFIX_PATH="${PWD}/deps/build/destdir/usr/local" -DSLIC3R_STATIC=1 ${BUILD_ARGS}"
+    cmake -S . -B build -G Ninja \
+        -DCMAKE_PREFIX_PATH="${PWD}/deps/build/destdir/usr/local" \
+        -DSLIC3R_STATIC=1 \
+        -DORCA_TOOLS=ON \
+        ${BUILD_ARGS}
+    echo "done"
     echo "Building OrcaSlicer ..."
-    cmake --build --preset ${PRESET}
+    cmake --build build --target OrcaSlicer
     echo "Building OrcaSlicer_profile_validator .."
-    cmake --build ${BUILD_DIR_NAME} --target OrcaSlicer_profile_validator
+    cmake --build build --target OrcaSlicer_profile_validator
     ./run_gettext.sh
+    echo "done"
 fi
 
-if [[ -e ${ROOT}/${BUILD_DIR_NAME}/src/BuildLinuxImage.sh ]]; then
+if [[ -e ${ROOT}/build/src/BuildLinuxImage.sh ]]; then
 # Give proper permissions to script
-chmod 755 ${ROOT}/${BUILD_DIR_NAME}/src/BuildLinuxImage.sh
+chmod 755 ${ROOT}/build/src/BuildLinuxImage.sh
 
-    pushd ${BUILD_DIR_NAME} # cmake exported env variable
+echo "[9/9] Generating Linux app..."
+    pushd build
         if [[ -n "${BUILD_IMAGE}" ]]
         then
-            ${ROOT}/${BUILD_DIR_NAME}/src/BuildLinuxImage.sh -i
+            ${ROOT}/build/src/BuildLinuxImage.sh -i
         else
-            ${ROOT}/${BUILD_DIR_NAME}/src/BuildLinuxImage.sh
+            ${ROOT}/build/src/BuildLinuxImage.sh
         fi
     popd
+echo "done"
 fi
