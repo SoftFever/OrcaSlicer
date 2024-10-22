91fa936f326cedfed0af4d9e36a7540759af5e8f
Kenneth Jiang <kenneth.jiang@gmail.com>
2024-10-22 13:24:54 -0700

    Sign up/in page looks good now

---
+++ resources/web/jusprin/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../homepage/**/*.html',
+    '../homepage/**/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#694b7c',
+      },
+    },
+  },
+  plugins: [],
+}
+
