3af2cfc5b504078575f8e8105cfddfd73feb3465
vovodroid <vovodroid@users.noreply.github.com>
2024-10-24 09:42:25 +0300

    Solid infill flow ratio

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -5182,6 +5182,8 @@ std::string GCode::_extrude(const ExtrusionPath &path, std::string description, 
         _mm3_per_mm *= m_config.top_solid_infill_flow_ratio;
     else if (path.role() == erBottomSurface)
         _mm3_per_mm *= m_config.bottom_solid_infill_flow_ratio;
+    else if (path.role() == erSolidInfill)
+        _mm3_per_mm *= m_config.solid_infill_flow_ratio;
     else if (path.role() == erInternalBridgeInfill)
         _mm3_per_mm *= m_config.internal_bridge_flow;
     else if(sloped)
