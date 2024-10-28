995c60bb2d9a813750aba06674e31a5257ed60e8
Kenneth Jiang <kenneth.jiang@gmail.com>
2024-10-27 21:38:22 -0700

    Local page that auto-redirect to server, or show error message if not connected

--- src/slic3r/GUI/JusPrin/ChatConfigPanel.cpp
+++ src/slic3r/GUI/JusPrin/ChatConfigPanel.cpp
@@ -79,7 +79,7 @@ ChatConfigPanel::~ChatConfigPanel()
 
 void ChatConfigPanel::load_url()
 {
-    wxString url = wxString::Format("file://%s/web/jusprin/chat_config_test.html", from_u8(resources_dir()));
+    wxString url = wxString::Format("file://%s/web/jusprin/jusprin_chat_preload.html", from_u8(resources_dir()));
     if (m_browser == nullptr)
         return;
 
