f1d82d02f2dde610ca22422f3cc646439fbb4a27
zheng zihui <flying@good.com>
2024-11-05 08:37:49 +0800

    首页颜色修改

--- src/slic3r/GUI/Widgets/SideButton.cpp
+++ src/slic3r/GUI/Widgets/SideButton.cpp
@@ -29,8 +29,8 @@ SideButton::SideButton(wxWindow* parent, wxString text, wxString icon, long stly
 
 
     border_color.append(0x6B6B6B, StateColor::Disabled);
-    border_color.append(wxColour(0, 137, 123), StateColor::Pressed);
-    border_color.append(wxColour(38, 166, 154), StateColor::Hovered);
+    border_color.append(0x5a416b, StateColor::Pressed);
+    border_color.append(0x7d5c93, StateColor::Hovered);
     border_color.append(0x694b7c, StateColor::Normal);
     border_color.setTakeFocusedAsHovered(false);
 
@@ -40,8 +40,8 @@ SideButton::SideButton(wxWindow* parent, wxString text, wxString icon, long stly
     text_color.append(0xFEFEFE, StateColor::Normal);
 
     background_color.append(0x6B6B6B, StateColor::Disabled);
-    background_color.append(wxColour(0, 137, 123), StateColor::Pressed);
-    background_color.append(wxColour(38, 166, 154), StateColor::Hovered);
+    background_color.append(0x5a416b, StateColor::Pressed);
+    background_color.append(0x7d5c93, StateColor::Hovered);
     background_color.append(0x694b7c, StateColor::Normal);
     background_color.setTakeFocusedAsHovered(false);
 
