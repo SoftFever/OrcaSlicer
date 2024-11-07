57cdb867ce86c6266eab794c5f8924de0f454d97
Dima Buzdyk <dima.buzdyk@gmail.com>
2024-11-08 01:23:43 +0500

    gui: option to use system font face
    Add hidden option to force use of system font face.
    Option may be enabled manually adding option to the "app" section
    of OrcaSlicer.conf:
    
        "use_system_fonts": true,

--- src/slic3r/GUI/Widgets/Label.cpp
+++ src/slic3r/GUI/Widgets/Label.cpp
@@ -16,17 +16,9 @@ wxFont Label::sysFont(int size, bool bold)
     size = size * 4 / 5;
 #endif
 
-    wxString face = "HarmonyOS Sans SC";
-
-    // Check if the current locale is Korean
-    if (wxLocale::GetSystemLanguage() == wxLANGUAGE_KOREAN) {
-        face = "NanumGothic";
-    }
-
-    wxFont font{size, wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, bold ? wxFONTWEIGHT_BOLD : wxFONTWEIGHT_NORMAL, false, face};
-    font.SetFaceName(face);
+    wxFont font{size, wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, bold ? wxFONTWEIGHT_BOLD : wxFONTWEIGHT_NORMAL, false, fontFace};
     if (!font.IsOk()) {
-      BOOST_LOG_TRIVIAL(warning) << boost::format("Can't find %1% font") % face;
+      BOOST_LOG_TRIVIAL(warning) << boost::format("Can't find %1% font") % fontFace.ToStdString();
       font = wxSystemSettings::GetFont(wxSYS_DEFAULT_GUI_FONT);
       BOOST_LOG_TRIVIAL(warning) << boost::format("Use system font instead: %1%") % font.GetFaceName();
       if (bold)
@@ -58,28 +50,40 @@ wxFont Label::Body_10;
 wxFont Label::Body_9;
 wxFont Label::Body_8;
 
-void Label::initSysFont()
+wxString Label::fontFace{"HarmonyOS Sans SC"};
+
+void Label::initSysFont(bool useSystemFonts)
 {
+    if (useSystemFonts) {
+        fontFace.assign(wxSystemSettings::GetFont(wxSYS_DEFAULT_GUI_FONT).GetFaceName());
+        BOOST_LOG_TRIVIAL(info) << "Use system fonts";
+    } else {
 #if defined(__linux__) || defined(_WIN32)
-    const std::string &resource_path = Slic3r::resources_dir();
-    wxString font_path = wxString::FromUTF8(resource_path + "/fonts/HarmonyOS_Sans_SC_Bold.ttf");
-    bool result = wxFont::AddPrivateFont(font_path);
-    // BOOST_LOG_TRIVIAL(info) << boost::format("add font of HarmonyOS_Sans_SC_Bold returns %1%")%result;
-    printf("add font of HarmonyOS_Sans_SC_Bold returns %d\n", result);
-    font_path = wxString::FromUTF8(resource_path + "/fonts/HarmonyOS_Sans_SC_Regular.ttf");
-    result = wxFont::AddPrivateFont(font_path);
-    // BOOST_LOG_TRIVIAL(info) << boost::format("add font of HarmonyOS_Sans_SC_Regular returns %1%")%result;
-    printf("add font of HarmonyOS_Sans_SC_Regular returns %d\n", result);
-    // Adding NanumGothic Regular and Bold
-    font_path = wxString::FromUTF8(resource_path + "/fonts/NanumGothic-Regular.ttf");
-    result = wxFont::AddPrivateFont(font_path);
-    // BOOST_LOG_TRIVIAL(info) << boost::format("add font of NanumGothic-Regular returns %1%")%result;
-    printf("add font of NanumGothic-Regular returns %d\n", result);
-    font_path = wxString::FromUTF8(resource_path + "/fonts/NanumGothic-Bold.ttf");
-    result = wxFont::AddPrivateFont(font_path);
-    // BOOST_LOG_TRIVIAL(info) << boost::format("add font of NanumGothic-Bold returns %1%")%result;
-    printf("add font of NanumGothic-Bold returns %d\n", result);
+        const std::string &resource_path = Slic3r::resources_dir();
+        wxString font_path = wxString::FromUTF8(resource_path + "/fonts/HarmonyOS_Sans_SC_Bold.ttf");
+        bool result = wxFont::AddPrivateFont(font_path);
+        // BOOST_LOG_TRIVIAL(info) << boost::format("add font of HarmonyOS_Sans_SC_Bold returns %1%")%result;
+        printf("add font of HarmonyOS_Sans_SC_Bold returns %d\n", result);
+        font_path = wxString::FromUTF8(resource_path + "/fonts/HarmonyOS_Sans_SC_Regular.ttf");
+        result = wxFont::AddPrivateFont(font_path);
+        // BOOST_LOG_TRIVIAL(info) << boost::format("add font of HarmonyOS_Sans_SC_Regular returns %1%")%result;
+        printf("add font of HarmonyOS_Sans_SC_Regular returns %d\n", result);
+        // Adding NanumGothic Regular and Bold
+        font_path = wxString::FromUTF8(resource_path + "/fonts/NanumGothic-Regular.ttf");
+        result = wxFont::AddPrivateFont(font_path);
+        // BOOST_LOG_TRIVIAL(info) << boost::format("add font of NanumGothic-Regular returns %1%")%result;
+        printf("add font of NanumGothic-Regular returns %d\n", result);
+        font_path = wxString::FromUTF8(resource_path + "/fonts/NanumGothic-Bold.ttf");
+        result = wxFont::AddPrivateFont(font_path);
+        // BOOST_LOG_TRIVIAL(info) << boost::format("add font of NanumGothic-Bold returns %1%")%result;
+        printf("add font of NanumGothic-Bold returns %d\n", result);
 #endif
+        // Check if the current locale is Korean
+        if (wxLocale::GetSystemLanguage() == wxLANGUAGE_KOREAN) {
+            fontFace.assign("NanumGothic");
+        }
+    }
+
     Head_48 = Label::sysFont(48, true);
     Head_32 = Label::sysFont(32, true);
     Head_24 = Label::sysFont(24, true);
