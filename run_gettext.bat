41bc62bf6eedf2beea98a6562279bab44cb3f4a1
zheng zihui <flying@good.com>
2024-12-13 22:05:40 +0800

    <fix>fix多语言

--- run_gettext.bat
+++ run_gettext.bat
@@ -1,5 +1,5 @@
 @echo off
-REM OrcaSlicer gettext
+REM JusPrin gettext
 REM Created by SoftFever on 27/5/23.
 
 REM Check for --full argument
@@ -9,12 +9,12 @@ for %%a in (%*) do (
 )
 
 if %FULL_MODE%==1 (
-    .\tools\xgettext.exe --keyword=L --keyword=_L --keyword=_u8L --keyword=L_CONTEXT:1,2c --keyword=_L_PLURAL:1,2 --add-comments=TRN --from-code=UTF-8 --no-location --debug --boost -f ./localization/i18n/list.txt -o ./localization/i18n/OrcaSlicer.pot
+    .\tools\xgettext.exe --keyword=L --keyword=_L --keyword=_u8L --keyword=L_CONTEXT:1,2c --keyword=_L_PLURAL:1,2 --add-comments=TRN --from-code=UTF-8 --no-location --debug --boost -f ./localization/i18n/list.txt -o ./localization/i18n/JusPrin.pot
     python scripts/HintsToPot.py ./resources ./localization/i18n
 )
 REM Print the current directory
 echo %cd%
-set pot_file="./localization/i18n/OrcaSlicer.pot"
+set pot_file="./localization/i18n/JusPrin.pot"
 
 REM Run the script for each .po file
 for /r "./localization/i18n/" %%f in (*.po) do (
@@ -26,7 +26,7 @@ goto :eof
     set "file=%~1"
     set "dir=%~dp1"
     set "name=%~n1"
-    set "lang=%name:OrcaSlicer_=%"
+    set "lang=%name:JusPrin_=%"
     if %FULL_MODE%==1 (
         .\tools\msgmerge.exe -N -o "%file%" "%file%" "%pot_file%"
     )
