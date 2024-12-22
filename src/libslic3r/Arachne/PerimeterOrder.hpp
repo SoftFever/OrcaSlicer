47ec9b9b0692d094f534ee891f30e00198be1338
Noisyfox <timemanager.rick@gmail.com>
2024-12-22 17:53:03 +0800

    SPE-1950: Optimization of computation complexity of perimeter ordering for Arachne generator.
    The previous implementation during the grouping of perimeters using depth-first searches unnecessarily searched nodes that had no impact on grouping, which significantly increased the search space.
    
    Cherry-picked from prusa3d/PrusaSlicer@86309ba939c17ae6e35f0cecb5db6f3032c71fe8
    
    Co-authored-by: Lukáš Hejl <hejl.lukas@gmail.com>

--- src/libslic3r/Arachne/PerimeterOrder.hpp
+++ src/libslic3r/Arachne/PerimeterOrder.hpp
@@ -1,7 +1,14 @@
 #ifndef slic3r_GCode_PerimeterOrder_hpp_
 #define slic3r_GCode_PerimeterOrder_hpp_
 
-#include <Arachne/utils/ExtrusionLine.hpp>
+#include <stddef.h>
+#include <limits>
+#include <vector>
+#include <cstddef>
+
+#include "libslic3r/Arachne/utils/ExtrusionLine.hpp"
+#include "libslic3r/BoundingBox.hpp"
+#include "libslic3r/Polygon.hpp"
 
 namespace Slic3r::Arachne::PerimeterOrder {
 
