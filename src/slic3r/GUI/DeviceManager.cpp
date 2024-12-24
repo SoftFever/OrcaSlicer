13d9fdb410354b488364875689e82c080b642c32
xin.zhang <xin.zhang@bambulab.com>
2024-12-24 17:26:49 +0800

    FIX:can not set chamber temp if there are Low temperature filament
    jira: [STUDIO-9343]
    Change-Id: I2af66de2a7683fe61053d0d2d0535cabc2da7477
    (cherry picked from commit 6617aa81c88e608cb9f20bbf185c4f81eb0516b2)

--- src/slic3r/GUI/DeviceManager.cpp
+++ src/slic3r/GUI/DeviceManager.cpp
@@ -3361,8 +3361,7 @@ int MachineObject::parse_json(std::string payload, bool key_field_only)
                         if (jj["errno"].is_number()) {
                             wxString text;
                             if (jj["errno"].get<int>() == -2) {
-                                 text = _L("Low temperature filament (PLA/PETG/TPU) is loaded in the extruder. "
-                                           "In order to avoid extruder clogging, it is not allowed to set the chamber temperature above 45\u2103." /* 45°C */);
+                                 text = _L("Low temperature filament(PLA/PETG/TPU) is loaded in the extruder.In order to avoid extruder clogging,it is not allowed to set the chamber temperature.");
                             }
                             else if (jj["errno"].get<int>() == -4) {
                                  text = _L("When you set the chamber temperature below 40\u2103, the chamber temperature control will not be activated, "
