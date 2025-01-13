9f831d7536def7c644f50f5b59dac975c4a08045
zhimin.zeng <zhimin.zeng@bambulab.com>
2025-01-13 19:34:48 +0800

    Fix: modify the max_k_value
    jira: none
    Change-Id: I5c2a9d00f63ee310cdc144521da6d37e70fd23d1
    (cherry picked from commit 8c5a202f4ac05e8109e148e9ef32844f0dec6805)
    (cherry picked from commit 0ab64d0ccecb038d7cc928c8ca0fe61911c93d0e)

--- src/slic3r/Utils/CalibUtils.cpp
+++ src/slic3r/Utils/CalibUtils.cpp
@@ -15,7 +15,7 @@
 namespace Slic3r {
 namespace GUI {
 const float MIN_PA_K_VALUE = 0.0;
-const float MAX_PA_K_VALUE = 1.0;
+const float MAX_PA_K_VALUE = 2.0;
 
 std::unique_ptr<Worker> CalibUtils::print_worker;
 wxString wxstr_temp_dir = fs::path(fs::temp_directory_path() / "calib").wstring();
