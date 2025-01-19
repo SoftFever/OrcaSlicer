eb218f28cb882066c8ec64772df9406abbcc1a24
chunmao.guo <chunmao.guo@bambulab.com>
2025-01-19 17:57:33 +0800

    FIX: adjust text switch printer inconsistant variant values
    Change-Id: Ie494b57698f45fbf0eea0546a486d6514002887e
    Jira: none
    (cherry picked from commit 33684a12f65768329873cb83d88c8582109e5c43)

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -5622,7 +5622,7 @@ bool Tab::may_discard_current_dirty_preset(PresetCollection *presets /*= nullptr
                 }
             }
             if (has_variants) {
-                auto msg = _L("Parameters related to the extruder model will be discarded because the extruder model list for the front and rear printers is inconsistent.");
+                auto msg = _L("Switching to a printer with different extruder types or numbers will discard or reset changes to extruder or multi-nozzle-related parameters.");
                 MessageDialog(this, msg, _L("Use Modified Value"), wxOK | wxICON_WARNING).ShowModal();
                 selected_options.erase(std::remove(selected_options.begin(), selected_options.end(), ""), selected_options.end());
             }
