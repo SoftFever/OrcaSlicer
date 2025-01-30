460ceddfebf36a8ba611972d57c5ce7027df2c12
Dima Buzdyk <dima.buzdyk@gmail.com>
2025-01-30 19:57:43 +1000

    glcanvas: workaround for touchpad nav on Win
    Releasing ALT on windows platform result in GLCanvas losing focus.
    Because of this mouse movements not being processed by on_mouse() event
    handler and camera rotation have not cleaned up.
    
    Make a workaround for Windows to cleanup camera rotate state on
    releasing ALT modifier.

--- src/slic3r/GUI/GLCanvas3D.cpp
+++ src/slic3r/GUI/GLCanvas3D.cpp
@@ -3519,6 +3519,12 @@ void GLCanvas3D::on_key(wxKeyEvent& evt)
                         m_dirty = true;
                     }
 //                    set_cursor(Standard);
+#ifdef __WXMSW__
+                    if (m_camera_movement && m_is_touchpad_navigation) {
+                        m_camera_movement = false;
+                        m_mouse.set_start_position_3D_as_invalid();
+                    }
+#endif
                 }
                 else if (keyCode == WXK_CONTROL)
                     m_dirty = true;
