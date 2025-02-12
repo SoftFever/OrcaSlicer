c2c6a630b4d4f2c0566c15ac1965aa259f18d293
igiannakas <ioannis@my-spot.co.uk>
2025-02-12 09:15:42 +0000

    Updated beta statements and tooltip changes

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -975,7 +975,7 @@ void PrintConfigDef::init_fff_params()
     def->category = L("Strength");
     def->tooltip = L("Internal bridging angle override. If left to zero, the bridging angle will be calculated "
         "automatically. Otherwise the provided angle will be used for internal bridges. "
-        "Use 180°for zero angle.\n\n It is recommended to leave it at 0 unless there is a specific model need not to.");
+        "Use 180°for zero angle.\n\nIt is recommended to leave it at 0 unless there is a specific model need not to.");
     def->sidetext = L("°");
     def->min = 0;
     def->mode = comAdvanced;
@@ -1472,21 +1472,23 @@ void PrintConfigDef::init_fff_params()
     def->set_default_value(new ConfigOptionBool(true));
     
     def = this->add("enable_extra_bridge_layer", coEnum);
-    def->label = L("Extra bridge layers");
+    def->label = L("Extra bridge layers (beta)");
     def->category = L("Quality");
     def->tooltip = L("This option enables the generation of an extra bridge layer over internal and/or external bridges.\n\n"
                      "Extra bridge layers help improve bridge appearance and reliability, as the solid infill is better supported. "
-                     "This is especially useful in fast printers, where the bridge speed and solid infill speed vary greatly."
-                     "The extra bridge layer results in reduced pillowing on top surfaces, as well as, reduced separation of the external bridge layer from its surrounding perimeters. \n\n"
-                     "It is generally recommended to set this to at least External bridge only, unless specific issues with the sliced model are found.\n\n"
+                     "This is especially useful in fast printers, where the bridge and solid infill speeds vary greatly. "
+                     "The extra bridge layer results in reduced pillowing on top surfaces, as well as reduced separation of the external bridge layer from its surrounding perimeters.\n\n"
+                     "It is generally recommended to set this to at least 'External bridge only', unless specific issues with the sliced model are found.\n\n"
                      "Options:\n"
-                     "1. Disabled - does not generate second bridge layers. This is the default, and set for compatibility purposes.\n"
-                     "2. External bridge only - generates second bridge layers for external facing bridges only. Please note, small bridges that are shorter "
-                     "or narrower than the set number of perimeters will be skipped as they would not benefit from a second bridge layer. The second bridge layer will be extruded "
-                     "parallel to the first bridge layer, to reinforce the bridge strength\n"
+                     "1. Disabled - does not generate second bridge layers. This is the default and is set for compatibility purposes.\n"
+                     "2. External bridge only - generates second bridge layers for external-facing bridges only. Please note that small bridges that are shorter "
+                     "or narrower than the set number of perimeters will be skipped as they would not benefit from a second bridge layer. If generated, the second bridge layer will be extruded "
+                     "parallel to the first bridge layer to reinforce the bridge strength.\n"
                      "3. Internal bridge only - generates second bridge layers for internal bridges over sparse infill only. Please note that the internal "
-                     "bridges count towards the top shell layer count of your model. The second internal bridge layer will be extruded as close to prependicular to the first as possible.\n"
-                     "4. Apply to all - generates second bridge layers for both internal and external facing bridges\n");
+                     "bridges count towards the top shell layer count of your model. The second internal bridge layer will be extruded as close to perpendicular to the first as possible. If multiple regions "
+                     "in the same island, with varying bridge angles are present, the last region of that island will be selected as the angle reference.\n"
+                     "4. Apply to all - generates second bridge layers for both internal and external-facing bridges\n");
+
     def->enum_keys_map = &ConfigOptionEnum<EnableExtraBridgeLayer>::get_enum_values();
     def->enum_values.push_back("disabled");
     def->enum_values.push_back("external_bridge_only");
@@ -1500,21 +1502,21 @@ void PrintConfigDef::init_fff_params()
     def->set_default_value(new ConfigOptionEnum<EnableExtraBridgeLayer>(eblDisabled));
 
     def = this->add("dont_filter_internal_bridges", coEnum);
-    def->label = L("Filter out small internal bridges (beta)");
+    def->label = L("Filter out small internal bridges");
     def->category = L("Quality");
-    def->tooltip = L("This option can help reducing pillowing on top surfaces in heavily slanted or curved models.\n\n"
-                      "By default, small internal bridges are filtered out and the internal solid infill is printed directly"
-                      " over the sparse infill. This works well in most cases, speeding up printing without too much compromise"
-                      " on top surface quality. \n\nHowever, in heavily slanted or curved models especially where too low sparse"
-                     " infill density is used, this may result in curling of the unsupported solid infill, causing pillowing.\n\n"
-                      "Disabling this option will print internal bridge layer over slightly unsupported internal"
-                      " solid infill. The options below control the amount of filtering, i.e. the amount of internal bridges "
+    def->tooltip = L("This option can help reduce pillowing on top surfaces in heavily slanted or curved models.\n\n"
+                     "By default, small internal bridges are filtered out and the internal solid infill is printed directly "
+                     "over the sparse infill. This works well in most cases, speeding up printing without too much compromise "
+                     "on top surface quality. \n\nHowever, in heavily slanted or curved models, especially where too low a sparse "
+                     "infill density is used, this may result in curling of the unsupported solid infill, causing pillowing.\n\n"
+                     "Enabling limited filtering or no filtering will print internal bridge layer over slightly unsupported internal "
+                     "solid infill. The options below control the sensitivity of the filtering, i.e. they control where internal bridges are "
                      "created.\n\n"
-                     "Filter - enable this option. This is the default behavior and works well in most cases.\n\n"
-                     "Limited filtering - creates internal bridges on heavily slanted surfaces, while avoiding creating "
-                     "unnecessary internal bridges. This works well for most difficult models.\n\n"
-                     "No filtering - creates internal bridges on every potential internal overhang. This option is useful "
-                     "for heavily slanted top surface models. However, in most cases it creates too many unnecessary bridges.");
+                     "1. Filter - enables this option. This is the default behavior and works well in most cases.\n\n"
+                     "2. Limited filtering - creates internal bridges on heavily slanted surfaces while avoiding unnecessary bridges. "
+                     "This works well for most difficult models.\n\n"
+                     "3. No filtering - creates internal bridges on every potential internal overhang. This option is useful for "
+                     "heavily slanted top surface models; however, in most cases, it creates too many unnecessary bridges.");
     def->enum_keys_map = &ConfigOptionEnum<InternalBridgeFilter>::get_enum_values();
     def->enum_values.push_back("disabled");
     def->enum_values.push_back("limited");
