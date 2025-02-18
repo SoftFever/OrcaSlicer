0c79462279ccb6e5624003ce0f5b3e881301cb60
Kenneth Jiang <kenneth.jiang@gmail.com>
2025-02-18 15:19:13 -0800

    Refactor JusPrinChatPanel: Move model object JSON methods to JusPrinPlateUtils

--- src/slic3r/GUI/JusPrin/JusPrinPlateUtils.hpp
+++ src/slic3r/GUI/JusPrin/JusPrinPlateUtils.hpp
@@ -5,7 +5,11 @@
 #include <GL/glew.h>
 
 #include "libslic3r/GCode/ThumbnailData.hpp"
+#include "libslic3r/Orient.hpp"
 #include "slic3r/GUI/Camera.hpp"
+#include "slic3r/GUI/Jobs/OrientJob.hpp"
+#include "slic3r/GUI/GUI_App.hpp"
+#include "slic3r/GUI/Plater.hpp"
 
 namespace Slic3r { namespace GUI {
 
@@ -13,10 +17,13 @@ class JusPrinPlateUtils {
 public:
     static nlohmann::json RenderPlateView(const nlohmann::json& params);
     static nlohmann::json GetPlates(const nlohmann::json& params);
+    static nlohmann::json GetAllModelObjectsJson();
 
 private:
     static void RenderThumbnail(ThumbnailData& thumbnail_data,
         const Vec3d& camera_position, const Vec3d& target);
+    static nlohmann::json GetModelObjectFeaturesJson(const ModelObject* obj);
+    static nlohmann::json CostItemsToJson(const Slic3r::orientation::CostItems& cost_items);
 };
 
 }} // namespace Slic3r::GUI
