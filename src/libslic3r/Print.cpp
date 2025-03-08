38fb975d2a67d9bfa2d9246035f2e24de36da9f2
anjis <anjis.zhou@elegoo.com>
2025-03-08 14:01:16 +0800

    Launching and directly loading GCode will result in missing information. (#8745)
    Fix the issue where launching the software and directly loading a GCode file fails to display inner and outer wall information.

--- src/libslic3r/Print.cpp
+++ src/libslic3r/Print.cpp
@@ -2942,6 +2942,7 @@ void Print::export_gcode_from_previous_file(const std::string& file, GCodeProces
 {
     try {
         GCodeProcessor processor;
+        GCodeProcessor::s_IsBBLPrinter = is_BBL_printer();
         const Vec3d origin = this->get_plate_origin();
         processor.set_xy_offset(origin(0), origin(1));
         //processor.enable_producers(true);
