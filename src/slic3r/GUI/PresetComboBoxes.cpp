aaac6847171df741eba722b00a8c9fa29455903e
chunmao.guo <chunmao.guo@bambulab.com>
2025-04-07 12:16:20 +0800

    FIX: DropDown hover item tip
    Change-Id: Ie3d9e654cd612b2db983c3410d7d8d0224200a8d
    Github: 6380
    (cherry picked from commit e6e093949d39fa208c4afaa3f2bc97e3b518ac21)

--- src/slic3r/GUI/PresetComboBoxes.cpp
+++ src/slic3r/GUI/PresetComboBoxes.cpp
@@ -1419,7 +1419,8 @@ void TabPresetComboBox::update()
         assert(bmp);
 
         const wxString name = get_preset_name(preset);
-        preset_descriptions.emplace(name, from_u8(preset.description));
+        if (preset.is_system)
+            preset_descriptions.emplace(name, from_u8(preset.description));
 
         if (preset.is_default || preset.is_system) {
             //BBS: move system to the end
