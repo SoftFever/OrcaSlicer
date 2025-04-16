285c237095207ff4ce666ec8e449102d11e5a22d
xin.zhang <xin.zhang@bambulab.com>
2025-04-16 12:22:55 +0800

    ENH: remove nozzle setting
    jira: [STUDIO-11598]
    Change-Id: Ia10fc3bd67b61a28480b38eef0c28a088c10135e
    (cherry picked from commit 9b585239f7137c43cbd766540e93401dbe5e82e4)

--- src/slic3r/GUI/PrintOptionsDialog.hpp
+++ src/slic3r/GUI/PrintOptionsDialog.hpp
@@ -24,17 +24,6 @@ namespace Slic3r { namespace GUI {
 class PrinterPartsDialog : public DPIDialog
 {
 protected:
-    wxWindowID ID_NOZZLE_TYPE_CHECKBOX_SINGLE;
-    wxWindowID ID_NOZZLE_TYPE_CHECKBOX_LEFT;
-    wxWindowID ID_NOZZLE_TYPE_CHECKBOX_RIGHT;
-
-    wxWindowID ID_NOZZLE_DIAMETER_CHECKBOX_SINGLE;
-    wxWindowID ID_NOZZLE_DIAMETER_CHECKBOX_LEFT;
-    wxWindowID ID_NOZZLE_DIAMETER_CHECKBOX_RIGHT;
-
-    wxWindowID ID_NOZZLE_FLOW_CHECKBOX_LEFT;
-    wxWindowID ID_NOZZLE_FLOW_CHECKBOX_RIGHT;
-
     MachineObject* obj{ nullptr };
 
     ComboBox* nozzle_type_checkbox;
@@ -54,19 +43,10 @@ protected:
     wxPanel *single_panel;
     wxPanel *multiple_panel;
 
-    std::string last_nozzle_type;
-    std::map<NozzleType, wxString> nozzle_type_map;
-    std::map<NozzleType, int> nozzle_type_selection_map;
-
-    std::map<NozzleFlowType, wxString> nozzle_flow_map;
-    std::map<NozzleFlowType, int> nozzle_flow_selection_map;
-
-    std::map<int, float> nozzle_stainless_diameter_map;
-    std::map<int, float> nozzle_hard_diameter_map;
 public:
     PrinterPartsDialog(wxWindow* parent);
     ~PrinterPartsDialog();
-    void set_nozzle_data(wxCommandEvent& evt);
+
     void on_dpi_changed(const wxRect& suggested_rect) override;
     void update_machine_obj(MachineObject* obj_);
     bool Show(bool show) override;
@@ -74,6 +54,9 @@ public:
 private:
     void  EnableEditing(bool enable);
 
+    wxString GetString(NozzleType nozzle_type) const;
+    wxString GetString(NozzleFlowType nozzle_flow_type) const;
+    wxString GetString(float diameter) const { return wxString::FromDouble(diameter); };
 };
 
 
