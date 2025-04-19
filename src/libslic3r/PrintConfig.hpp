19d8e59160eac8fde42b62cd41be15aee278261b
EpiphanyPrinting <Ephiphanyprinting@gmail.com>
2025-04-18 20:53:00 -0600

    Update PrintConfig.hpp

--- src/libslic3r/PrintConfig.hpp
+++ src/libslic3r/PrintConfig.hpp
@@ -1042,7 +1042,7 @@ PRINT_CONFIG_CLASS_DEFINE(
     ((ConfigOptionFloat,                scarf_joint_flow_ratio))
     ((ConfigOptionPercent,              scarf_overhang_threshold))
 
-        //resonance avoidance
+        //resonance avoidance ported from qidi slicer
     ((ConfigOptionBool,                resonance_avoidance))
     ((ConfigOptionFloat,               min_resonance_avoidance_speed))
     ((ConfigOptionFloat,               max_resonance_avoidance_speed))
