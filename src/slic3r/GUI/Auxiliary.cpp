42152d2a6404beba1e30330fc335ae91a6c08895
kfbest <kfbest@users.noreply.github.com>
2025-04-19 23:29:08 -0400

    yet another debug suggestion

--- src/slic3r/GUI/Auxiliary.cpp
+++ src/slic3r/GUI/Auxiliary.cpp
@@ -1093,7 +1093,8 @@ void AuxiliaryPanel::update_all_cover()
      m_text_description->SetForegroundColour(*wxBLACK);
      m_text_description->Wrap(-1);
      m_sizer_description->Add(m_text_description, 0, wxALIGN_CENTER, 0);
-     m_input_description = static_cast<TextInput*>(new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(FromDIP(450), FromDIP(100)), wxTE_MULTILINE | wxTE_PROCESS_ENTER));
+     wxTextCtrl *m_input_description = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, 
+                                                 wxSize(FromDIP(450), FromDIP(100)), wxTE_MULTILINE | wxTE_PROCESS_ENTER);
      m_input_description->SetFont(::Label::Body_14);
      m_sizer_description->Add(m_text_description, 1, wxEXPAND | wxALIGN_CENTER, 0);
 
