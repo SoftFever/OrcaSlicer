02aad8ee43dfc6455f50962aa17050171c815a5c
yw4z <ywsyildiz@gmail.com>
2025-04-23 20:43:20 +0300

    add enums for style and type

--- src/slic3r/GUI/Widgets/Button.hpp
+++ src/slic3r/GUI/Widgets/Button.hpp
@@ -10,6 +10,21 @@ public:
     static int ChoiceGap();
 };
 
+enum class ButtonType{
+    None      = 0,
+    Compact   = 1,
+    Window    = 2,
+    Choice    = 3,
+    Parameter = 4,
+};
+
+enum class ButtonStyle{
+    Regular   = 1,
+    Confirm   = 2,
+    Alert     = 3,
+    Disabled  = 4,
+};
+
 class Button : public StaticBox
 {
     wxRect textSize;
@@ -47,8 +62,10 @@ public:
     
     void SetPaddingSize(const wxSize& size);
 
+    //void SetStyle(const ButtonStyle style = ButtonStyle::Regular, const ButtonType& type = ButtonType::None);
+    
     void SetStyle(const wxString style /* Regular/Confirm/Alert/Disabled */, const wxString& type = "" /* Choice/Window/Parameter */);
-
+    
     void SetType(const wxString size);
 
     void SetTextColor(StateColor const &color);
