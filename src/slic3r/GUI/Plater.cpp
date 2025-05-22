9b68aef078e4af311e301218cb28e0c0f1459815
zhimin.zeng <zhimin.zeng@bambulab.com>
2025-05-22 09:45:44 +0800

    FIX: the nozzle type sync status is incorrect for P/A printers
    jira: STUDIO-12310
    Change-Id: I7a01bcfdfda45de4c2eb65b30e11e107779d3ecb
    (cherry picked from commit 4a15a6da6c10383cc6a9c5ac47ce61c4acbc60ae)

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -1385,8 +1385,11 @@ void Sidebar::priv::update_sync_status(const MachineObject *obj)
         }
     };
 
-    auto is_same_nozzle_info = [](const ExtruderInfo &left, const ExtruderInfo &right) {
-        return abs(left.diameter - right.diameter) < EPSILON /*&& left.nozzle_volue_type == right.nozzle_volue_type*/;
+    auto is_same_nozzle_info = [obj](const ExtruderInfo &left, const ExtruderInfo &right) {
+        bool is_same_nozzle_type = true;
+        if (obj->is_nozzle_flow_type_supported())
+            is_same_nozzle_type = true;//left.nozzle_volue_type == right.nozzle_volue_type; // TODO: Orca hack
+        return abs(left.diameter - right.diameter) < EPSILON && is_same_nozzle_type;
     };
 
     // 2. update extruder status
