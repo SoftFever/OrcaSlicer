d0c5b6be775a9b0b8e80515b2829fcd2f51eafca
alexdaima <61457613+alexdaima@users.noreply.github.com>
2025-05-27 12:32:48 +0800

    Test build process fix that worked locally

--- deps/TIFF/TIFF.cmake
+++ deps/TIFF/TIFF.cmake
@@ -3,8 +3,8 @@ find_package(OpenGL QUIET REQUIRED)
 if (APPLE)
     message(STATUS "Compiling TIFF for macos ${CMAKE_SYSTEM_VERSION}.")
     orcaslicer_add_cmake_project(TIFF
-        URL https://gitlab.com/libtiff/libtiff/-/archive/v4.3.0/libtiff-v4.3.0.zip
-        URL_HASH SHA256=4fca1b582c88319f3ad6ecd5b46320eadaf5eb4ef6f6c32d44caaae4a03d0726
+        URL https://gitlab.com/libtiff/libtiff/-/archive/v4.1.0/libtiff-v4.1.0.zip
+        URL_HASH SHA256=17a3e875acece9be40b093361cfef47385d4ef22c995ffbf36b2871f5785f9b8
         DEPENDS ${ZLIB_PKG} ${PNG_PKG} dep_JPEG
         CMAKE_ARGS
             -Dlzma:BOOL=OFF
@@ -17,7 +17,7 @@ if (APPLE)
 else()
     orcaslicer_add_cmake_project(TIFF
         URL https://gitlab.com/libtiff/libtiff/-/archive/v4.1.0/libtiff-v4.1.0.zip
-        URL_HASH SHA256=c56edfacef0a60c0de3e6489194fcb2f24c03dbb550a8a7de5938642d045bd32
+        URL_HASH SHA256=17a3e875acece9be40b093361cfef47385d4ef22c995ffbf36b2871f5785f9b8
         DEPENDS ${ZLIB_PKG} ${PNG_PKG} dep_JPEG
         CMAKE_ARGS
             -Dlzma:BOOL=OFF
@@ -28,6 +28,3 @@ else()
     )
 
 endif()
-
-
-
