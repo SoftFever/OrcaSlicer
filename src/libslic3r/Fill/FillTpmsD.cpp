e135d9d663b1554da2b7380148429280063e51bb
RF47 <162915171+RF47@users.noreply.github.com>
2025-06-01 01:26:39 -0300

    Improve Precision +  refactoring
    clean tpms.hpp
    
    Co-Authored-By: Ian Bassi <12130714+ianalexis@users.noreply.github.com>

--- src/libslic3r/Fill/FillTpmsD.cpp
+++ src/libslic3r/Fill/FillTpmsD.cpp
@@ -7,7 +7,7 @@
 #include <unordered_set>
 #include <utility>
 #include <tbb/parallel_for.h>
-//From Creality Print
+// From Creality Print
 namespace Slic3r {
 
 using namespace std;
@@ -69,7 +69,7 @@ public:
             if (segmentid0 != segmentid1) {
                 segment_ids.insert(segmentid0 < segmentid1 ? std::make_pair(segmentid0, segmentid1) :
                                                              std::make_pair(segmentid1, segmentid0));
-            } 
+            }
         }
 
         unordered_map<int, vector<int>> graph;
@@ -122,24 +122,23 @@ private:
              std::vector<int>&                          polyline)
     {
         std::vector<int> stack;
-        stack.reserve(graph.size()); 
+        stack.reserve(graph.size());
         stack.push_back(start_node);
         while (!stack.empty()) {
-            int node = stack.back(); 
+            int node = stack.back();
             stack.pop_back();
-            if (!visited.insert(node).second) { 
-                continue;                       
+            if (!visited.insert(node).second) {
+                continue;
             }
             polyline.push_back(node);
             auto& neighbors = graph[node];
             for (const auto& neighbor : neighbors) {
                 if (visited.find(neighbor) == visited.end()) {
-                    stack.push_back(neighbor); 
+                    stack.push_back(neighbor);
                 }
             }
         }
-    }  
-
+    }
 };
 
 namespace MarchingSquares {
@@ -281,30 +280,25 @@ void process_block(int                                               i,
     }
 }
 
- void    drawContour(double                                            contourValue,
-                      int                                               gridSize_w,
-                      int                                               gridSize_h,
-                      vector<vector<double>>&                           data,
-                      std::vector<std::vector<MarchingSquares::Point>>& posxy,
+void drawContour(double                                            contourValue,
+                 int                                               gridSize_w,
+                 int                                               gridSize_h,
+                 vector<vector<double>>&                           data,
+                 std::vector<std::vector<MarchingSquares::Point>>& posxy,
                  Polylines&                                        repls)
 {
-    vector<Point> contourPoints;
-    int total_size = (gridSize_h - 1) * (gridSize_w - 1);
+    vector<Point>         contourPoints;
+    int                   total_size = (gridSize_h - 1) * (gridSize_w - 1);
     vector<vector<Point>> contourPointss;
     contourPointss.resize(total_size);
     tbb::parallel_for(tbb::blocked_range<size_t>(0, total_size),
                       [&contourValue, &posxy, &contourPointss, &data, &gridSize_w](const tbb::blocked_range<size_t>& range) {
                           for (size_t k = range.begin(); k < range.end(); ++k) {
-                              int i = k / (gridSize_w - 1); // 
+                              int i = k / (gridSize_w - 1); //
                               int j = k % (gridSize_w - 1); //
-                                  process_block(i, j, data, contourValue, posxy, contourPointss[k]);
-                             
+                              process_block(i, j, data, contourValue, posxy, contourPointss[k]);
                           }
                       });
-
-
-
-
 
     vector<pair<myPoint, myPoint>> segments2;
     myPoint                        p1, p2;
@@ -318,7 +312,6 @@ void process_block(int                                               i,
         }
     }
 
-
     LineSegmentMerger       merger;
     vector<vector<myPoint>> result;
     merger.mergeSegments(segments2, result);
@@ -328,33 +321,21 @@ void process_block(int                                               i,
         for (myPoint& pt : p) {
             repltmp.points.push_back(Slic3r::Point(pt.x, pt.y));
         }
-        repltmp.simplify(scale_(0.1f));
+        repltmp.simplify(scale_(0.05f));
         repls.push_back(repltmp);
     }
 }
-} 
-
-using namespace std;
-
-
-
-constexpr double FillTpmsD::PatternTolerance;
-
-float get_linearinterpolation(float a, float b, float c, float d, float x)
-{
-    float y = c - ((c - d) * (a - x) / (a - b));
-    return y;
-}
+} // namespace MarchingSquares
 
 static float sin_table[360];
 static float cos_table[360];
 static bool  g_is_init = false;
 
-#define PIratio 57.29577951308232  // 180/PI
-static void  initialize_lookup_tables()
+#define PIratio 57.29577951308232 // 180/PI
+static void initialize_lookup_tables()
 {
     for (int i = 0; i < 360; ++i) {
-        float angle  = i * (M_PI / 180.0); 
+        float angle  = i * (M_PI / 180.0);
         sin_table[i] = std::sin(angle);
         cos_table[i] = std::cos(angle);
     }
@@ -372,44 +353,43 @@ static float get_cos(float angle)
     angle     = angle * PIratio;
     int index = static_cast<int>(std::fmod(angle, 360) + 360) % 360;
     return cos_table[index];
-}  
+}
 
- FillTpmsD::FillTpmsD() {
- 
-     if (!g_is_init) {
-         initialize_lookup_tables();
-         g_is_init = true;
-     }
- 
- }
+FillTpmsD::FillTpmsD()
+{
+    if (!g_is_init) {
+        initialize_lookup_tables();
+        g_is_init = true;
+    }
+}
 void FillTpmsD::_fill_surface_single(const FillParams&              params,
                                      unsigned int                   thickness_layers,
                                      const std::pair<float, Point>& direction,
                                      ExPolygon                      expolygon,
                                      Polylines&                     polylines_out)
 {
-        auto infill_angle = float(this->angle - (CorrectionAngle * 2 * M_PI) / 360.);
-    if(std::abs(infill_angle) >= EPSILON)
+    auto infill_angle = float(this->angle - (CorrectionAngle * 2 * M_PI) / 360.);
+    if (std::abs(infill_angle) >= EPSILON)
         expolygon.rotate(-infill_angle);
 
     float vari_T = 2.98 * spacing / params.density; // Infill density adjustment factor for TPMS-D
-   
+
     BoundingBox bb      = expolygon.contour.bounding_box();
     auto        cenpos  = unscale(bb.center());
     auto        boxsize = unscale(bb.size());
     float       xlen    = boxsize.x();
     float       ylen    = boxsize.y();
 
-    float delta    = 0.5f;
+    float delta    = 0.25f;
     float myperiod = 2 * PI / vari_T;
-    float c_z          = myperiod * this->z;
-    float cos_z        = get_cos(c_z);
-    float sin_z        = get_sin(c_z);
+    float c_z      = myperiod * this->z;
+    float cos_z    = get_cos(c_z);
+    float sin_z    = get_sin(c_z);
 
     auto scalar_field = [&](float x, float y) {
         // TPMS-D
         float a_x = myperiod * x;
-        float b_y = myperiod * y;        
+        float b_y = myperiod * y;
         float r   = get_cos(a_x) * get_cos(b_y) * cos_z - get_sin(a_x) * get_sin(b_y) * sin_z;
         return r;
     };
@@ -429,16 +409,16 @@ void FillTpmsD::_fill_surface_single(const FillParams&              params,
         posxy.push_back(colposxy);
     }
 
-    std::vector<std::vector<double>> data(posxy.size(), std::vector<double>(posxy[0].size())); 
+    std::vector<std::vector<double>> data(posxy.size(), std::vector<double>(posxy[0].size()));
 
-    int   width      = posxy[0].size();
-    int   height     = posxy.size();
-    int   total_size = (height) * (width);
+    int width      = posxy[0].size();
+    int height     = posxy.size();
+    int total_size = (height) * (width);
     tbb::parallel_for(tbb::blocked_range<size_t>(0, total_size),
-                      [ &width, &scalar_field, &data, &posxy](const tbb::blocked_range<size_t>& range) {
+                      [&width, &scalar_field, &data, &posxy](const tbb::blocked_range<size_t>& range) {
                           for (size_t k = range.begin(); k < range.end(); ++k) {
-                              int i      = k / (width); 
-                              int j      = k % (width); 
+                              int i      = k / (width);
+                              int j      = k % (width);
                               data[i][j] = scalar_field(posxy[i][j].x, posxy[i][j].y);
                           }
                       });
@@ -455,11 +435,11 @@ void FillTpmsD::_fill_surface_single(const FillParams&              params,
             append(polylines_out, chain_polylines(polylines));
         else
             this->connect_infill(std::move(polylines), expolygon, polylines_out, this->spacing, params);
-	    // new paths must be rotated back
+        // new paths must be rotated back
         if (std::abs(infill_angle) >= EPSILON) {
-	        for (auto it = polylines_out.begin() + polylines_out_first_idx; it != polylines_out.end(); ++ it)
-	        	it->rotate(infill_angle);
-	    }
+            for (auto it = polylines_out.begin() + polylines_out_first_idx; it != polylines_out.end(); ++it)
+                it->rotate(infill_angle);
+        }
     }
 }
 
