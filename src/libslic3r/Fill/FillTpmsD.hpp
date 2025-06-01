e3e78b3eaae6b01b4d6e8f38dae1ee25b22fa79d
RF47 <162915171+RF47@users.noreply.github.com>
2025-06-01 00:03:58 -0300

    density Adjusted
    Co-Authored-By: Ian Bassi <12130714+ianalexis@users.noreply.github.com>

--- src/libslic3r/Fill/FillTpmsD.hpp
+++ src/libslic3r/Fill/FillTpmsD.hpp
@@ -19,9 +19,6 @@ public:
     // speed in default configuration (degrees)
     static constexpr float CorrectionAngle = -45.;
 
-    // Density adjustment to have a good %of weight.
-    static constexpr double DensityAdjust = 2.44;
-
     // Gyroid upper resolution tolerance (mm^-2)
     static constexpr double PatternTolerance = 0.2;
 
@@ -37,6 +34,5 @@ protected:
 
 } // namespace Slic3r
 
-//static std::vector<float> FillTpmsD::sin_table.reserve(FillTpmsD::TABLE_SIZE);
 
 #endif // slic3r_FillTpmsD_hpp_
