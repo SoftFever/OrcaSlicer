d819072301a4ed4e2f1b2d76635054ab4169c216
yw4z <ywsyildiz@gmail.com>
2025-06-10 20:10:06 +0300

    update

--- src/slic3r/GUI/GUI_App.cpp
+++ src/slic3r/GUI/GUI_App.cpp
@@ -2924,6 +2924,7 @@ void GUI_App::init_label_colours()
     bool is_dark_mode = dark_mode();
     m_color_label_modified = is_dark_mode ? wxColour("#F1754E") : wxColour("#F1754E");
     m_color_label_sys      = is_dark_mode ? wxColour("#B2B3B5") : wxColour("#363636");
+    m_color_label_error    = wxColour("#E14747"); // same color used on block notification
 
 #if defined(_WIN32) || defined(__linux__) || defined(__APPLE__)
     m_color_label_default           = is_dark_mode ? wxColour(250, 250, 250) : m_color_label_sys; // wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOWTEXT);
