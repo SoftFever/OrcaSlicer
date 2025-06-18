0010dc6bb4fc7cba6ffc552c6d1b0c6a45ae2c8c
Henk <40023052+elektrohenk@users.noreply.github.com>
2025-06-18 02:48:48 +0200

    Add 2D honeycomb infill pattern (#9483)
    * Add 2D honeycomb infill pattern
    
    * Reverted change of 2D lattice infill void area estimation
    
    ---------
    
    Co-authored-by: Noisyfox <timemanager.rick@gmail.com>

--- src/slic3r/GUI/ConfigManipulation.cpp
+++ src/slic3r/GUI/ConfigManipulation.cpp
@@ -804,6 +804,8 @@ void ConfigManipulation::toggle_print_fff_options(DynamicPrintConfig *config, co
     bool lattice_options = config->opt_enum<InfillPattern>("sparse_infill_pattern") == InfillPattern::ip2DLattice;
     for (auto el : { "lattice_angle_1", "lattice_angle_2"})
         toggle_line(el, lattice_options);
+
+    toggle_line("infill_overhang_angle", config->opt_enum<InfillPattern>("sparse_infill_pattern") == InfillPattern::ip2DHoneycomb);
 }
 
 void ConfigManipulation::update_print_sla_config(DynamicPrintConfig* config, const bool is_global_config/* = false*/)
