9e7f7903c053fa3f8ac369a610e337ac451f3446
Noisyfox <timemanager.rick@gmail.com>
2025-06-23 16:01:40 +0800

    Update UI style

--- src/slic3r/GUI/Gizmos/GLGizmoFuzzySkin.hpp
+++ src/slic3r/GUI/Gizmos/GLGizmoFuzzySkin.hpp
@@ -10,13 +10,15 @@ namespace Slic3r::GUI {
 class GLGizmoFuzzySkin : public GLGizmoPainterBase
 {
 public:
-    GLGizmoFuzzySkin(GLCanvas3D &parent, const std::string &icon_filename, unsigned int sprite_id) : GLGizmoPainterBase(parent, icon_filename, sprite_id) {}
+    GLGizmoFuzzySkin(GLCanvas3D& parent, const std::string& icon_filename, unsigned int sprite_id);
 
     void render_painter_gizmo() override;
 
 protected:
     void        on_render_input_window(float x, float y, float bottom_limit) override;
     std::string on_get_name() const override;
+
+    void show_tooltip_information(float caption_max, float x, float y);
 
     wxString handle_snapshot_action_name(bool shift_down, Button button_down) const override;
 
@@ -26,6 +28,9 @@ protected:
 
     EnforcerBlockerType get_left_button_state_type() const override { return EnforcerBlockerType::FUZZY_SKIN; }
     EnforcerBlockerType get_right_button_state_type() const override { return EnforcerBlockerType::NONE; }
+
+    // BBS
+    wchar_t                           m_current_tool = 0;
 
 private:
     bool on_init() override;
@@ -39,7 +44,7 @@ private:
 
     // This map holds all translated description texts, so they can be easily referenced during layout calculations
     // etc. When language changes, GUI is recreated, and this class constructed again, so the change takes effect.
-    std::map<std::string, std::string> m_desc;
+    std::map<std::string, wxString> m_desc;
 };
 
 } // namespace Slic3r::GUI
