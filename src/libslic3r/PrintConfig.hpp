575e8349e72b4339e790c474fa27e7422ee96a2a
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-06-29 20:27:54 +0200

    feat: add force_simple_config and force_advanced_config logic

--- src/libslic3r/PrintConfig.hpp
+++ src/libslic3r/PrintConfig.hpp
@@ -1249,6 +1249,8 @@ PRINT_CONFIG_CLASS_DEFINE(
     ((ConfigOptionBool,                has_scarf_joint_seam))
     
     ((ConfigOptionStrings,             hide_config))
+    ((ConfigOptionStrings,             force_simple_config))
+    ((ConfigOptionStrings,             force_advanced_config))
 )
 
 // This object is mapped to Perl as Slic3r::Config::Print.
