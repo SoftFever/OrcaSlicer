27050d80f5cc5ed323ba737c833f1d739f163b4e
Rodrigo <162915171+RF47@users.noreply.github.com>
2025-07-13 10:35:34 -0300

    adjusting disk diameter

--- src/slic3r/GUI/GUI_ObjectList.cpp
+++ src/slic3r/GUI/GUI_ObjectList.cpp
@@ -2154,7 +2154,7 @@ static TriangleMesh create_mesh(const std::string& type_name, const BoundingBoxf
     else if (type_name == "Cone")
         mesh = TriangleMesh(its_make_cone(0.5 * side, side));
     else if (type_name == "Disc")
-        mesh = TriangleMesh(its_make_cylinder(0.5 * 15, 0.2f));
+        mesh = TriangleMesh(its_make_cylinder(0.5 * side, 0.2f));
     else if (type_name == "Torus")
         mesh = TriangleMesh(its_make_torus(0.5 * side, 0.125 * side,(PI / 60)));
     return mesh;
