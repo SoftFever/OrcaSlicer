81d1735bc21079aabc955b7144e8df90821755f8
RF47 <162915171+RF47@users.noreply.github.com>
2025-07-17 18:43:33 -0300

    Disable Infill PA
    Co-Authored-By: Ian Bassi <12130714+ianalexis@users.noreply.github.com>

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -2467,6 +2467,7 @@ optgroup->append_single_option_line("skirt_loops", "others_settings_skirt#loops"
 
         optgroup = page->new_optgroup(L("G-code output"), L"param_gcode");
         optgroup->append_single_option_line("reduce_infill_retraction", "others_settings_g_code_output#reduce-infill-retraction");
+        optgroup->append_single_option_line("disable_infill_pressure_advance","");
         optgroup->append_single_option_line("gcode_add_line_number", "others_settings_g_code_output#add-line-number");
         optgroup->append_single_option_line("gcode_comments", "others_settings_g_code_output#verbose-g-code");
         optgroup->append_single_option_line("gcode_label_objects", "others_settings_g_code_output#label-objects");
