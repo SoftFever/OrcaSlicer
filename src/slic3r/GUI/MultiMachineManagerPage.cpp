f9b86622315341a3f3dc395a1dccc9dea0a26b0c
RF47 <162915171+RF47@users.noreply.github.com>
2025-07-16 22:16:03 -0300

    Fix / silence compile warnings

--- src/slic3r/GUI/MultiMachineManagerPage.cpp
+++ src/slic3r/GUI/MultiMachineManagerPage.cpp
@@ -676,7 +676,10 @@ void MultiMachineManagerPage::start_timer()
 
     m_flipping_timer->SetOwner(this);
     m_flipping_timer->Start(1000);
+#pragma warning(push)
+#pragma warning(disable: 4996)
     wxPostEvent(this, wxTimerEvent());
+#pragma warning(pop)
 }
 
 void MultiMachineManagerPage::update_page_number()
