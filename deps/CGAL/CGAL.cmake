cd65a3d1d48c42c15e09e1ce229c60b5bc4078cc
Amy While <26681721+elihwyma@users.noreply.github.com>
2025-07-23 17:04:11 +0100

    chore: update CGAL and GMP

--- deps/CGAL/CGAL.cmake
+++ deps/CGAL/CGAL.cmake
@@ -7,9 +7,8 @@ orcaslicer_add_cmake_project(
     # GIT_REPOSITORY https://github.com/CGAL/cgal.git
     # GIT_TAG        bec70a6d52d8aacb0b3d82a7b4edc3caa899184b # releases/CGAL-5.0
     # For whatever reason, this keeps downloading forever (repeats downloads if finished)
-    URL      https://github.com/CGAL/cgal/archive/refs/tags/v5.4.zip
-    URL_HASH SHA256=d7605e0a5a5ca17da7547592f6f6e4a59430a0bc861948974254d0de43eab4c0
-    PATCH_COMMAND git apply ${CGAL_DIRECTORY_FLAG} --verbose --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/0001-clang19.patch
+    URL      https://github.com/CGAL/cgal/archive/refs/tags/v5.6.2.zip
+    URL_HASH SHA256=29acaeee5a76a95029fac23131bb1c3a4a75df9a0e7e43b465a1f32d0628f45d
     DEPENDS dep_Boost dep_GMP dep_MPFR
 )
 
