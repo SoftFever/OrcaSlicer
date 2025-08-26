52f3fb26f986295aeafaeebdb0053378d8dd7994
tao wang <tao.wang@bambulab.com>
2025-08-26 20:07:08 +0800

    NEW:support nozzle mapping
    Change-Id: I1b9e906d99bc7a3172dc31976422f67e08cfdcd3
    (cherry picked from commit 24fcf7d25c54e571627ce3383888e5d4deb01851)

--- src/slic3r/GUI/Jobs/PrintJob.hpp
+++ src/slic3r/GUI/Jobs/PrintJob.hpp
@@ -60,6 +60,7 @@ public:
     std::string m_ftp_folder;
     std::string m_access_code;
     std::string task_bed_type;
+    std::string task_nozzle_mapping;
     std::string task_ams_mapping;
     std::string task_ams_mapping2;
     std::string task_ams_mapping_info;
