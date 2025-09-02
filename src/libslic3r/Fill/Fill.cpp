6d1f010920229182ae334a5a53e6efbbfb24f433
Ian Bassi <ian.bassi@outlook.com>
2025-09-02 17:12:27 -0300

    Ironin logic

--- src/libslic3r/Fill/Fill.cpp
+++ src/libslic3r/Fill/Fill.cpp
@@ -1536,11 +1536,15 @@ void Layer::make_ironing()
 				}
 			}
 			if (ironing_params.extruder != -1) {
-				//TODO just_infill is currently not used.
 				ironing_params.just_infill 	= false;
-				ironing_params.line_spacing = config.ironing_spacing;
-                ironing_params.inset 		= config.ironing_inset;
-				ironing_params.height 		= default_layer_height * 0.01 * config.ironing_flow;
+
+				double filament_ironing_flow = config.filament_ironing_flow;
+				double filament_ironing_spacing = config.filament_ironing_spacing;
+				double filament_ironing_inset = config.filament_ironing_inset;
+
+				ironing_params.line_spacing = (filament_ironing_spacing > 0) ? filament_ironing_spacing : config.ironing_spacing;
+                ironing_params.inset 		= (filament_ironing_inset > 0) ? filament_ironing_inset : config.ironing_inset;
+				ironing_params.height 		= default_layer_height * 0.01 * ((filament_ironing_flow > 0) ? filament_ironing_flow : config.ironing_flow);
 				ironing_params.speed 		= config.ironing_speed;
                 ironing_params.angle        = (config.ironing_angle >= 0 ? config.ironing_angle : config.infill_direction) * M_PI / 180.;
 				ironing_params.pattern      = config.ironing_pattern;
