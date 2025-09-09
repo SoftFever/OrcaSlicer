bbe3dd855f0e7ef62c3ef8b2fd6e4caa00b47702
Raoul Rubien <rubienr@sbox.tugraz.at>
2025-09-09 21:27:41 +0200

    fixes: catching polymorphic type by value [-Wcatch-value=]

--- src/libslic3r/Format/STEP.cpp
+++ src/libslic3r/Format/STEP.cpp
@@ -484,7 +484,7 @@ unsigned int Step::get_triangle_num(double linear_defletion, double angle_deflet
                 return 0;
             }
         }
-    } catch(Exception e) {
+    } catch(const Exception &e) {
         return 0;
     }
     
