b4008ee60d870421a1e7f8e91ed66d7564c7056a
weizhen.xie <weizhen.xie@bambulab.com>
2025-09-16 10:08:44 +0800

    ENH: add initial layer time to plate_x.json and slice_info.config
    Jira: STUDIO-14504
    Change-Id: I32460f1c19cce7c729806e35370e8b4b57aa069e
    (cherry picked from commit 04a487d210134c28245c1f0f7d0986266a1c66f2)

--- src/slic3r/GUI/PartPlate.cpp
+++ src/slic3r/GUI/PartPlate.cpp
@@ -5920,6 +5920,7 @@ int PartPlateList::store_to_3mf_structure(PlateDataPtrs& plate_data_list, bool w
 					plate_data_item->is_label_object_enabled = m_plate_list[i]->m_gcode_result->label_object_enabled;
                     plate_data_item->limit_filament_maps = m_plate_list[i]->m_gcode_result->limit_filament_maps;
                     plate_data_item->layer_filaments  = m_plate_list[i]->m_gcode_result->layer_filaments;
+                    plate_data_item->first_layer_time = std::to_string(m_plate_list[i]->cali_bboxes_data.first_layer_time);
 					Print *print                      = nullptr;
 					m_plate_list[i]->get_print((PrintBase **) &print, nullptr, nullptr);
 					if (print) {
