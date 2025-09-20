3997ec029a9b82e25ec313fb6ee1ae8f88e50166
RF47 <162915171+RF47@users.noreply.github.com>
2025-09-20 18:42:49 -0300

    cmake 4 windows
    Update CMakeLists.txt
    
    cmake 4 windows
    
    Update build_release_vs2022.bat
    
    minimum version 3.5

--- build_release_vs2022.bat
+++ build_release_vs2022.bat
@@ -46,6 +46,8 @@ if "%1"=="slicer" (
 echo "building deps.."
 
 echo on
+REM Set minimum CMake policy to avoid <3.5 errors
+set CMAKE_POLICY_VERSION_MINIMUM=3.5
 cmake ../ -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=%build_type%
 cmake --build . --config %build_type% --target deps -- -m
 @echo off
