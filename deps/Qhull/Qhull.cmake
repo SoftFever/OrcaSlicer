ca9433614fa0e9dfc52092b1e172adb6d6877e7d
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2025-09-23 17:38:59 +0200

    Update Qhull.cmake
    switch to last version

--- deps/Qhull/Qhull.cmake
+++ deps/Qhull/Qhull.cmake
@@ -1,7 +1,7 @@
 include(GNUInstallDirs)
 orcaslicer_add_cmake_project(Qhull
-    URL "https://github.com/qhull/qhull/archive/v8.0.1.zip"
-    URL_HASH SHA256=5287f5edd6a0372588f5d6640799086a4033d89d19711023ef8229dd9301d69b
+    URL "https://github.com/qhull/qhull/archive/v8.1-alpha6.zip"
+    URL_HASH SHA256=d79b73774236f82e4940ce74c8b6cbb6ef3c72ef053d01d1bbfb19ab65dbfc22
     CMAKE_ARGS 
         -DINCLUDE_INSTALL_DIR=${CMAKE_INSTALL_INCLUDEDIR}
 )
