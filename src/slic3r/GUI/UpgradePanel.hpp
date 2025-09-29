fbf9424ec5a3e4b5ae61b3c56e15c8558a7725c1
milk <yongxin.chang@bambulab.com>
2025-09-29 14:40:22 +0800

    FIX:Change model name for upgradepanel
    jira:[STUDIO-13910]
    
    Change-Id: I2afad42cdd8907252203fe6d1a552d8d3cf2c2df
    (cherry picked from commit 5aa8921e4ab4d687a2ec93b73489f6215b72dbd2)

--- src/slic3r/GUI/UpgradePanel.hpp
+++ src/slic3r/GUI/UpgradePanel.hpp
@@ -19,6 +19,7 @@ class ExtensionPanel : public wxPanel
 {
 public:
     wxStaticText* m_staticText_ext;
+    wxStaticText* m_staticText_ext_val;
     wxStaticText* m_staticText_ext_ver;
     wxStaticText* m_staticText_ext_ver_val;
     wxStaticText* m_staticText_ext_sn_val;
