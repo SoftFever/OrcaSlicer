bambulab/BambuStudio

FIX:The estimated flushing amount of consumables for multi-color printing is inaccurate.
change: Multiply the flush_volumes_matrix in the exported gcode by the coefficient and output the final value.
Also fixed the issue where the flush matrix page was limited to 900, but the Gcode output value could be greater than 900 (the product of the matrix and the multiplier was limited to 900 before storing the value)

jira: STUDIO-13332

Change-Id: I893f27bec206c3b9da3273241d1cd5f1883e55a9
(cherry picked from commit 8aa91cd86c8c75b3736c616bcfbed4872db9734c)

src/libslic3r/GCode.cpp
@@ -2103,7 +2103,33 @@ void GCode::_do_export(Print& print, GCodeOutputStream &file, ThumbnailsGenerato
     {
         file.write("; CONFIG_BLOCK_START\n");
         std::string full_config;
-        append_full_config(print, full_config);
+        DynamicPrintConfig print_cfg_temp = print.full_print_config();
+
+        {//correct the flush_volumes_matrix with flush_multiplier values
+            std::vector<double> temp_cfg_flush_multiplier   = print_cfg_temp.option<ConfigOptionFloats>("flush_multiplier")->values;
+            std::vector<double> temp_flush_volumes_matrix = print_cfg_temp.option<ConfigOptionFloats>("flush_volumes_matrix")->values;
+            auto                temp_filament_color         = print_cfg_temp.option<ConfigOptionStrings>("filament_colour")->values;
+            size_t              heads_count_tmp = temp_cfg_flush_multiplier.size(),
+                matrix_value_count = temp_flush_volumes_matrix.size() / temp_cfg_flush_multiplier.size(),
+                   filament_count_tmp = temp_filament_color.size();
+            if (filament_count_tmp * filament_count_tmp * heads_count_tmp == temp_flush_volumes_matrix.size()) {
+                for (size_t idx = 0; idx < heads_count_tmp; ++idx) {
+                    double temp_cfg_flush_multiplier_idx = temp_cfg_flush_multiplier[idx];
+                    size_t temp_begin_t = idx * matrix_value_count, temp_end_t = (idx + 1) * matrix_value_count;
+                    std::transform(temp_flush_volumes_matrix.begin() + temp_begin_t,
+                        temp_flush_volumes_matrix.begin() + temp_end_t,
+                                   temp_flush_volumes_matrix.begin() + temp_begin_t,
+                                   [temp_cfg_flush_multiplier_idx](double inputx) { return inputx * temp_cfg_flush_multiplier_idx; });
+                }
+                print_cfg_temp.option<ConfigOptionFloats>("flush_volumes_matrix")->values = temp_flush_volumes_matrix;
+            } else if (filament_count_tmp == 1) {
+            }// Not applicable to flush matrix situations
+            else
+            { // flush_volumes_matrix value count error?
+                throw Slic3r::SlicingError(_(L("Flush volumes matrix do not match to the correct size!")));
+            }
+        }
+        append_full_config(print_cfg_temp, full_config);
         if (!full_config.empty())
             file.write(full_config);
         file.write("; CONFIG_BLOCK_END\n\n");
@@ -4557,9 +4583,8 @@ void GCode::apply_print_config(const PrintConfig &print_config)
     m_scaled_resolution = scaled<double>(print_config.resolution.value);
 }
 
-void GCode::append_full_config(const Print &print, std::string &str)
+void GCode::append_full_config(const DynamicPrintConfig &cfg, std::string &str)
 {
-    const DynamicPrintConfig &cfg = print.full_print_config();
     // Sorted list of config keys, which shall not be stored into the G-code. Initializer list.
     static const std::set<std::string_view> banned_keys({
         "compatible_printers"sv,

SoftFever/OrcaSlicer

(cherry picked from commit 038fc1c18220cef8c269d272059afea503fb5d5b)

src/libslic3r/GCode.cpp
@@ -5059,7 +5059,29 @@ void GCode::apply_print_config(const PrintConfig &print_config)
 
 void GCode::append_full_config(const Print &print, std::string &str)
 {
-    const DynamicPrintConfig &cfg = print.full_print_config();
+    DynamicPrintConfig cfg = print.full_print_config();
+    { // correct the flush_volumes_matrix with flush_multiplier values
+        std::vector<double> temp_cfg_flush_multiplier = cfg.option<ConfigOptionFloats>("flush_multiplier")->values;
+        std::vector<double> temp_flush_volumes_matrix = cfg.option<ConfigOptionFloats>("flush_volumes_matrix")->values;
+        auto                temp_filament_color       = cfg.option<ConfigOptionStrings>("filament_colour")->values;
+        size_t              heads_count_tmp           = temp_cfg_flush_multiplier.size(),
+               matrix_value_count                     = temp_flush_volumes_matrix.size() / temp_cfg_flush_multiplier.size(),
+               filament_count_tmp                     = temp_filament_color.size();
+        if (filament_count_tmp * filament_count_tmp * heads_count_tmp == temp_flush_volumes_matrix.size()) {
+            for (size_t idx = 0; idx < heads_count_tmp; ++idx) {
+                double temp_cfg_flush_multiplier_idx = temp_cfg_flush_multiplier[idx];
+                size_t temp_begin_t = idx * matrix_value_count, temp_end_t = (idx + 1) * matrix_value_count;
+                std::transform(temp_flush_volumes_matrix.begin() + temp_begin_t, temp_flush_volumes_matrix.begin() + temp_end_t,
+                               temp_flush_volumes_matrix.begin() + temp_begin_t,
+                               [temp_cfg_flush_multiplier_idx](double inputx) { return inputx * temp_cfg_flush_multiplier_idx; });
+            }
+            cfg.option<ConfigOptionFloats>("flush_volumes_matrix")->values = temp_flush_volumes_matrix;
+        } else if (filament_count_tmp == 1) {
+        } // Not applicable to flush matrix situations
+        else { // flush_volumes_matrix value count error?
+            throw Slic3r::SlicingError(_(L("Flush volumes matrix do not match to the correct size!")));
+        }
+    }
     // Sorted list of config keys, which shall not be stored into the G-code. Initializer list.
     static const std::set<std::string_view> banned_keys( {
         "compatible_printers"sv,