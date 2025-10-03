321d6c8c38f2acc1c661c728212cb40e7a3a24ef
Ian Bassi <ian.bassi@outlook.com>
2025-10-03 12:55:15 -0300

    RepRap P"type" Type of input shaping to use, not case sensitive.

--- src/libslic3r/GCodeWriter.cpp
+++ src/libslic3r/GCodeWriter.cpp
@@ -385,7 +385,6 @@ std::string GCodeWriter::set_input_shaping(char axis, float damp, float freq, st
     } else if (FLAVOR_IS(gcfRepRapFirmware)) {
         gcode << "M593";
         if (!type.empty() && type != "Default") {
-            std::transform(type.begin(), type.end(), type.begin(), ::tolower);// RepRapFirmware expects lowercase input shaper type
             gcode << " P\"" << type << "\"";
         }
         if (freq > 0.0f) {
