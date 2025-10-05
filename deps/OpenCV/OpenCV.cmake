df1c5722ef60c9a80f7395723887253f216f35bd
Noisyfox <timemanager.rick@gmail.com>
2025-10-05 16:06:40 +0800

    Revert "OpenCV don't build its own libjpeg, to avoid symbol conflict"
    This reverts commit 7a90b26e57449567f864efce2d312f348a0d4328.

--- deps/OpenCV/OpenCV.cmake
+++ deps/OpenCV/OpenCV.cmake
@@ -12,7 +12,6 @@ orcaslicer_add_cmake_project(OpenCV
     URL https://github.com/opencv/opencv/archive/refs/tags/4.6.0.tar.gz
     URL_HASH SHA256=1ec1cba65f9f20fe5a41fda1586e01c70ea0c9a6d7b67c9e13edf0cfe2239277
     PATCH_COMMAND git apply ${OpenCV_DIRECTORY_FLAG} --verbose --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/0001-vs2022.patch  ${CMAKE_CURRENT_LIST_DIR}/0002-clang19-macos.patch
-    DEPENDS ${JPEG_PKG}
     CMAKE_ARGS
     -DBUILD_SHARED_LIBS=0
        -DBUILD_PERE_TESTS=OFF
@@ -21,7 +20,7 @@ orcaslicer_add_cmake_project(OpenCV
        -DBUILD_EXAMPLES=OFF
        -DBUILD_JASPER=OFF
        -DBUILD_JAVA=OFF
-       -DBUILD_JPEG=OFF
+       -DBUILD_JPEG=ON
        -DBUILD_APPS_LIST=version
        -DBUILD_opencv_apps=OFF
        -DBUILD_opencv_java=OFF
