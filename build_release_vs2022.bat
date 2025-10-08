fbd24c9f5aeefa8d554eb60768722cff933d58fd
Ocraftyone <Ocraftyone@users.noreply.github.com>
2025-10-08 02:59:36 -0400

    Add argument parsing

--- build_release_vs2022.bat
+++ build_release_vs2022.bat
@@ -1,9 +1,134 @@
 @REM OrcaSlicer build script for Windows
 @echo off
+
+:: Unset errorlevel variable
+:: If errorlevel is manually set, it is not updated upon program calls
+set errorlevel=
+
+:: Create a local scope for the script
+setlocal enableDelayedExpansion
+
 set WP=%CD%
 
+:: Arg definition count
+set argdefn=0
+
+:: Error check macro
+set "error_check=if not ^!errorlevel^! == 0 echo Exiting script with code ^!errorlevel^! & exit /b ^!errorlevel^!"
+
+:: Define arguments
+call :add_arg pack_deps bool p pack
+call :add_arg build_deps bool d deps
+call :add_arg build_slicer bool s slicer
+call :add_arg build_debug bool b debug
+call :add_arg build_debuginfo bool e debuginfo
+
+:: handle arguments from input
+:handle_args_loop
+    if "%1" == "" goto :handle_args_loop_end
+
+    setlocal
+    set arg=%1
+    set finalize_cmd=
+
+    call :debug_msg Begin handling arg "%arg%"
+
+    if not "%arg:~0,2%" == "--" goto :HAL_check_short
+    
+    ::IF long arg
+    call :debug_msg Processing long arg
+    
+    call :find_arg long %arg:~2%
+    %error_check%
+    set idx=%ret%
+    
+    call :get_arg_type %idx%
+    %error_check%
+    set type=%ret%
+    
+    if /I "%type%" == "string" (
+        set string_val=%2
+        :: Ensure there is a string value
+        if "!string_val!" == "" (
+            echo Error in handle_args_loop: The option "%arg:~2%" requires a value
+            exit /b 1
+        )
+        :: Ensure the string value isn't another argument
+        if "!string_val:~0,1!" == "-" (
+            echo Error in handle_args_loop: The option "%arg:~2%" requires a value. "!string_val!" is invalid as it may be another argument. Enclose the value with double quotes to override this.
+            exit /b 1
+        )
+        shift
+    )
+    
+    call :set_arg %idx% %string_val%
+    %error_check%
+    goto :handle_args_loop_reset
+
+    :HAL_check_short
+    if not "%arg:~0,1%" == "-" (
+        :: IF invalid arg
+        echo Unknown argument: %arg%
+        exit /b 1
+    )
+
+    :: IF short arg
+    call :debug_msg processing short args
+    set /A charidx=1
+    :short_arg_loop
+        if "!arg:~%charidx%,1!" == "" goto :handle_args_loop_reset
+        
+        call :find_arg short !arg:~%charidx%,1!
+        %error_check%
+        set idx=%ret%
+        
+        call :get_arg_type %idx%
+        %error_check%
+        set type=%ret%
+        
+        set /A start_idx = %charidx% + 1
+        if /I "%type%" == "string" (
+            :: Check if there are additional characters after the found string argument
+            set remaining=!arg:~%start_idx%!
+            if defined remaining (
+                :: If there are remaining characters, use them as the string value
+                set string_val=!remaining!
+            ) else (
+                :: Otherwise, use the next argument
+                set string_val=%2
+                shift
+            )
+            :: Ensure there is a string value
+            if "!string_val!" == "" (
+                echo Error in handle_args_loop: The option "!arg:~%charidx%,1!" requires a value
+                exit /b 1
+            )
+            :: Ensure the string value isn't another argument
+            if "!string_val:~1!" == "-" (
+                echo Error in handle_args_loop: The option "!arg:~%charidx%,1!" requires a value. "!string_val!" is invalid as it may be another argument. Enclose the value with double quotes to override this.
+                exit /b 1
+            )
+            call :set_arg !idx! !string_val!
+            %error_check%
+            goto :handle_args_loop_reset
+        )
+        
+        call :set_arg %idx%
+        %error_check%
+        set /A charidx+=1
+    goto :short_arg_loop
+
+    :handle_args_loop_reset
+    :: End local scope for this loop and use finalize_cmd to set global variables from the set_arg function
+    endlocal %finalize_cmd%
+    shift
+    goto :handle_args_loop
+
+:handle_args_loop_end
+
+
 @REM Pack deps
-if "%1"=="pack" (
+if "%pack_deps%"=="ON" (
     setlocal ENABLEDELAYEDEXPANSION 
     cd %WP%/deps/build
     for /f "tokens=2-4 delims=/ " %%a in ('date /t') do set build_date=%%c%%b%%a
@@ -13,17 +138,11 @@ if "%1"=="pack" (
     exit /b 0
 )
 
-set debug=OFF
-set debuginfo=OFF
-if "%1"=="debug" set debug=ON
-if "%2"=="debug" set debug=ON
-if "%1"=="debuginfo" set debuginfo=ON
-if "%2"=="debuginfo" set debuginfo=ON
-if "%debug%"=="ON" (
+if "%build_debug%"=="ON" (
     set build_type=Debug
     set build_dir=build-dbg
 ) else (
-    if "%debuginfo%"=="ON" (
+    if "%build_debuginfo%"=="ON" (
         set build_type=RelWithDebInfo
         set build_dir=build-dbginfo
     ) else (
@@ -41,7 +160,7 @@ set DEPS=%CD%/OrcaSlicer_dep
 set "SIG_FLAG="
 if defined ORCA_UPDATER_SIG_KEY set "SIG_FLAG=-DORCA_UPDATER_SIG_KEY=%ORCA_UPDATER_SIG_KEY%"
 
-if "%1"=="slicer" (
+if "%build_slicer%"=="ON" (
     GOTO :slicer
 )
 echo "building deps.."
@@ -51,7 +170,7 @@ cmake ../ -G "Visual Studio 17 2022" -A x64 -DDESTDIR="%DEPS%" -DCMAKE_BUILD_TYP
 cmake --build . --config %build_type% --target deps -- -m
 @echo off
 
-if "%1"=="deps" exit /b 0
+if "%build_deps%"=="ON" exit /b 0
 
 :slicer
 echo "building Orca Slicer..."
@@ -67,3 +186,119 @@ cd ..
 call scripts/run_gettext.bat
 cd %build_dir%
 cmake --build . --target install --config %build_type%
+
+:: End of script
+exit /b 0
+
+
+
+:: ---------------------------------------------
+:: BEGIN FUNCTION DEFINITIONS
+:: ---------------------------------------------
+
+:debug_msg
+    if "%debugscript%" == "ON" echo %*
+    exit /b 0
+
+:: add_arg <variable_name> <type:bool,string> <short_flag> [<long_flag>]
+:add_arg
+    call :debug_msg starting function: add_arg "%~1" "%~2" "%~3" "%~4"
+
+    set argdefs[%argdefn%].VARIABLE_NAME=%~1
+    set argdefs[%argdefn%].TYPE=%~2
+    set argdefs[%argdefn%].SHORT_FLAG=%~3
+    set argdefs[%argdefn%].LONG_FLAG=%~4
+
+    :: ensure variable is not set
+    set %~1=
+
+    call :debug_msg VARIABLE_NAME: !argdefs[%argdefn%].VARIABLE_NAME!
+    call :debug_msg TYPE: !argdefs[%argdefn%].TYPE!
+    call :debug_msg SHORT_FLAG: !argdefs[%argdefn%].SHORT_FLAG!
+    call :debug_msg LONG_FLAG: !argdefs[%argdefn%].LONG_FLAG!
+
+    :: Increment argument
+    set /A argdefn+=1
+
+    exit /b 0
+
+
+:: find_arg <type:short,long> <flag>
+:find_arg
+    setlocal
+    call :debug_msg starting function: find_arg "%~1" "%~2"
+
+    set type=
+    if /I "%~1" == "short" set type=SHORT
+    if /I "%~1" == "long" set type=LONG
+    if not defined type (
+        endlocal
+        set ret=
+        exit /b 1
+    )
+
+    call :debug_msg find_arg type=%type%
+    set /A range_end = %argdefn% - 1
+    for /L %%i in (0, 1, %range_end%) do (
+        if "!argdefs[%%i].%type%_FLAG!" == "%~2" (
+            set idx=%%i
+            goto :find_arg_cont
+        )
+    )
+
+    echo Error in find_arg: Failed to find arg "%~2"
+
+    endlocal
+    set ret=
+    exit /b 1
+
+    :find_arg_cont
+    call :debug_msg find_arg: found at %idx%
+    endlocal & (
+        set ret=%idx%
+    )
+    exit /b 0
+
+
+:: set_arg <arg_index> [<string_val>]
+:set_arg
+    call :debug_msg starting function: set_arg "%~1" "%~2"
+
+    if "%~1" == "" (
+        echo Error in set_arg: no index provided
+        exit /b 1
+    )
+
+    setlocal
+    if /I "!argdefs[%~1].TYPE!" == "bool" (
+        call :debug_msg set_arg: setting bool type to ON
+        set val=ON
+    ) else (
+        set val=%~2
+        set quote_char="
+        if "!val:~0,1!" == "!quote_char!" (
+            if "!val:~-1,1!" == "!quote_char!" (
+                set val=%val:~1,-1%
+            )
+        )
+        call :debug_msg set_arg: setting string type to %~2
+    )
+    set var_name=!argdefs[%~1].VARIABLE_NAME!
+
+    endlocal & (
+        :: Set variable in parent scope
+        set %var_name%=%val%
+        :: Add variable to finalize command
+        set "finalize_cmd=%finalize_cmd% & set "%var_name%=%val%""
+    )
+
+    exit /b 0
+
+
+:: get_arg_type <arg_index>
+:get_arg_type
+    call :debug_msg starting function get_arg_type "%~1"
+    setlocal
+    set type=!argdefs[%~1].TYPE!
+    endlocal & set ret=%type%
+    exit /b 0
