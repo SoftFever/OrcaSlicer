bfd8a0cbd78a67cc88d514356e6955b7eee0276e
milk <yongxin.chang@bambulab.com>
2025-10-13 16:50:47 +0800

    FIX:add darkmode of thermalprecondition
    jira:[STUDIO-14662]
    
    Change-Id: I75998ff04e1073d07e23ff6970dce90627b7b860
    (cherry picked from commit 93fc862f7e74c52d7a9cfb75d24ecf0685788341)

--- src/slic3r/GUI/ThermalPreconditioningDialog.hpp
+++ src/slic3r/GUI/ThermalPreconditioningDialog.hpp
@@ -11,29 +11,30 @@
 namespace Slic3r {
 
 class MachineObject;
+namespace GUI {
 
 class ThermalPreconditioningDialog : public wxDialog
 {
 public:
     ThermalPreconditioningDialog(wxWindow *parent, std::string dev_id, const wxString &remaining_time);
-    ~ThermalPreconditioningDialog() ;
-
+    ~ThermalPreconditioningDialog();
 
     void update_thermal_remaining_time();
 
 private:
     void create_ui();
-    void on_ok_clicked(wxCommandEvent& event);
-    void            on_timer(wxTimerEvent &event);
+    void on_ok_clicked(wxCommandEvent &event);
+    void on_timer(wxTimerEvent &event);
 
-    std::string   m_dev_id;
-    wxTimer*         m_refresh_timer;
-    wxStaticText* m_remaining_time_label;
-    wxStaticText* m_explanation_label;
-    wxButton* m_ok_button;
-    wxStaticBitmap* m_title_bitmap;
+    std::string     m_dev_id;
+    wxTimer        *m_refresh_timer;
+    wxStaticText   *m_remaining_time_label;
+    wxStaticText   *m_explanation_label;
+    wxButton       *m_ok_button;
+    wxStaticBitmap *m_title_bitmap;
 
     DECLARE_EVENT_TABLE()
 };
 
+} // namespace GUI
 } // namespace Slic3r
