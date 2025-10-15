33a59aaa9628e1423597ee5b8e573117a8e76ae0
RF47 <162915171+RF47@users.noreply.github.com>
2025-10-15 19:38:45 -0300

    Improve multiline fill offset and polyline closure
    Changed offset type from jtRound to jtMiter in multiline_fill for better geometry. Updated polyline conversion to require at least 3 points and ensured polylines are closed if not already. Updated FillGyroid, FillTpmsD, and FillTpmsFK to pass the 'close' argument to multiline_fill.
    
    cleaning
    
    FillAdaptive Noncross
    
    Only use clipper if worth it
    
    safeguard
    
    fix overlap
    
    Update FillRectilinear.cpp
    
    FilllRectilineal multiline clipper
    
    Update FillRectilinear.cpp
    
    FilllRectilineal multiline clipper
    
    Update FillRectilinear.cpp
    
    Update FillRectilinear.cpp
    
    fix 3d honeycomb
    
    Simplify polylines
    
    Update FillBase.cpp
    
    Update FillBase.cpp
    
    cleaning
    
    Improved Multiline Function
    
    This ensures `multiline_fill()` will correctly generate multiline infill with
    closed loop polylines if the input infill line is a closedloop polyline. This
    ensures that the multiline infill doesn't have little gaps or overlaps at the
    "closed point" of the original infill line.
    
    This changes how the tangent is calculated for the first and last points in a
    polyline if the first and last points are the same, making it a closed loop.
    Instead of just using the first or last line segment, it uses the line segment
    between the points before the last point and after the first point, the same
    way that all the other poly-line mid points are handled.
    
    It also uses eigen vector operations to calculate the points instead of
    explicitly calculating the x and y values. This is probably faster, and if not
    then it is at least more concise.
    
    Hibrid Multiline Function
    
    Update FillRectilinear.cpp
    
    Update FillRectilinear.cpp
    
    Update FillRectilinear.cpp
    
    Update FillRectilinear.cpp
    
    clipperutils multiline hibrido
    
    Update FillBase.cpp
    
    Update FillBase.cpp
    
    Update FillBase.cpp
    
    multiline hibrido
    
    arc tolerance
    
    multiline con union
    
    Update FillBase.cpp
    
    Update FillBase.cpp
    
    Update FillBase.cpp
    
    Co-Authored-By: Ian Bassi <12130714+ianalexis@users.noreply.github.com>
    Co-Authored-By: Donovan Baarda <dbaarda@gmail.com>

--- src/libslic3r/Fill/FillGyroid.cpp
+++ src/libslic3r/Fill/FillGyroid.cpp
@@ -185,7 +185,7 @@ void FillGyroid::_fill_surface_single(
 		pl.translate(bb.min);
 
     // Apply multiline offset if needed
-    multiline_fill(polylines, params, spacing);
+    multiline_fill(polylines, params, spacing, true);
 
 	polylines = intersection_pl(polylines, expolygon);
 
