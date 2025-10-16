a89d63a27ec66c78d40b41614ec200effc1f77e6
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2025-10-16 13:57:46 +0200

    build: add flake for dependencies

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cmake
+          gettext
+          libtool
+          automake
+          autoconf
+          texinfo
+        ];
+      };
+    });
+}
