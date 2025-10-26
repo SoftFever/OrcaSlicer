bambulab/BambuStudio

FIX: TabPrinter toggle_option with variant_index
Change-Id: I9d3a2f2d4707add057c881d20830f9fc1b4a4a89
Jira: STUDIO-13499

src/slic3r/GUI/Tab.cpp
@@ -4710,7 +4710,7 @@ void TabPrinter::toggle_options()
             //BBS
             toggle_option(el, retraction && !use_firmware_retraction, i);
 
-        bool wipe = retraction && m_config->opt_bool_nullable("wipe", i);
+        bool wipe = retraction && m_config->opt_bool_nullable("wipe", variant_index);
         toggle_option("retract_before_wipe", wipe, i);
 
         if (use_firmware_retraction && wipe) {

SoftFever/OrcaSlicer

(cherry picked from commit efa38cd598b27f76e620becc981e2cde93e9c4b6)

src/slic3r/GUI/Tab.cpp
@@ -5140,8 +5140,9 @@ void TabPrinter::toggle_options()
             //BBS
             toggle_option(el, retraction && !use_firmware_retraction, i);
 
-        bool wipe = retraction && m_config->opt_bool("wipe", i);
+        bool wipe = retraction && m_config->opt_bool("wipe", variant_index);
         toggle_option("retract_before_wipe", wipe, i);
+
         if (use_firmware_retraction && wipe) {
             //wxMessageDialog dialog(parent(),
             MessageDialog dialog(parent(),