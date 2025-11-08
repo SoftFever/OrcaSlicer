6ce89246a10989531543db67f16184fdd859bd23
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2025-11-08 17:37:35 +0100

    Update CGAL.cmake

--- deps/CGAL/CGAL.cmake
+++ deps/CGAL/CGAL.cmake
@@ -5,10 +5,10 @@ endif ()
 orcaslicer_add_cmake_project(
     CGAL
     # GIT_REPOSITORY https://github.com/CGAL/cgal.git
-    # GIT_TAG        bec70a6d52d8aacb0b3d82a7b4edc3caa899184b # releases/CGAL-6.0.2
+    # GIT_TAG        3654f780ae0c64675cabaef0e5ddaf904c48b4b7 # releases/CGAL-5.6.3
     # For whatever reason, this keeps downloading forever (repeats downloads if finished)
-    URL      https://github.com/CGAL/cgal/releases/download/v6.0.2/CGAL-6.0.2.zip
-    URL_HASH SHA256=1b63ecf4506c6744c5c95196df9e6f8742bae131d6b312089c826048859ee1e8
+    URL      https://github.com/CGAL/cgal/releases/download/v5.6.3/CGAL-5.6.3.zip
+    URL_HASH SHA256=5d577acb4a9918ccb960491482da7a3838f8d363aff47e14d703f19fd84733d4
     DEPENDS dep_Boost dep_GMP dep_MPFR
 )
 
