b692038d79df3b8ba3fb31712213c6ee541edac7
Ian Bassi <ian.bassi@outlook.com>
2025-11-21 15:08:20 -0300

    Switch multiline offset logic to Clipper2
    Replaces Clipper-based multiline offset logic in FillBase.cpp with Clipper2, using InflatePaths and Union for offsetting and merging. Adds new conversion utilities in Clipper2Utils for handling Paths64 to Polygons/Polylines and updates headers accordingly.

--- src/libslic3r/Clipper2Utils.hpp
+++ src/libslic3r/Clipper2Utils.hpp
@@ -4,8 +4,15 @@
 #include "ExPolygon.hpp"
 #include "Polygon.hpp"
 #include "Polyline.hpp"
+#include "clipper2/clipper.h"
 
 namespace Slic3r {
+
+Clipper2Lib::Paths64 Slic3rPolylines_to_Paths64(const Slic3r::Polylines& in);
+Clipper2Lib::Paths64 Slic3rPolygons_to_Paths64(const Slic3r::Polygons& in);
+Clipper2Lib::Paths64 Slic3rExPolygons_to_Paths64(const Slic3r::ExPolygons& in);
+Slic3r::Polylines  Paths64_to_polylines(const Clipper2Lib::Paths64& in);
+Slic3r::Polygons   Paths64_to_polygons(const Clipper2Lib::Paths64& in);
 
 Slic3r::Polylines  intersection_pl_2(const Slic3r::Polylines& subject, const Slic3r::Polygons& clip);
 Slic3r::Polylines  diff_pl_2(const Slic3r::Polylines& subject, const Slic3r::Polygons& clip);
