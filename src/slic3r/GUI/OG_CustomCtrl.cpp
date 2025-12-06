21c3b2801f78cc7c48a2786c28f37eac66095c45
pi-squared-studio <pi.squared.studio@gmail.com>
2025-12-06 18:14:33 +0300

    separator placement tips in code

--- src/slic3r/GUI/OG_CustomCtrl.cpp
+++ src/slic3r/GUI/OG_CustomCtrl.cpp
@@ -774,6 +774,7 @@ int OG_CustomCtrl::CtrlLine::get_max_opt_pos() // ORCA: Find the max option fiel
 
 void OG_CustomCtrl::CtrlLine::render_separator(wxDC& dc, wxCoord h_pos, wxCoord v_pos)
 {
+    // v_pos += 0; // here is vertical separator line displacement
     wxPoint begin(h_pos, v_pos);
     wxPoint end(get_max_opt_pos(), v_pos);
     wxPen old_pen = dc.GetPen();
@@ -781,7 +782,7 @@ void OG_CustomCtrl::CtrlLine::render_separator(wxDC& dc, wxCoord h_pos, wxCoord 
     dc.SetPen(fc);
     dc.DrawLine(begin, end);
     dc.SetPen(old_pen);
-    height = 1;
+    height = 1;  // here is overall height of separator area
     //draw_title(dc, {h_pos, v_pos}, og_line.label, &fc, h_pos); //TODO: Make separators labelable
 }
 
