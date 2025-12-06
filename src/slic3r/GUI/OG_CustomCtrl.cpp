8d238871eda6930c37ee1767b359d3a901a91067
pi-squared-studio <pi.squared.studio@gmail.com>
2025-12-06 19:19:16 +0300

    fix separator width

--- src/slic3r/GUI/OG_CustomCtrl.cpp
+++ src/slic3r/GUI/OG_CustomCtrl.cpp
@@ -753,28 +753,26 @@ void OG_CustomCtrl::CtrlLine::update_visibility(ConfigOptionMode mode)
 
 int OG_CustomCtrl::CtrlLine::get_max_opt_pos() // ORCA: Find the max option field position
 {
-    Field* field;
+    int h = 0;
     for (auto line : ctrl->opt_group->get_lines())
         if (!line.is_separator()) {
-            field = ctrl->opt_group->get_field(line.get_options().front().opt_id);
-            break;
+            Field* field = ctrl->opt_group->get_field(line.get_options().front().opt_id);
+            if (field->getSizer()) {
+                auto children = field->getSizer()->GetChildren();
+                for (auto child : children)
+                    if (child->IsWindow())
+                        h = max(h, child->GetWindow()->GetPosition().x + child->GetWindow()->GetSize().x);
+            } else if (field->getWindow())
+                h = max(h, field->getWindow()->GetPosition().x + field->getWindow()->GetSize().x);
         }
-    if (field) {
-        if (field->getSizer()) {
-            auto children = field->getSizer()->GetChildren();
-            for (auto child : children)
-                if (child->IsWindow())
-                    return child->GetWindow()->GetPosition().x + child->GetWindow()->GetSize().x;
-        } else if (field->getWindow()) {
-            return field->getWindow()->GetPosition().x + field->getWindow()->GetSize().x;
-        }
-    }
+    if (h)
+        return h;
     return ctrl->GetSize().GetWidth() - ctrl->m_h_gap;
 }
 
 void OG_CustomCtrl::CtrlLine::render_separator(wxDC& dc, wxCoord h_pos, wxCoord v_pos)
 {
-    // v_pos += 0; // here is vertical separator line displacement
+    // v_pos += 0; // here is separator line vertical displacement
     wxPoint begin(h_pos, v_pos);
     wxPoint end(get_max_opt_pos(), v_pos);
     wxPen old_pen = dc.GetPen();
